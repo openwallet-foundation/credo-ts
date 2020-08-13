5322b31554d675eff14506352235e3a93c55b12d
Jakub Koci <jakub.koci@gmail.com>
2020-08-13 12:58:48 +0200

    Generalize usage of DID in ledger tests
    Signed-off-by: Jakub Koci <jakub.koci@gmail.com>

--- src/lib/__tests__/ledger.test.ts
+++ src/lib/__tests__/ledger.test.ts
@@ -3,8 +3,13 @@ import { Agent, InboundTransporter, OutboundTransporter } from '..';
 import { OutboundPackage } from '../types';
 import path from 'path';
 import indy from 'indy-sdk';
+import { DidInfo } from '../wallet/Wallet';
 
 jest.setTimeout(15000);
+
+// TODO Is there a more restrict way how to define these regex?
+const DidRegExp = /[-_./a-zA-Z0-9]{8,64}/;
+const VerkeyRegExp = /[-_./a-zA-Z0-9]{8,64}/;
 
 const faberConfig = {
   label: 'Faber',
@@ -15,6 +20,7 @@ const faberConfig = {
 describe('ledger', () => {
   let faberAgent: Agent;
   let schemaId: SchemaId;
+  let faberAgentPublicDid: DidInfo | Record<string, undefined>;
 
   beforeAll(async () => {
     faberAgent = new Agent(faberConfig, new DummyInboundTransporter(), new DummyOutboundTransporter(), indy);
@@ -37,31 +43,22 @@ describe('ledger', () => {
     // We're pretending we have Steward DID to have the write permission to the ledger. This is a small simplification
     // because, in the real world, the agent doesn't necessarily need the write permission and could just create and
     // sign request, send it to another agent that has the permission and ask him to write it on its behalf.
-    const didInfo = { seed: 'ENTER_SEED' };
+    const seed = process.env.TEST_AGENT_PUBLIC_DID_SEED;
 
-    await faberAgent.initPublicDid(didInfo.seed);
-    const faberAgentPublicDid = faberAgent.getPublicDid();
-    console.log('faberAgentPublicDid', faberAgentPublicDid);
-
-    expect(faberAgentPublicDid).toEqual({
-      did: 'TygUVyv8Je7GRPL9o1nJm2',
-      verkey: 'Fhoe1CzB6zJsfX2VcnvvpzhzRoL2JE2v253scM6Bm43z',
-    });
-  });
-
-  test('test taa', async () => {
-    const { did } = faberAgent.getPublicDid();
-
-    if (!did) {
-      throw new Error('Agent does not have publid did.');
+    if (!seed) {
+      throw new Error('There is no framework seed in environment variables.');
     }
 
-    const result = await faberAgent.ledger.getTransactionAuthorAgreement();
-    expect(result).toEqual(
+    const didInfo = { seed };
+
+    await faberAgent.initPublicDid(didInfo.seed);
+    faberAgentPublicDid = faberAgent.getPublicDid();
+    console.log('faberAgentPublicDid', faberAgentPublicDid);
+
+    expect(faberAgentPublicDid).toEqual(
       expect.objectContaining({
-        digest: expect.any(String),
-        ratification_ts: expect.any(Number),
-        text: expect.any(String),
+        did: expect.stringMatching(DidRegExp),
+        verkey: expect.stringMatching(VerkeyRegExp),
       })
     );
   });
@@ -75,8 +72,8 @@ describe('ledger', () => {
 
     const result = await faberAgent.ledger.getPublicDid(did);
     expect(result).toEqual({
-      did: 'TygUVyv8Je7GRPL9o1nJm2',
-      verkey: 'Fhoe1CzB6zJsfX2VcnvvpzhzRoL2JE2v253scM6Bm43z',
+      did: faberAgentPublicDid.did,
+      verkey: faberAgentPublicDid.verkey,
       role: '101',
     });
   });
@@ -92,11 +89,11 @@ describe('ledger', () => {
     [schemaId] = await faberAgent.ledger.registerCredentialSchema(schemaTemplate);
     const [ledgerSchemaId, ledgerSchema] = await faberAgent.ledger.getSchema(schemaId);
 
-    expect(ledgerSchemaId).toBe(`TygUVyv8Je7GRPL9o1nJm2:2:${schemaName}:1.0`);
+    expect(ledgerSchemaId).toBe(`${faberAgentPublicDid.did}:2:${schemaName}:1.0`);
     expect(ledgerSchema).toEqual(
       expect.objectContaining({
         attrNames: expect.arrayContaining(['name', 'age']),
-        id: `TygUVyv8Je7GRPL9o1nJm2:2:${schemaName}:1.0`,
+        id: `${faberAgentPublicDid.did}:2:${schemaName}:1.0`,
         name: schemaName,
         seqNo: expect.any(Number),
         ver: '1.0',
@@ -117,7 +114,7 @@ describe('ledger', () => {
     const [credDefId] = await faberAgent.ledger.registerCredentialDefinition(credentialDefinitionTemplate);
     const [ledgerCredDefId, ledgerCredDef] = await faberAgent.ledger.getCredentialDefinition(credDefId);
 
-    const credDefIdRegExp = new RegExp(`TygUVyv8Je7GRPL9o1nJm2:3:CL:[0-9]+:TAG`);
+    const credDefIdRegExp = new RegExp(`${faberAgentPublicDid.did}:3:CL:[0-9]+:TAG`);
     expect(ledgerCredDefId).toEqual(expect.stringMatching(credDefIdRegExp));
     expect(ledgerCredDef).toEqual(
       expect.objectContaining({
