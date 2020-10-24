0aac0f2563737ca549f441ab21f0d78f52026509
Timo Glastra <timo@animo.id>
2020-10-24 16:03:12 +0200

    add JsonTransformer unit tests
    Signed-off-by: Timo Glastra <timo@animo.id>

---
+++ src/lib/utils/__tests__/JsonTransformer.test.ts
@@ -0,0 +1,72 @@
+import { ConnectionInvitationMessage } from '../../protocols/connections/ConnectionInvitationMessage';
+import { JsonTransformer } from '../JsonTransformer';
+
+describe('JsonTransformer', () => {
+  describe('toJSON', () => {
+    it('transforms class instance to JSON object', () => {
+      const invitation = new ConnectionInvitationMessage({
+        did: 'did:sov:test1234',
+        id: 'afe2867e-58c3-4a8d-85b2-23370dd9c9f0',
+        label: 'test-label',
+      });
+
+      const json = {
+        '@type': 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation',
+        '@id': 'afe2867e-58c3-4a8d-85b2-23370dd9c9f0',
+        label: 'test-label',
+        did: 'did:sov:test1234',
+      };
+
+      expect(JsonTransformer.toJSON(invitation)).toEqual(json);
+    });
+  });
+
+  describe('fromJSON', () => {
+    it('transforms JSON object to class instance', () => {
+      const json = {
+        '@type': 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation',
+        '@id': 'afe2867e-58c3-4a8d-85b2-23370dd9c9f0',
+        label: 'test-label',
+        did: 'did:sov:test1234',
+      };
+
+      const invitation = new ConnectionInvitationMessage({
+        did: 'did:sov:test1234',
+        id: 'afe2867e-58c3-4a8d-85b2-23370dd9c9f0',
+        label: 'test-label',
+      });
+
+      expect(JsonTransformer.fromJSON(json, ConnectionInvitationMessage)).toEqual(invitation);
+    });
+  });
+
+  describe('serialize', () => {
+    it('transforms class instance to JSON string', () => {
+      const invitation = new ConnectionInvitationMessage({
+        did: 'did:sov:test1234',
+        id: 'afe2867e-58c3-4a8d-85b2-23370dd9c9f0',
+        label: 'test-label',
+      });
+
+      const jsonString =
+        '{"@type":"did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation","@id":"afe2867e-58c3-4a8d-85b2-23370dd9c9f0","label":"test-label","did":"did:sov:test1234"}';
+
+      expect(JsonTransformer.serialize(invitation)).toEqual(jsonString);
+    });
+  });
+
+  describe('deserialize', () => {
+    it('transforms JSON string to class instance', () => {
+      const jsonString =
+        '{"@type":"did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation","@id":"afe2867e-58c3-4a8d-85b2-23370dd9c9f0","label":"test-label","did":"did:sov:test1234"}';
+
+      const invitation = new ConnectionInvitationMessage({
+        did: 'did:sov:test1234',
+        id: 'afe2867e-58c3-4a8d-85b2-23370dd9c9f0',
+        label: 'test-label',
+      });
+
+      expect(JsonTransformer.deserialize(jsonString, ConnectionInvitationMessage)).toEqual(invitation);
+    });
+  });
+});
