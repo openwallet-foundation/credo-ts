db8e95d34b9b3d532481ed58d2da7a30c2bcfc18
Parth <parthkm78@gmail.com>
2020-12-16 18:17:03 +0530

    Message Type changes
    Signed-off-by: Parth <parthkm78@gmail.com>

--- src/lib/protocols/proof/messages/messages.ts
+++ src/lib/protocols/proof/messages/messages.ts
@@ -1,5 +1,5 @@
 export enum MessageType {
-  PresentProof = 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/propose-presentation',
+  ProposePresentation = 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/propose-presentation',
   RequestPresentation = 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/request-presentation',
   Presentation = 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/presentation',
   PresentationPreview = 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/presentation-preview',
