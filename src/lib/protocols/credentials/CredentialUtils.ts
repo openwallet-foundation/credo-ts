7d850a0a8539b447e788336bbf3e6f103d197f07
Timo Glastra <timo@animo.id>
2021-01-15 12:18:51 +0100

    add method to check encoding, improve encode (#160)
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/lib/protocols/credentials/CredentialUtils.ts
+++ src/lib/protocols/credentials/CredentialUtils.ts
@@ -25,11 +25,69 @@ export class CredentialUtils {
     }, {});
   }
 
-  private static encode(value: any) {
-    if (!isNaN(value)) {
+  /**
+   * Check whether the raw value matches the encoded version according to the encoding format described in Aries RFC 0037
+   * Use this method to ensure the received proof (over the encoded) value is the same as the raw value of the data.
+   *
+   * @param raw
+   * @param encoded
+   * @returns Whether raw and encoded value match
+   *
+   * @see https://github.com/hyperledger/aries-framework-dotnet/blob/a18bef91e5b9e4a1892818df7408e2383c642dfa/src/Hyperledger.Aries/Utils/CredentialUtils.cs#L78-L89
+   * @see https://github.com/hyperledger/aries-rfcs/blob/be4ad0a6fb2823bb1fc109364c96f077d5d8dffa/features/0037-present-proof/README.md#verifying-claims-of-indy-based-verifiable-credentials
+   */
+  public static checkValidEncoding(raw: any, encoded: string) {
+    return encoded === CredentialUtils.encode(raw);
+  }
+
+  /**
+   * Encode value according to the encoding format described in Aries RFC 0036/0037
+   *
+   * @param value
+   * @returns Encoded version of value
+   *
+   * @see https://github.com/hyperledger/aries-cloudagent-python/blob/0000f924a50b6ac5e6342bff90e64864672ee935/aries_cloudagent/messaging/util.py#L106-L136
+   * @see https://github.com/hyperledger/aries-rfcs/blob/be4ad0a6fb2823bb1fc109364c96f077d5d8dffa/features/0037-present-proof/README.md#verifying-claims-of-indy-based-verifiable-credentials
+   * @see https://github.com/hyperledger/aries-rfcs/blob/be4ad0a6fb2823bb1fc109364c96f077d5d8dffa/features/0036-issue-credential/README.md#encoding-claims-for-indy-based-verifiable-credentials
+   */
+  public static encode(value: any) {
+    const isString = typeof value === 'string';
+    const isEmpty = isString && value === '';
+    const isNumber = typeof value === 'number';
+    const isBoolean = typeof value === 'boolean';
+
+    // If bool return bool as number string
+    if (isBoolean) {
+      return Number(value).toString();
+    }
+
+    // If value is int32 return as number string
+    if (isNumber && this.isInt32(value)) {
       return value.toString();
     }
 
+    // If value is an int32 number string return as number string
+    if (isString && !isEmpty && !isNaN(Number(value)) && this.isInt32(Number(value))) {
+      return value;
+    }
+
+    if (isNumber) {
+      value = value.toString();
+    }
+
+    // If value is null we must use the string value 'None'
+    if (value === null || value === undefined) {
+      value = 'None';
+    }
+
     return new BigNumber(sha256.array(value)).toString();
+  }
+
+  private static isInt32(number: number) {
+    const minI32 = -2147483648;
+    const maxI32 = 2147483647;
+
+    // Check if number is integer and in range of int32
+    return Number.isInteger(number) && number >= minI32 && number <= maxI32;
   }
 }
