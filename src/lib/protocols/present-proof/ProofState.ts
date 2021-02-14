1918f22b18d0d5c726aeeaf51b301294960e384c
Timo Glastra <timo@animo.id>
2021-02-14 14:53:26 +0100

    update record states to match rfc
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/lib/protocols/present-proof/ProofState.ts
+++ src/lib/protocols/present-proof/ProofState.ts
@@ -1,10 +1,15 @@
+/**
+ * Present Proof protocol states as defined in RFC 0037
+ *
+ * @see https://github.com/hyperledger/aries-rfcs/tree/master/features/0037-present-proof#states
+ */
 export enum ProofState {
-  RequestSent = 'REQUEST_SENT',
-  RequestReceived = 'REQUEST_RECEIVED',
-  ProposalSent = 'PROPOSAL_SENT',
-  ProposalReceived = 'PROPOSAL_RECEIVED',
-  PresentationSent = 'PRESENTATION_SENT',
-  PresentationReceived = 'PRESENTATION_RECEIVED',
-  RejectSent = 'REJECT_SENT',
-  Done = 'DONE',
+  RequestSent = 'request-sent',
+  RequestReceived = 'request-received',
+  ProposalSent = 'proposal-sent',
+  ProposalReceived = 'proposal-received',
+  PresentationSent = 'presentation-sent',
+  PresentationReceived = 'presentation-received',
+  RejectSent = 'reject-sent',
+  Done = 'done',
 }
