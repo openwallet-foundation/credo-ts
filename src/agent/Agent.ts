eba8754cc370206cedbe60fb897f7e44d5bd6fa3
jakubkoci <jakub.koci@gmail.com>
2021-04-15 11:36:53 +0200

    Set inbound property as read only
    Co-authored-by: Timo Glastra <timo@animo.id>
    Signed-off-by: Jakub Koci <jakub.koci@gmail.com>

--- src/agent/Agent.ts
+++ src/agent/Agent.ts
@@ -40,7 +40,7 @@ export class Agent {
   protected messageReceiver: MessageReceiver
   protected dispatcher: Dispatcher
   protected messageSender: MessageSender
-  public inboundTransporter?: InboundTransporter
+  public readonly inboundTransporter?: InboundTransporter
 
   protected connectionService: ConnectionService
   protected proofService: ProofService
