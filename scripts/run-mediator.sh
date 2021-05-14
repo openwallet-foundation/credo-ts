60fc65fb4c337209161186941618ecba8410197b
Timo Glastra <timo@animo.id>
2021-05-14 10:18:20 +0200

    build: make indy-sdk dev dependency (#269)
    * ci: add names, use seed
    * build: fix syntax error in mediator script
    
    Signed-off-by: Timo Glastra <timo@animo.id>

--- scripts/run-mediator.sh
+++ scripts/run-mediator.sh
@@ -2,6 +2,7 @@
 
 AGENT="$1"
 YARN_COMMAND=yarn
+BUILD="$2"
 
 
 if [[ "$AGENT" = "mediator01" ]] || [[ "$AGENT" = "alice" ]]; then
@@ -13,6 +14,7 @@ if [[ "$AGENT" = "mediator01" ]] || [[ "$AGENT" = "alice" ]]; then
   WALLET_KEY=0000000000000000000000000Mediator01
   PUBLIC_DID=DtWRdd6C5dN5vpcN6XRAvu
   PUBLIC_DID_SEED=00000000000000000000000Forward01
+  MEDIATOR_COMMAND="prod:start"
 elif [[ "$AGENT" = "mediator02" ]] || [[ "$AGENT" = "bob" ]]; then
   AGENT_ENDPOINT="${AGENT_ENDPOINT:-http://localhost:3002}"
   AGENT_HOST=http://localhost
@@ -22,13 +24,30 @@ elif [[ "$AGENT" = "mediator02" ]] || [[ "$AGENT" = "bob" ]]; then
   WALLET_KEY=0000000000000000000000000Mediator02
   PUBLIC_DID=SHbU5SEwdmkQkVQ1sMwSEv
   PUBLIC_DID_SEED=00000000000000000000000Forward02
+  MEDIATOR_COMMAND="prod:start"
+elif [[ "$AGENT" = "mediator03" ]] || [[ "$AGENT" = "alice-ws" ]]; then
+  AGENT_ENDPOINT="${AGENT_ENDPOINT:-http://localhost:3003}"
+  AGENT_HOST=http://localhost
+  AGENT_PORT=3003
+  AGENT_LABEL=RoutingMediator03
+  WALLET_NAME=mediator03
+  WALLET_KEY=0000000000000000000000000Mediator03
+  PUBLIC_DID=DtWRdd6C5dN5vpcN6XRAvu
+  PUBLIC_DID_SEED=00000000000000000000000Forward03
+  MEDIATOR_COMMAND="prod:start-ws"
+elif [[ "$AGENT" = "mediator04" ]] || [[ "$AGENT" = "bob-ws" ]]; then
+  AGENT_ENDPOINT="${AGENT_ENDPOINT:-http://localhost:3004}"
+  AGENT_HOST=http://localhost
+  AGENT_PORT=3004
+  AGENT_LABEL=RoutingMediator04
+  WALLET_NAME=mediator04
+  WALLET_KEY=0000000000000000000000000Mediator04
+  PUBLIC_DID=SHbU5SEwdmkQkVQ1sMwSEv
+  PUBLIC_DID_SEED=00000000000000000000000Forward04
+  MEDIATOR_COMMAND="prod:start-ws"
 else
-  echo "Please specify which agent you want to run. Choose from 'alice' or 'bob'."
+  echo "Please specify which agent you want to run. Choose from 'alice', 'alice-ws', 'bob' or 'bob-ws'."
   exit 1
-fi
-
-if [ "$2" = "server" ]; then
-  YARN_COMMAND=.yarn/bin/yarn
 fi
 
 # Docker image already compiles. Not needed to do again
@@ -36,4 +55,4 @@ if [ "$RUN_MODE" != "docker" ]; then
   ${YARN_COMMAND} prod:build
 fi
 
-AGENT_ENDPOINT=${AGENT_ENDPOINT} AGENT_HOST=${AGENT_HOST} AGENT_PORT=${AGENT_PORT} AGENT_LABEL=${AGENT_LABEL} WALLET_NAME=${WALLET_NAME} WALLET_KEY=${WALLET_KEY} PUBLIC_DID=${PUBLIC_DID} PUBLIC_DID_SEED=${PUBLIC_DID_SEED} ${YARN_COMMAND} prod:start
+AGENT_ENDPOINT=${AGENT_ENDPOINT} AGENT_HOST=${AGENT_HOST} AGENT_PORT=${AGENT_PORT} AGENT_LABEL=${AGENT_LABEL} WALLET_NAME=${WALLET_NAME} WALLET_KEY=${WALLET_KEY} PUBLIC_DID=${PUBLIC_DID} PUBLIC_DID_SEED=${PUBLIC_DID_SEED} ${YARN_COMMAND} ${MEDIATOR_COMMAND}
