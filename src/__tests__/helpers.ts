a768bf43a19311756918394570ec00fa712eaac1
Timo Glastra <timo@animo.id>
2021-05-19 19:10:11 +0200

    fix: test failing because of moved import
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/__tests__/helpers.ts
+++ src/__tests__/helpers.ts
@@ -52,24 +52,6 @@ export function getBaseConfig(name: string, extraConfig: Partial<InitConfig> = {
   return config
 }
 
-// Custom matchers which can be used to extend Jest matchers via extend, e. g. `expect.extend({ toBeConnectedWith })`.
-export function toBeConnectedWith(received: ConnectionRecord, connection: ConnectionRecord) {
-  const pass = received.theirDid === connection.did && received.theirKey === connection.verkey
-  if (pass) {
-    return {
-      message: () =>
-        `expected connection ${received.did}, ${received.verkey} not to be connected to with ${connection.did}, ${connection.verkey}`,
-      pass: true,
-    }
-  } else {
-    return {
-      message: () =>
-        `expected connection ${received.did}, ${received.verkey} to be connected to with ${connection.did}, ${connection.verkey}`,
-      pass: false,
-    }
-  }
-}
-
 export async function waitForProofRecord(
   agent: Agent,
   {
