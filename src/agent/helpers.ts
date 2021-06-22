6a26337fe1f52d661bd33208354a85e15512aec4
jakubkoci <jakub.koci@gmail.com>
2021-06-22 16:36:55 +0200

    feat: Pack and send a message based on DidDoc services (#304)
    * Pack and send a message based on DidDoc services
    * Use keys from the inbound message instead of service when sending via return route
    
    Signed-off-by: Jakub Koci <jakub.koci@gmail.com>

--- src/agent/helpers.ts
+++ src/agent/helpers.ts
@@ -1,39 +1,13 @@
-import type { ConnectionRecord, ConnectionInvitationMessage } from '../modules/connections'
+import type { ConnectionRecord } from '../modules/connections'
 import type { OutboundMessage } from '../types'
 import type { AgentMessage } from './AgentMessage'
 
-import { AriesFrameworkError } from '../error'
-
 export function createOutboundMessage<T extends AgentMessage = AgentMessage>(
   connection: ConnectionRecord,
-  payload: T,
-  invitation?: ConnectionInvitationMessage
+  payload: T
 ): OutboundMessage<T> {
-  if (invitation) {
-    // TODO: invitation recipientKeys, routingKeys, endpoint could be missing
-    // When invitation uses DID
-    return {
-      connection,
-      payload,
-      recipientKeys: invitation.recipientKeys || [],
-      routingKeys: invitation.routingKeys || [],
-      senderVk: connection.verkey,
-    }
-  }
-
-  const { theirDidDoc } = connection
-
-  if (!theirDidDoc) {
-    throw new AriesFrameworkError(`DidDoc for connection with verkey ${connection.verkey} not found!`)
-  }
-
-  const [service] = theirDidDoc.didCommServices
-
   return {
     connection,
     payload,
-    recipientKeys: service.recipientKeys,
-    routingKeys: service.routingKeys ?? [],
-    senderVk: connection.verkey,
   }
 }
