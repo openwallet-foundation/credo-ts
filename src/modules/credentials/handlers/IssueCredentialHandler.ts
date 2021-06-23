8ae9fcdb3faa77b00ffc14a9a4f32288e389d1e3
Berend Sliedrecht <berend@animo.id>
2021-06-23 15:35:12 +0200

    Issue credential handler now checks for changes
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- src/modules/credentials/handlers/IssueCredentialHandler.ts
+++ src/modules/credentials/handlers/IssueCredentialHandler.ts
@@ -1,5 +1,6 @@
 import type { AgentConfig } from '../../../agent/AgentConfig'
 import type { Handler, HandlerInboundMessage } from '../../../agent/Handler'
+import type { CredentialRecord } from '../repository'
 import type { CredentialService } from '../services'
 
 import { createOutboundMessage } from '../../../agent/helpers'
@@ -26,13 +27,30 @@ export class IssueCredentialHandler implements Handler {
     )
 
     // Always accept any credential no matter what
-    if (
-      autoAccept === AutoAcceptCredentialAndProof.always ||
-      autoAccept == AutoAcceptCredentialAndProof.attributesNotChanged
-    ) {
-      const { message } = await this.credentialService.createAck(credentialRecord)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return createOutboundMessage(messageContext.connection!, message)
+    if (autoAccept === AutoAcceptCredentialAndProof.always) {
+      return await this.nextStep(credentialRecord, messageContext)
+    } else if (autoAccept === AutoAcceptCredentialAndProof.attributesNotChanged) {
+      if (credentialRecord.credentialAttributes && credentialRecord.credentialMessage) {
+        try {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          const credentialMessageValues = credentialRecord.credentialMessage.indyCredential!.values
+          const credentialRecordValues = CredentialUtils.convertAttributesToValues(
+            credentialRecord.credentialAttributes
+          )
+          CredentialUtils.assertValuesMatch(credentialMessageValues, credentialRecordValues)
+          return await this.nextStep(credentialRecord, messageContext)
+          // eslint-disable-next-line no-empty
+        } catch {}
+      }
     }
+  }
+
+  private async nextStep(
+    credentialRecord: CredentialRecord,
+    messageContext: HandlerInboundMessage<IssueCredentialHandler>
+  ) {
+    const { message } = await this.credentialService.createAck(credentialRecord)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return createOutboundMessage(messageContext.connection!, message)
   }
 }
