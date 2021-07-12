679595a89a9e53ff7ff1994a24155637a923ca82
Timo Glastra <timo@animo.id>
2021-07-12 21:22:53 +0200

    style: lint fixes
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/agent/__tests__/MessageSender.test.ts
+++ src/agent/__tests__/MessageSender.test.ts
@@ -15,8 +15,6 @@ import { TransportService } from '../TransportService'
 import { createOutboundMessage } from '../helpers'
 
 import { DummyTransportSession } from './stubs'
-import { MessageRepository } from '../../storage/MessageRepository'
-import { InMemoryMessageRepository } from '../../storage/InMemoryMessageRepository'
 
 jest.mock('../TransportService')
 jest.mock('../EnvelopeService')
