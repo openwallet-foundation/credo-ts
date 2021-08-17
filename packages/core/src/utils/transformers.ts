107a4702affc9924c0331efbb6f9404e5f424996
seajensen <seajensen@gmail.com>
2021-08-17 10:35:25 -0600

    feat: added function for parsing date from multiple formats
    Signed-off-by: seajensen <seajensen@gmail.com>

--- packages/core/src/utils/transformers.ts
+++ packages/core/src/utils/transformers.ts
@@ -1,5 +1,5 @@
 import { Transform, TransformationType } from 'class-transformer'
-
+import { DateTime } from 'luxon'
 import { JsonTransformer } from './JsonTransformer'
 
 /**
@@ -37,4 +37,20 @@ export function RecordTransformer<T>(Class: { new (...args: any[]): T }) {
         return value
     }
   })
+}
+/*
+ * Function that parses date from multiple formats
+ * including SQL formats.
+ */
+
+export function DateParser(value: string): Date {
+  const parsedDate = new Date(value)
+  if (parsedDate instanceof Date && !isNaN(parsedDate.getTime())) {
+    return parsedDate
+  }
+  const luxonDate = DateTime.fromSQL(value)
+  if (luxonDate.isValid) {
+    return new Date(luxonDate.toString())
+  }
+  return new Date()
 }
