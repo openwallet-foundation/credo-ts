b05dc784ddd6ea7f62b9dd71b0675adb69f88c67
seajensen <seajensen@gmail.com>
2021-08-19 10:05:06 -0600

    fix: removed unused import in basic messages
    Signed-off-by: seajensen <seajensen@gmail.com>

--- packages/core/src/modules/basic-messages/messages/BasicMessage.ts
+++ packages/core/src/modules/basic-messages/messages/BasicMessage.ts
@@ -1,4 +1,4 @@
-import { Expose, Transform, Type } from 'class-transformer'
+import { Expose, Transform } from 'class-transformer'
 import { Equals, IsDate, IsString } from 'class-validator'
 
 import { AgentMessage } from '../../../agent/AgentMessage'
