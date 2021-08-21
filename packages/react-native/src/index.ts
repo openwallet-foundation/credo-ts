4ff41f0cb2556abb70d733deec8a27a3b11a4cc7
James Ebert <jamesebert.k@gmail.com>
2021-08-20 18:44:30 -0600

    chore: altered buffer import location
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -3,6 +3,8 @@ import '@azure/core-asynciterator-polyfill'
 
 import type { AgentDependencies } from '@aries-framework/core'
 
+import { Buffer } from 'buffer'
+global.Buffer = global.Buffer || Buffer
 import { EventEmitter } from 'events'
 // Eslint complains indy-sdk-react-native has no default export
 // But that's not true
@@ -13,8 +15,6 @@ import { ReactNativeFileSystem } from './ReactNativeFileSystem'
 
 const fetch = global.fetch as unknown as AgentDependencies['fetch']
 const WebSocket = global.WebSocket as unknown as AgentDependencies['WebSocketClass']
-import { Buffer } from 'buffer'
-global.Buffer = global.Buffer || Buffer
 
 const agentDependencies: AgentDependencies = {
   FileSystem: ReactNativeFileSystem,
