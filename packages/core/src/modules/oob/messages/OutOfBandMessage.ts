eb5ccccb472b80b59b45930a35f61ebee1898aaa
Jakub Koci <jakub.koci@gmail.com>
2021-11-22 10:21:20 +0100

    Add oob invitation encoding and decoding
    Signed-off-by: Jakub Koci <jakub.koci@gmail.com>

--- packages/core/src/modules/oob/messages/OutOfBandMessage.ts
+++ packages/core/src/modules/oob/messages/OutOfBandMessage.ts
@@ -2,11 +2,15 @@ import type { UnpackedMessage } from '../../../types'
 import type { DidCommService } from '../../connections/models/did/service/DidCommService'
 
 import { Expose, Type } from 'class-transformer'
-import { Equals, IsArray, IsInstance, ValidateNested } from 'class-validator'
+import { ArrayNotEmpty, Equals, IsArray, IsInstance, IsOptional, ValidateNested } from 'class-validator'
+import { parseUrl } from 'query-string'
 
 import { AgentMessage } from '../../../agent/AgentMessage'
 import { Attachment, AttachmentData } from '../../../decorators/attachment/Attachment'
+import { AriesFrameworkError } from '../../../error'
 import { JsonEncoder } from '../../../utils/JsonEncoder'
+import { JsonTransformer } from '../../../utils/JsonTransformer'
+import { MessageValidator } from '../../../utils/MessageValidator'
 
 interface OutOfBandMessageOptions {
   id?: string
@@ -48,10 +52,43 @@ export class OutOfBandMessage extends AgentMessage {
     return this.requests?.map((request) => {
       if (request.data.base64) {
         return JsonEncoder.fromBase64(request.data.base64)
-      } else {
-        throw new Error('There is no base64 encoded data in attachment')
       }
+
+      if (request.data.json) {
+        return request.data.json
+      }
+
+      throw new AriesFrameworkError('There is no supported data attribute in attachment')
     })
+  }
+
+  public toUrl({ domain }: { domain: string }) {
+    const invitationJson = this.toJSON()
+    const encodedInvitation = JsonEncoder.toBase64URL(invitationJson)
+    const invitationUrl = `${domain}?oob=${encodedInvitation}`
+    return invitationUrl
+  }
+
+  public static async fromUrl(invitationUrl: string) {
+    const parsedUrl = parseUrl(invitationUrl).query
+    const encodedInvitation = parsedUrl['oob']
+
+    if (typeof encodedInvitation === 'string') {
+      const invitationJson = JsonEncoder.fromBase64(encodedInvitation)
+      const invitation = this.fromJson(invitationJson)
+
+      return invitation
+    } else {
+      throw new AriesFrameworkError(
+        'InvitationUrl is invalid. It needs to contain one, and only one, of the following parameters; `oob`'
+      )
+    }
+  }
+
+  public static async fromJson(json: Record<string, unknown>) {
+    const invitation = JsonTransformer.fromJSON(json, OutOfBandMessage)
+    await MessageValidator.validate(invitation)
+    return invitation
   }
 
   @Equals(OutOfBandMessage.type)
@@ -65,10 +102,8 @@ export class OutOfBandMessage extends AgentMessage {
 
   public readonly goal?: string
 
-  // TODO what type is it, is there any enum or should we create a new one
-  public readonly accept?: string[] = []
+  public readonly accept?: string[]
 
-  // TODO what type is it, should we create an enum
   @Expose({ name: 'handshake_protocols' })
   public handshakeProtocols?: string[]
 
@@ -79,7 +114,10 @@ export class OutOfBandMessage extends AgentMessage {
     each: true,
   })
   @IsInstance(Attachment, { each: true })
+  @IsOptional()
   private requests?: Attachment[]
 
-  public readonly services: DidCommService[] = []
+  @IsArray()
+  @ArrayNotEmpty()
+  public services: DidCommService[] = []
 }
