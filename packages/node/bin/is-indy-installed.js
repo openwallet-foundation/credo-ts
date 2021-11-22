cda9bbd2d26bc082ac937bbdedb20e2ab4cc76f0
Berend Sliedrecht <berend@animo.id>
2021-11-22 14:53:54 +0100

    fix(node): added node v12 support for libindy check
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- packages/node/bin/is-indy-installed.js
+++ packages/node/bin/is-indy-installed.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 /* eslint-disable no-console, @typescript-eslint/no-var-requires, no-undef */
 
-const indy = require('indy-sdk')
-const { randomUUID } = require('node:crypto')
+const { randomUUID } = require('crypto')
+const { createWallet, deleteWallet } = require('indy-sdk')
 
 const uuid = randomUUID()
 const id = `test-wallet-id-${uuid}`
 
-indy
-  .createWallet({ id }, { key: id })
-  .then(() => indy.deleteWallet({ id }, { key: id }))
+createWallet({ id }, { key: id })
+  .then(() => deleteWallet({ id }, { key: id }))
   .then(() => {
     console.log('Libindy was installed correctly')
   })
