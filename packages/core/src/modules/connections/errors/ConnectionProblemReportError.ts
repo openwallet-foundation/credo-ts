ca3c07f1ce6620974962206f570ba072c14cbe26
Amit Padmani <amit@northernblock.io>
2021-12-10 19:14:57 +0530

    refactor: refactored ConnectionProblemReportError class
    Signed-off-by: Amit Padmani <amit@northernblock.io>

--- packages/core/src/modules/connections/errors/ConnectionProblemReportError.ts
+++ packages/core/src/modules/connections/errors/ConnectionProblemReportError.ts
@@ -1,17 +1,17 @@
 import type { ConnectionProblemReportReason } from '.'
 import type { ProblemReportErrorOptions } from '../../problem-reports'
 
-import { AriesFrameworkError } from '../../../error/AriesFrameworkError'
+import { ProblemReportError } from '../../problem-reports'
 import { ConnectionProblemReportMessage } from '../messages'
 
 interface ConnectionProblemReportErrorOptions extends ProblemReportErrorOptions {
   problemCode: ConnectionProblemReportReason
 }
-export class ConnectionProblemReportError extends AriesFrameworkError {
+export class ConnectionProblemReportError extends ProblemReportError {
   public problemReport: ConnectionProblemReportMessage
 
   public constructor(public message: string, { problemCode }: ConnectionProblemReportErrorOptions) {
-    super(message)
+    super(message, { problemCode })
     this.problemReport = new ConnectionProblemReportMessage({
       description: {
         en: message,
