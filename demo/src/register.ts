64f7d22be04b54d43d1e8bde5a954f9cc4d5c26a
annelein <anneleinvanreijen@gmail.com>
2022-01-03 15:17:25 +0100

    feat(proof_flow): add proof proposal flow
    Signed-off-by: annelein <anneleinvanreijen@gmail.com>

--- demo/src/register.ts
+++ demo/src/register.ts
@@ -2,7 +2,7 @@ import { Agent } from '@aries-framework/core'
 import { uuid } from '@aries-framework/core/build/utils/uuid'  
 import { CredDef } from 'indy-sdk-react-native'
 
-export const register_schema = async (klm: Agent) : Promise<CredDef> => {
+export const register_credential_schema = async (klm: Agent) : Promise<CredDef> => {
     const schema = await klm.ledger.registerSchema({
         name: 'koninklijke luchtvaart maatschappij' + uuid(),
         version: '1.0.6',
