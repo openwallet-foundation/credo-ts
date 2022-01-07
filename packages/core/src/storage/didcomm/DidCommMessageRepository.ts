7ecb3762adf9141f7bd39b7c147cd2c4ca3c53a9
Timo Glastra <timo@animo.id>
2022-01-07 16:03:51 +0100

    feat: add didcomm repository convenience  methods
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/storage/didcomm/DidCommMessageRepository.ts
+++ packages/core/src/storage/didcomm/DidCommMessageRepository.ts
@@ -1,3 +1,7 @@
+import type { AgentMessage } from '../../agent/AgentMessage'
+import type { JsonMap } from '../../types'
+import type { DidCommMessageRole } from './DidCommMessageRole'
+
 import { inject, scoped, Lifecycle } from 'tsyringe'
 
 import { InjectionSymbols } from '../../constants'
@@ -10,5 +14,38 @@ import { DidCommMessageRecord } from './DidCommMessageRecord'
 export class DidCommMessageRepository extends Repository<DidCommMessageRecord> {
   public constructor(@inject(InjectionSymbols.StorageService) storageService: StorageService<DidCommMessageRecord>) {
     super(DidCommMessageRecord, storageService)
+  }
+
+  public async saveAgentMessage({
+    role,
+    agentMessage,
+    connectionId,
+  }: {
+    role: DidCommMessageRole
+    agentMessage: AgentMessage
+    connectionId?: string
+  }) {
+    const didCommMessageRecord = new DidCommMessageRecord({
+      message: agentMessage.toJSON() as JsonMap,
+      role,
+      connectionId,
+    })
+
+    await this.save(didCommMessageRecord)
+  }
+
+  public async getAgentMessage<MessageClass extends typeof AgentMessage = typeof AgentMessage>({
+    threadId,
+    messageClass,
+  }: {
+    threadId: string
+    messageClass: MessageClass
+  }): Promise<InstanceType<MessageClass>> {
+    const record = await this.getSingleByQuery({
+      threadId,
+      messageType: messageClass.type,
+    })
+
+    return record.getMessageInstance(messageClass)
   }
 }
