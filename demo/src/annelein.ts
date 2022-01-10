9e7fff68d6186f54eb48d0e534e44efe134aa2a9
annelein <anneleinvanreijen@gmail.com>
2022-01-10 08:33:34 +0100

    correcting some functions
    Signed-off-by: annelein <anneleinvanreijen@gmail.com>

--- demo/src/annelein.ts
+++ demo/src/annelein.ts
@@ -1,4 +1,4 @@
-import { CredentialEventTypes, CredentialState, CredentialStateChangedEvent, ProofEventTypes, ProofState, ProofStateChangedEvent } from '@aries-framework/core'
+import { CredentialEventTypes, CredentialState, CredentialStateChangedEvent, PresentationPreview, PresentationPreviewAttribute, ProofEventTypes, ProofState, ProofStateChangedEvent } from '@aries-framework/core'
 import inquirer from 'inquirer'
 import { restart } from './restart';
 import { ConnectionRecord } from '@aries-framework/core';
@@ -19,14 +19,16 @@ enum options {
 
 class Annelein extends BaseAgent {
   connectionRecordId?: string
+  credDef: string
   
   constructor(port: number, name: string) {
     super(port, name)
     super.initializeAgent() // this is not awaited..
     this.credentialOfferListener()
+    this.credDef = '7KuDTpQh3GJ7Gp6kErpWvM:3:CL:115269:latest'
   }
 
-  async credentialOfferListener() {
+  private async credentialOfferListener() {
     this.agent.events.on(
       CredentialEventTypes.CredentialStateChanged,
       async ({ payload }: CredentialStateChangedEvent) => {
@@ -39,12 +41,17 @@ class Annelein extends BaseAgent {
     )
   }
 
-  async printConnectionInvite() {
-    const invite = await this.agent.connections.createConnection()
-    this.connectionRecordId = invite.connectionRecord.id
-
-    console.log('\nYour invitation link:\n', invite.invitation.toUrl({domain: `http://localhost:${this.port}`}), '\n')
-    return invite.connectionRecord
+  private async proposalSentListener () {
+    this.agent.events.on(
+      ProofEventTypes.ProofStateChanged,
+      async ({ payload }: ProofStateChangedEvent) => {
+        if (payload.proofRecord.state !== ProofState.ProposalSent) {
+          return 
+        }
+        console.log("\x1b[32m\nProposal sent!\n\x1b[0m");
+        return
+      }
+    )
   }
 
   private async getConnectionRecord() {
@@ -54,7 +61,15 @@ class Annelein extends BaseAgent {
     return await this.agent.connections.getById(this.connectionRecordId)
   }
 
-  async waitForConnection() {
+  private async printConnectionInvite() {
+    const invite = await this.agent.connections.createConnection()
+    this.connectionRecordId = invite.connectionRecord.id
+
+    console.log('\nYour invitation link:\n', invite.invitation.toUrl({domain: `http://localhost:${this.port}`}), '\n')
+    return invite.connectionRecord
+  }
+
+  private async waitForConnection() {
     // dit was eerst met if statement net zoals hier boven
     const connectionRecord = await this.getConnectionRecord()
 
@@ -68,17 +83,33 @@ class Annelein extends BaseAgent {
     await this.waitForConnection()
   }
 
+  private async newPresentationPreview() {
+    const presentationPreview = new PresentationPreview({
+      attributes: [
+        new PresentationPreviewAttribute({
+          name: 'name',
+          credentialDefinitionId: this.credDef,
+          value: 'annelein',
+        }),
+        new PresentationPreviewAttribute({
+          name: 'date of birth',
+          credentialDefinitionId: this.credDef,
+          value: '09/09/1999',
+        }),
+        new PresentationPreviewAttribute({
+          name: 'country of residence',
+          credentialDefinitionId: this.credDef,
+          value: 'the Netherlands',
+        })
+      ],
+    })
+    return presentationPreview
+  }
+
   async sendProofProposal () {
-    this.agent.events.on(
-      ProofEventTypes.ProofStateChanged,
-      async ({ payload }: ProofStateChangedEvent) => {
-        if (payload.proofRecord.state !== ProofState.ProposalSent) {
-          return 
-        }
-        console.log("\x1b[32m\nProposal sent!\n\x1b[0m");
-        return
-      }
-    )
+    const connectionRecord = await this.getConnectionRecord()
+    const presentationPreview = await this.newPresentationPreview()
+    await this.agent.proofs.proposeProof(connectionRecord.id, presentationPreview)
   }
 
   async sendMessage (message: string) {
