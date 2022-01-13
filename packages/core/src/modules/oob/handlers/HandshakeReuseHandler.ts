bf0bf05cbac92fb8b261ef5ef954226557032aea
Jakub Koci <jakub.koci@gmail.com>
2022-01-13 10:46:21 +0100

    Log error insted of commented code
    Signed-off-by: Jakub Koci <jakub.koci@gmail.com>

--- packages/core/src/modules/oob/handlers/HandshakeReuseHandler.ts
+++ packages/core/src/modules/oob/handlers/HandshakeReuseHandler.ts
@@ -1,12 +1,17 @@
-import type { Handler, HandlerInboundMessage } from '../../../agent/Handler'
+import type { Handler } from '../../../agent/Handler'
+import type { Logger } from '../../../logger'
 
 import { HandshakeReuseMessage } from '../messages/HandshakeReuseMessage'
 
 export class HandshakeReuseHandler implements Handler {
   public supportedMessages = [HandshakeReuseMessage]
+  private logger: Logger
 
-  public async handle(inboundMessage: HandlerInboundMessage<HandshakeReuseHandler>) {
-    // const connection = inboundMessage.assertReadyConnection()
-    // return createOutboundMessage(connection, new AgentMessage())
+  public constructor(logger: Logger) {
+    this.logger = logger
+  }
+
+  public async handle() {
+    this.logger.error(`Out of band ${HandshakeReuseMessage.type} message not implemented yet.`)
   }
 }
