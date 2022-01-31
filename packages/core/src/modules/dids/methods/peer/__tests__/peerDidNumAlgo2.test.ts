28b1715e388f5ed15cb937712b663627c3619465
Timo Glastra <timo@animo.id>
2022-01-31 11:43:55 +0100

    fix: incorrect encoding of services for did:peer (#610)
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/modules/dids/methods/peer/__tests__/peerDidNumAlgo2.test.ts
+++ packages/core/src/modules/dids/methods/peer/__tests__/peerDidNumAlgo2.test.ts
@@ -2,20 +2,23 @@ import { JsonTransformer } from '../../../../../utils'
 import { DidDocument } from '../../../domain'
 import { didToNumAlgo2DidDocument, didDocumentToNumAlgo2Did } from '../peerDidNumAlgo2'
 
-import didPeer2ez6L from './__fixtures__/didPeer2ez6L.json'
+import didPeer2Ez6L from './__fixtures__/didPeer2Ez6L.json'
+import didPeer2Ez6LMoreServices from './__fixtures__/didPeer2Ez6LMoreServices.json'
 
 describe('peerDidNumAlgo2', () => {
   describe('didDocumentToNumAlgo2Did', () => {
     test('transforms method 2 peer did to a did document', async () => {
-      expect(didToNumAlgo2DidDocument(didPeer2ez6L.id).toJSON()).toMatchObject(didPeer2ez6L)
+      expect(didToNumAlgo2DidDocument(didPeer2Ez6L.id).toJSON()).toMatchObject(didPeer2Ez6L)
+
+      expect(didToNumAlgo2DidDocument(didPeer2Ez6LMoreServices.id).toJSON()).toMatchObject(didPeer2Ez6LMoreServices)
     })
   })
 
   describe('didDocumentToNumAlgo2Did', () => {
     test('transforms method 2 peer did document to a did', async () => {
-      const expectedDid = didPeer2ez6L.id
+      const expectedDid = didPeer2Ez6L.id
 
-      const didDocument = JsonTransformer.fromJSON(didPeer2ez6L, DidDocument)
+      const didDocument = JsonTransformer.fromJSON(didPeer2Ez6L, DidDocument)
 
       expect(didDocumentToNumAlgo2Did(didDocument)).toBe(expectedDid)
     })
