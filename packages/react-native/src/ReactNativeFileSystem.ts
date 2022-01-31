542c0c3f3c9a477691635beccbc9bc345f5664bf
Timo Glastra <timo@animo.id>
2022-01-31 10:44:27 +0100

    refactor: use rnfs for react native
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/react-native/src/ReactNativeFileSystem.ts
+++ packages/react-native/src/ReactNativeFileSystem.ts
@@ -1,7 +1,7 @@
 import type { FileSystem } from '@aries-framework/core'
 
 import { getDirFromFilePath } from '@aries-framework/core'
-import * as ExpoFileSystem from 'expo-file-system'
+import * as RNFS from 'react-native-fs'
 
 export class ReactNativeFileSystem implements FileSystem {
   public readonly basePath
@@ -9,81 +9,38 @@ export class ReactNativeFileSystem implements FileSystem {
   /**
    * Create new ReactNativeFileSystem class instance.
    *
-   * @param basePath The base path to use for reading and writing files. ExpoFileSystem.documentDirectory if not specified
+   * @param basePath The base path to use for reading and writing files. RNFS.TemporaryDirectoryPath if not specified
    *
-   * @see https://docs.expo.dev/versions/latest/sdk/filesystem/#filesystemcachedirectory
+   * @see https://github.com/itinance/react-native-fs#constants
    */
   public constructor(basePath?: string) {
-    if (!basePath) {
-      // Returns null on web. Safe to cast for iOS / Android
-      // https://github.com/expo/expo/issues/5558
-      let expoPath = ExpoFileSystem.documentDirectory as string
-
-      // Remove trailing /
-      if (expoPath.endsWith('/')) {
-        expoPath = expoPath.substring(0, expoPath.length - 1)
-      }
-
-      expoPath = expoPath.replace('file://', '')
-
-      this.basePath = expoPath
-    } else {
-      this.basePath = basePath
-    }
+    this.basePath = basePath ?? RNFS.TemporaryDirectoryPath
   }
 
   public async exists(path: string): Promise<boolean> {
-    // Expo requires the path to begin with `file://`
-    const expoPath = this.getExpoPath(path)
-
-    const fileInfo = await ExpoFileSystem.getInfoAsync(expoPath)
-
-    return fileInfo.exists
+    return RNFS.exists(path)
   }
 
   public async write(path: string, data: string): Promise<void> {
-    const fileDirectory = getDirFromFilePath(path)
-
     // Make sure parent directories exist
-    await this.ensureDirectoryExists(fileDirectory)
+    await RNFS.mkdir(getDirFromFilePath(path))
 
-    // Expo requires the path to begin with `file://`
-    const expoPath = this.getExpoPath(path)
-
-    return ExpoFileSystem.writeAsStringAsync(expoPath, data, { encoding: 'utf8' })
+    return RNFS.writeFile(path, data, 'utf8')
   }
 
   public async read(path: string): Promise<string> {
-    // Expo requires the path to begin with `file://`
-    const expoPath = this.getExpoPath(path)
-
-    return ExpoFileSystem.readAsStringAsync(expoPath, { encoding: 'utf8' })
+    return RNFS.readFile(path, 'utf8')
   }
 
   public async downloadToFile(url: string, path: string) {
-    // Expo requires the path to begin with `file://`
-    const expoPath = this.getExpoPath(path)
-
-    const fileDirectory = getDirFromFilePath(expoPath)
-
     // Make sure parent directories exist
-    await this.ensureDirectoryExists(fileDirectory)
+    await RNFS.mkdir(getDirFromFilePath(path))
 
-    await ExpoFileSystem.downloadAsync(url, expoPath)
-  }
+    const { promise } = RNFS.downloadFile({
+      fromUrl: url,
+      toFile: path,
+    })
 
-  private getExpoPath(path: string) {
-    if (!path.startsWith('file://')) {
-      return `file://${path}`
-    }
-
-    return path
-  }
-
-  private async ensureDirectoryExists(path: string) {
-    const dirInfo = await ExpoFileSystem.getInfoAsync(path)
-    if (!dirInfo.exists) {
-      await ExpoFileSystem.makeDirectoryAsync(path, { intermediates: true })
-    }
+    await promise
   }
 }
