72eff05fe5e26a2eb4c369995dee96811ab0b488
James Ebert <jamesebert.k@gmail.com>
2022-01-31 21:45:47 -0700

    feat: verify proofs containing proof of non_revocation
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- packages/core/src/modules/indy/services/IndyVerifierService.ts
+++ packages/core/src/modules/indy/services/IndyVerifierService.ts
@@ -6,34 +6,64 @@ import { AgentConfig } from '../../../agent/AgentConfig'
 import { IndySdkError } from '../../../error'
 import { isIndyError } from '../../../utils/indyError'
 
+import { IndyLedgerService } from '../../ledger/services/IndyLedgerService'
+
 @scoped(Lifecycle.ContainerScoped)
 export class IndyVerifierService {
   private indy: typeof Indy
+  private ledgerService: IndyLedgerService
 
-  public constructor(agentConfig: AgentConfig) {
+  public constructor(agentConfig: AgentConfig, ledgerService: IndyLedgerService) {
     this.indy = agentConfig.agentDependencies.indy
+    this.ledgerService = ledgerService
   }
 
   public async verifyProof({
     proofRequest,
     proof,
     schemas,
-    credentialDefinitions,
-    revocationRegistryDefinitions = {},
-    revocationStates = {},
+    credentialDefinitions
   }: VerifyProofOptions): Promise<boolean> {
     try {
+
+      const { revocationRegistryDefinitions, revocationRegistryStates } = await this.getRevocationRegistries(proof)
+      
       return await this.indy.verifierVerifyProof(
         proofRequest,
         proof,
         schemas,
         credentialDefinitions,
         revocationRegistryDefinitions,
-        revocationStates
+        revocationRegistryStates
       )
     } catch (error) {
       throw isIndyError(error) ? new IndySdkError(error) : error
     }
+  }
+
+  private async getRevocationRegistries(proof: Indy.IndyProof) {
+    let revocationRegistryDefinitions:Indy.RevocRegDefs = {}
+    let revocationRegistryStates:Indy.RevStates = {}
+    for(const identifier of proof.identifiers){
+      const revRegId = identifier.rev_reg_id
+      const timestamp = identifier.timestamp
+
+      //Fetch Revocation Registry Definition if not already fetched
+      if(revRegId && !revocationRegistryDefinitions[revRegId]){
+        const { revocRegDef } = await this.ledgerService.getRevocationRegistryDefinition(revRegId)
+        revocationRegistryDefinitions[revRegId] = revocRegDef
+      }
+
+      //Fetch Revocation Registry by Timestamp if not already fetched
+      if(revRegId && timestamp && !revocationRegistryStates[revRegId]?.[timestamp]){
+        if(!revocationRegistryStates[revRegId]){
+          revocationRegistryStates[revRegId] = {}
+        }
+        const { revocReg } = await this.ledgerService.getRevocationRegistry(revRegId, timestamp)
+        revocationRegistryStates[revRegId][timestamp] = revocReg
+      }
+    }
+    return { revocationRegistryDefinitions, revocationRegistryStates }
   }
 }
 
@@ -42,6 +72,4 @@ export interface VerifyProofOptions {
   proof: Indy.IndyProof
   schemas: Indy.Schemas
   credentialDefinitions: Indy.CredentialDefs
-  revocationRegistryDefinitions?: Indy.RevRegsDefs
-  revocationStates?: Indy.RevStates
 }
