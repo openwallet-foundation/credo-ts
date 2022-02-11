42f6af6013bb6342db2bb35994b34b097858455d
Patrick Kenyon <treek.kenyon@gmail.com>
2022-02-11 16:53:58 -0700

    Test satisfaction
    Signed-off-by: Patrick Kenyon <treek.kenyon@gmail.com>

--- packages/core/tests/credentials.test.ts
+++ packages/core/tests/credentials.test.ts
@@ -530,10 +530,17 @@ describe('credentials', () => {
   })
 
   test('Test revocation notification for Faber credential id', async () => {
+    testLogger.test('faberCredentialRecord.revocationNotification starts as undefined')
     expect(faberCredentialRecord.revocationNotification).toBeUndefined()
 
+    testLogger.test("Set faberCredentialRecord's revocationRegistryId & credentialRevocationId")
+    const revocationRegistryId =
+      'RmbvMQPRMnerJ5Lpv9dRny:4:RmbvMQPRMnerJ5Lpv9dRny:3:CL:7473:rev:CL_ACCUM:332aa053-d8d8-4a24-a788-3fc81a007cf8'
+    const credentialRevocationId = '22'
+
+    faberCredentialRecord.setTags({ revocationRegistryId, credentialRevocationId })
+
     testLogger.test('Creating revocation notification message')
-    const { revocationRegistryId, credentialRevocationId } = faberCredentialRecord
     const revocationNotificationThreadId = `indy::${revocationRegistryId}::${credentialRevocationId}`
 
     const revNotifMessage = new RevocationNotificationMessage({
