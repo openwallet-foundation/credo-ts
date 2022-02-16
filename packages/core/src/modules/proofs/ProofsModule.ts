d0ea2393560c6ccf11c144e23c71fe7591aa2571
Timo Glastra <timo@animo.id>
2022-02-16 11:43:25 +0100

    refactor: default to filter revoked credentials
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/modules/proofs/ProofsModule.ts
+++ packages/core/src/modules/proofs/ProofsModule.ts
@@ -356,7 +356,7 @@ export class ProofsModule {
 
     return this.proofService.getRequestedCredentialsForProofRequest(indyProofRequest, {
       presentationProposal: presentationPreview,
-      filterByNonRevocationRequirements: config?.filterByNonRevocationRequirements,
+      filterByNonRevocationRequirements: config?.filterByNonRevocationRequirements ?? true,
     })
   }
 
@@ -477,12 +477,17 @@ export interface GetRequestedCredentialsConfig {
    * Whether to filter the retrieved credentials using the presentation preview.
    * This configuration will only have effect if a presentation proposal message is available
    * containing a presentation preview.
+   *
+   * @default false
    */
   filterByPresentationPreview?: boolean
 
   /**
    * Whether to filter the retrieved credentials using the non-revocation request in the proof request.
    * This configuration will only have effect if the proof request requires proof on non-revocation of any kind.
+   * Default to true
+   *
+   * @default true
    */
   filterByNonRevocationRequirements?: boolean
 }
