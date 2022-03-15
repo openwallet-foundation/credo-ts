ffb57910891e41b7c674b4260231b95224f2e1fb
Berend Sliedrecht <berend@animo.id>
2022-03-15 14:52:10 +0100

    fix: renamed secretKey -> privateKey
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- packages/core/src/storage/keyPair/KeyPairRecord.ts
+++ packages/core/src/storage/keyPair/KeyPairRecord.ts
@@ -1,13 +1,14 @@
 import type { KeyType } from '../../crypto/KeyType'
 import type { TagsBase } from '../BaseRecord'
 
+import { BufferEncoder } from '../../utils'
 import { uuid } from '../../utils/uuid'
 import { BaseRecord } from '../BaseRecord'
 
-export interface KeyPairRecordProps {
+interface KeyPairRecordProps {
   id?: string
-  publicKey: string
-  secretKey: string
+  publicKey: Uint8Array
+  privateKey: Uint8Array
   keyType: Exclude<KeyType, KeyType.Ed25519 | KeyType.X25519>
 }
 
@@ -20,10 +21,22 @@ export interface KeyPairRecordProps {
  * - Bls12381g1g2
  *
  * Ed25519 and X25519 are handled via the indy-sdk
+ *
  */
 export class KeyPairRecord extends BaseRecord {
-  public publicKey!: string
-  public secretKey!: string
+  /**
+   * Base58 encoded public key
+   */
+  public publicKeyBase58!: string
+
+  /**
+   * Base58 encoded secretKey
+   */
+  public privateKeyBase58!: string
+
+  /**
+   * Key type of the keypair
+   */
   public keyType!: KeyType
 
   public constructor(props: KeyPairRecordProps) {
@@ -31,8 +44,8 @@ export class KeyPairRecord extends BaseRecord {
 
     if (props) {
       this.id = props.id ?? uuid()
-      this.publicKey = props.publicKey
-      this.secretKey = props.secretKey
+      this.publicKeyBase58 = BufferEncoder.toBase58(props.publicKey)
+      this.privateKeyBase58 = BufferEncoder.toBase58(props.privateKey)
       this.keyType = props.keyType
     }
   }
