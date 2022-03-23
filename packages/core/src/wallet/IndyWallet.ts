43cc18ab54ff75718891c1e3372b5b6f35e9caba
Berend Sliedrecht <berend@animo.id>
2022-03-23 10:49:08 +0100

    fix: using typedarrayencoder now
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- packages/core/src/wallet/IndyWallet.ts
+++ packages/core/src/wallet/IndyWallet.ts
@@ -12,8 +12,7 @@ import { KeyType } from '../crypto'
 import { BbsService } from '../crypto/BbsService'
 import { Key } from '../crypto/Key'
 import { AriesFrameworkError, IndySdkError, RecordDuplicateError, RecordNotFoundError } from '../error'
-import { BufferEncoder } from '../utils'
-import { JsonEncoder } from '../utils/JsonEncoder'
+import { TypedArrayEncoder, JsonEncoder } from '../utils'
 import { isIndyError } from '../utils/indyError'
 
 import { WalletDuplicateError, WalletNotFoundError, WalletError } from './error'
@@ -386,7 +385,7 @@ export class IndyWallet implements Wallet {
         return BbsService.sign({
           messages: data,
           publicKey: key.publicKey,
-          privateKey: BufferEncoder.fromBase58(blsKeyPair.publicKeyBase58),
+          privateKey: TypedArrayEncoder.fromBase58(blsKeyPair.publicKeyBase58),
         })
       }
     } catch (error) {
