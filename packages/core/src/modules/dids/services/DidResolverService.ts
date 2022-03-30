381d6fd0caae4452e8c2400e28de63aa4ebfd3ac
James Ebert <jamesebert.k@gmail.com>
2022-03-30 17:29:53 -0600

    fix: did:sov service type resolving behavior
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- packages/core/src/modules/dids/services/DidResolverService.ts
+++ packages/core/src/modules/dids/services/DidResolverService.ts
@@ -7,7 +7,7 @@ import { Lifecycle, scoped } from 'tsyringe'
 import { AgentConfig } from '../../../agent/AgentConfig'
 import { IndyLedgerService } from '../../ledger'
 import { parseDid } from '../domain/parse'
-import { IndyDidResolver } from '../methods/indy/IndyDidResolver'
+import { SovDidResolver } from '../methods/sov/SovDidResolver'
 import { KeyDidResolver } from '../methods/key/KeyDidResolver'
 import { PeerDidResolver } from '../methods/peer/PeerDidResolver'
 import { WebDidResolver } from '../methods/web/WebDidResolver'
@@ -22,7 +22,7 @@ export class DidResolverService {
     this.logger = agentConfig.logger
 
     this.resolvers = [
-      new IndyDidResolver(indyLedgerService),
+      new SovDidResolver(indyLedgerService),
       new WebDidResolver(),
       new KeyDidResolver(),
       new PeerDidResolver(didRepository),
