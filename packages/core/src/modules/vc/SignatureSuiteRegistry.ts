e20aa349e96e0f97a1a3cb0cbcbaaa30fae04800
Karim <karim@animo.id>
2022-04-02 16:33:41 +0200

    feat: add BbsBlsSiganture2020 to sig-registry
    Signed-off-by: Karim <karim@animo.id>

--- packages/core/src/modules/vc/SignatureSuiteRegistry.ts
+++ packages/core/src/modules/vc/SignatureSuiteRegistry.ts
@@ -1,4 +1,5 @@
 import jsigs from '@digitalcredentials/jsonld-signatures'
+import { BbsBlsSignature2020 } from '@mattrglobal/jsonld-signatures-bbs'
 
 import { KeyType } from '../../crypto'
 import { Ed25519Signature2018 } from '../../crypto/signature-suites'
@@ -16,10 +17,16 @@ export interface SuiteInfo {
 export class SignatureSuiteRegistry {
   private suiteMapping: SuiteInfo[] = [
     {
-      suiteClass: Ed25519Signature2018,
+      suiteClass: BbsBlsSignature2020,
       proofType: 'Ed25519Signature2018',
       requiredKeyType: 'Ed25519VerificationKey2018',
       keyType: KeyType.Ed25519,
+    },
+    {
+      suiteClass: Ed25519Signature2018,
+      proofType: 'BbsBlsSignature2020',
+      requiredKeyType: 'BbsBlsSignatureProof2020',
+      keyType: KeyType.Bls12381g2,
     },
   ]
 
