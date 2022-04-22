9f04375edc5eaffa0aa3583efcf05c83d74987bb
Patrick Kenyon <treek.kenyon@gmail.com>
2022-04-22 01:10:25 -0600

    feat: support revocation notification messages (#579)
    Signed-off-by: Patrick Kenyon <treek.kenyon@gmail.com>
    
    Co-authored-by: James Ebert <jamesebert.k@gmail.com>

--- packages/core/src/modules/credentials/CredentialEvents.ts
+++ packages/core/src/modules/credentials/CredentialEvents.ts
@@ -4,11 +4,19 @@ import type { CredentialRecord } from './repository/CredentialRecord'
 
 export enum CredentialEventTypes {
   CredentialStateChanged = 'CredentialStateChanged',
+  RevocationNotificationReceived = 'RevocationNotificationReceived',
 }
 export interface CredentialStateChangedEvent extends BaseEvent {
   type: typeof CredentialEventTypes.CredentialStateChanged
   payload: {
     credentialRecord: CredentialRecord
     previousState: CredentialState | null
+  }
+}
+
+export interface RevocationNotificationReceivedEvent extends BaseEvent {
+  type: typeof CredentialEventTypes.RevocationNotificationReceived
+  payload: {
+    credentialRecord: CredentialRecord
   }
 }
