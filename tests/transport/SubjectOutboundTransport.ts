01c5bb3b67786fa7efa361d02bfddde7d113eacf
Timo Glastra <timo@animo.id>
2022-05-01 23:17:07 +0200

    fix: allow agent without inbound endpoint to connect when using multi-use invitation (#712)
    Previously the session is only stored by the connection id, but this is not always enough for the session to be reused. When using a multi-use invitation the connection id will change while processing the message, meaning the session cannot be reused. This also helps with cases where no connection is established. Fixes #483
    
    Signed-off-by: Timo Glastra <timo@animo.id>

--- tests/transport/SubjectOutboundTransport.ts
+++ tests/transport/SubjectOutboundTransport.ts
@@ -2,31 +2,42 @@ import type { Agent, Logger } from '../../packages/core/src'
 import type { OutboundTransport } from '../../packages/core/src/transport/OutboundTransport'
 import type { OutboundPackage } from '../../packages/core/src/types'
 import type { SubjectMessage } from './SubjectInboundTransport'
-import type { Subject } from 'rxjs'
+import type { Subscription } from 'rxjs'
+
+import { Subject } from 'rxjs'
 
 import { InjectionSymbols, AriesFrameworkError } from '../../packages/core/src'
 
 export class SubjectOutboundTransport implements OutboundTransport {
   private logger!: Logger
-  private ourSubject: Subject<SubjectMessage>
+  private ourSubject = new Subject<SubjectMessage>()
+  private returnRouteMessageSubscription?: Subscription
   private subjectMap: { [key: string]: Subject<SubjectMessage> | undefined }
 
   public supportedSchemes = ['rxjs']
 
-  public constructor(
-    ourSubject: Subject<SubjectMessage>,
-    subjectMap: { [key: string]: Subject<SubjectMessage> | undefined }
-  ) {
-    this.ourSubject = ourSubject
+  public constructor(subjectMap: { [key: string]: Subject<SubjectMessage> | undefined }) {
     this.subjectMap = subjectMap
   }
 
   public async start(agent: Agent): Promise<void> {
     this.logger = agent.injectionContainer.resolve(InjectionSymbols.Logger)
+    this.subscribe(agent)
   }
 
   public async stop(): Promise<void> {
-    // Nothing required to stop
+    this.returnRouteMessageSubscription?.unsubscribe()
+    this.ourSubject.complete()
+  }
+
+  private subscribe(agent: Agent) {
+    this.returnRouteMessageSubscription = this.ourSubject.subscribe({
+      next: async ({ message }: SubjectMessage) => {
+        this.logger.test('Received message')
+
+        await agent.receiveMessage(message)
+      },
+    })
   }
 
   public async sendMessage(outboundPackage: OutboundPackage) {
