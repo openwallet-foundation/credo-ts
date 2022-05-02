0b2ea5fb0d44ea8efdeff6bd1fd643b23d3abe0f
NB Prasad Katkar <prasad@northernblock.io>
2022-05-02 17:09:46 +0530

    fix: eslint fixes
    Signed-off-by: NB Prasad Katkar <prasad@northernblock.io>

--- packages/core/src/modules/proofs/formats/ProofFormats.ts
+++ packages/core/src/modules/proofs/formats/ProofFormats.ts
@@ -2,7 +2,3 @@ export const INDY_ATTACH_ID = 'indy'
 export const V2_INDY_PRESENTATION_PROPOSAL = 'hlindy/proof-req@v2.0'
 export const V2_INDY_PRESENTATION_REQUEST = 'hlindy/proof-req@v2.0'
 export const V2_INDY_PRESENTATION = 'hlindy/proof@v2.0'
-
-const LD_ATTACH_ID = '...'
-const LD_PROPOSE_FORMAT = '...'
-const LD_REQUEST_FORMAT = '...'
