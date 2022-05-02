b281673b3503bb85ebda7afdd68b6d792d8f5bf5
KolbyRKunz <KolbyKunz@yahoo.com>
2022-05-02 09:34:56 -0600

    feat: pickup v2 protocol (#711)
    Signed-off-by: KolbyRKunz <KolbyKunz@yahoo.com>
    
    BREAKING CHANGE: The mediator pickup strategy enum value `MediatorPickupStrategy.Explicit` has been renamed to `MediatorPickupStrategy.PickUpV1` to better align with the naming of the new `MediatorPickupStrategy.PickUpV2`

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -68,6 +68,7 @@ export interface InitConfig {
   clearDefaultMediator?: boolean
   mediatorPollingInterval?: number
   mediatorPickupStrategy?: MediatorPickupStrategy
+  maximumMessagePickup?: number
 
   useLegacyDidSovPrefix?: boolean
   connectionImageUrl?: string
