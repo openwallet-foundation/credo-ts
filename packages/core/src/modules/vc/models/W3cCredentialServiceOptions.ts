c90729de8394dd53c7cdd956d3524802e08136a7
Karim <karim@animo.id>
2022-05-06 15:06:17 +0200

    fix: replace faulty challenge assignment
    Signed-off-by: Karim <karim@animo.id>

--- packages/core/src/modules/vc/models/W3cCredentialServiceOptions.ts
+++ packages/core/src/modules/vc/models/W3cCredentialServiceOptions.ts
@@ -38,6 +38,7 @@ export interface SignPresentationOptions {
   signatureType: string
   purpose: ProofPurpose
   verificationMethod: string
+  challenge?: string
 }
 
 export interface VerifyPresentationOptions {
