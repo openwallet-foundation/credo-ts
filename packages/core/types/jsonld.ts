8b2dddbf6e5e24cd1f81ed5280bb1e5801897122
Karim Stekelenburg <karim@animo.id>
2022-05-19 23:43:15 +0200

    feat: jsonld-credential support (#718)
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

---
+++ packages/core/types/jsonld.ts
@@ -0,0 +1,29 @@
+//@ts-ignore
+import jsonld from '@digitalcredentials/jsonld'
+//@ts-ignore
+import nodeDocumentLoader from '@digitalcredentials/jsonld/lib/documentLoaders/node'
+//@ts-ignore
+import xhrDocumentLoader from '@digitalcredentials/jsonld/lib/documentLoaders/xhr'
+
+interface JsonLd {
+  compact(document: any, context: any, options?: any): any
+  fromRDF(document: any): any
+  frame(document: any, revealDocument: any, options?: any): any
+  canonize(document: any, options?: any): any
+  expand(document: any, options?: any): any
+  getValues(document: any, key: string): any
+  addValue(document: any, key: string, value: any): void
+}
+
+export interface DocumentLoaderResult {
+  contextUrl?: string | null
+  documentUrl: string
+  document: Record<string, unknown>
+}
+
+export type DocumentLoader = (url: string) => Promise<DocumentLoaderResult>
+
+export const documentLoaderXhr = xhrDocumentLoader as () => DocumentLoader
+export const documentLoaderNode = nodeDocumentLoader as () => DocumentLoader
+
+export default jsonld as unknown as JsonLd
