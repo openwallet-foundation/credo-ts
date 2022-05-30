5fdc25c23a0bd5da6927e5f565483a7e7bb9cf29
Artemkaaas <artem.ivanov@evernym.com>
2022-05-30 11:26:57 +0300

    Fixed merge errors

--- samples/extension-module/dummy/handlers/DummyRequestHandler.ts
+++ samples/extension-module/dummy/handlers/DummyRequestHandler.ts
@@ -1,9 +1,10 @@
 import type { DummyService } from '../services'
 import type { Handler, HandlerInboundMessage } from '@aries-framework/core'
+import type { DIDCommV1Message } from '@aries-framework/core/src/agent/didcomm'
 
 import { DummyRequestMessage } from '../messages'
 
-export class DummyRequestHandler implements Handler {
+export class DummyRequestHandler implements Handler<typeof DIDCommV1Message> {
   public supportedMessages = [DummyRequestMessage]
   private dummyService: DummyService
 
--- samples/extension-module/dummy/handlers/DummyResponseHandler.ts
+++ samples/extension-module/dummy/handlers/DummyResponseHandler.ts
@@ -1,9 +1,10 @@
 import type { DummyService } from '../services'
 import type { Handler, HandlerInboundMessage } from '@aries-framework/core'
+import type { DIDCommV1Message } from '@aries-framework/core/src/agent/didcomm'
 
 import { DummyResponseMessage } from '../messages'
 
-export class DummyResponseHandler implements Handler {
+export class DummyResponseHandler implements Handler<typeof DIDCommV1Message> {
   public supportedMessages = [DummyResponseMessage]
   private dummyService: DummyService
 
