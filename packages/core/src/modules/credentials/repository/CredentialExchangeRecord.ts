6d62c6ab9dbb1d4763794192941fd4ae93a4aa0e
Mike Richardson <mike.richardson@ontario.ca>
2022-05-31 08:42:00 +0300

    Set credentials array to [] by default in CredentialExchangeRecord
    Signed-off-by: Mike Richardson <mike.richardson@ontario.ca>

--- packages/core/src/modules/credentials/repository/CredentialExchangeRecord.ts
+++ packages/core/src/modules/credentials/repository/CredentialExchangeRecord.ts
@@ -61,7 +61,7 @@ export class CredentialExchangeRecord extends BaseRecord<
   public revocationNotification?: RevocationNotification
   public errorMessage?: string
   public protocolVersion!: CredentialProtocolVersion
-  public credentials: CredentialRecordBinding[] = [] // default in case constructor not used (as in tests)
+  public credentials: CredentialRecordBinding[] = [] // default in case constructor not used (as can happen in tests)
 
   @Type(() => CredentialPreviewAttribute)
   public credentialAttributes?: CredentialPreviewAttribute[]
