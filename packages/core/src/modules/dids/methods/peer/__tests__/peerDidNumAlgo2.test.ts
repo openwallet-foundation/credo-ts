75bc04666a7bb12b9a1ecbdba931e768950ed304
DenisRybas <denis.rybas@dsr-corporation.com>
2022-06-03 10:42:25 +0300

    tests and tests vectors updated

--- packages/core/src/modules/dids/methods/peer/__tests__/peerDidNumAlgo2.test.ts
+++ packages/core/src/modules/dids/methods/peer/__tests__/peerDidNumAlgo2.test.ts
@@ -2,23 +2,20 @@ import { JsonTransformer } from '../../../../../utils'
 import { DidDocument } from '../../../domain'
 import { didToNumAlgo2DidDocument, didDocumentToNumAlgo2Did } from '../peerDidNumAlgo2'
 
-import didPeer2Ez6L from './__fixtures__/didPeer2Ez6L.json'
-import didPeer2Ez6LMoreServices from './__fixtures__/didPeer2Ez6LMoreServices.json'
+import didPeer2Ez6LSbysBase58 from './__fixtures__/didPeer2Ez6LSbysBase58.json'
 
 describe('peerDidNumAlgo2', () => {
   describe('didDocumentToNumAlgo2Did', () => {
     test('transforms method 2 peer did to a did document', async () => {
-      expect(didToNumAlgo2DidDocument(didPeer2Ez6L.id).toJSON()).toMatchObject(didPeer2Ez6L)
-
-      expect(didToNumAlgo2DidDocument(didPeer2Ez6LMoreServices.id).toJSON()).toMatchObject(didPeer2Ez6LMoreServices)
+      expect(didToNumAlgo2DidDocument(didPeer2Ez6LSbysBase58.id).toJSON()).toMatchObject(didPeer2Ez6LSbysBase58)
     })
   })
 
   describe('didDocumentToNumAlgo2Did', () => {
     test('transforms method 2 peer did document to a did', async () => {
-      const expectedDid = didPeer2Ez6L.id
+      const expectedDid = didPeer2Ez6LSbysBase58.id
 
-      const didDocument = JsonTransformer.fromJSON(didPeer2Ez6L, DidDocument)
+      const didDocument = JsonTransformer.fromJSON(didPeer2Ez6LSbysBase58, DidDocument)
 
       expect(didDocumentToNumAlgo2Did(didDocument)).toBe(expectedDid)
     })
