d13c3c9662367375fbed370af1a192fe42a27f00
Moriarty <moritz@animo.id>
2022-06-09 12:54:49 +0200

    chore: fix accDecorator tests
    Signed-off-by: Moriarty <moritz@animo.id>

--- packages/core/src/decorators/ack/AckDecorator.test.ts
+++ packages/core/src/decorators/ack/AckDecorator.test.ts
@@ -1,7 +1,8 @@
+import { wordSize } from 'bn.js'
+
 import { BaseMessage } from '../../agent/BaseMessage'
 import { ClassValidationError } from '../../error/ClassValidationError'
 import { JsonTransformer } from '../../utils/JsonTransformer'
-import { MessageValidator } from '../../utils/MessageValidator'
 import { Compose } from '../../utils/mixins'
 
 import { AckDecorated } from './AckDecoratorExtension'
@@ -50,18 +51,17 @@ describe('Decorators | AckDecoratorExtension', () => {
     expect(caughtError.message.includes('An instance of TestMessage has failed the validation')).toBeTruthy()
     expect(caughtError.message.includes('property id has failed the following constraints: matches')).toBeTruthy()
     expect(caughtError.message.includes('- property type has failed the following constraints: matches')).toBeTruthy()
-    expect(
-      caughtError.message.includes('property pleaseAck.on has failed the following constraints: isArray, isEnum')
-    ).toBeTruthy()
     expect(caughtError.validationErrors).toMatchObject([
       {
         children: [],
-        constraints: { matches: 'id must match /[-_./a-zA-Z0-9]{8,64}/ regular expression' },
+        constraints: {
+          matches: 'id must match /[-_./a-zA-Z0-9]{8,64}/ regular expression',
+        },
         property: 'id',
         target: {
-          id: undefined,
-          pleaseAck: {},
-          type: undefined,
+          pleaseAck: {
+            on: ['RECEIPT'],
+          },
         },
         value: undefined,
       },
@@ -72,81 +72,11 @@ describe('Decorators | AckDecoratorExtension', () => {
         },
         property: 'type',
         target: {
-          id: undefined,
-          pleaseAck: {},
-          type: undefined,
+          pleaseAck: {
+            on: ['RECEIPT'],
+          },
         },
         value: undefined,
-      },
-      {
-        children: [
-          {
-            children: [],
-            constraints: { isArray: 'on must be an array', isEnum: 'each value in on must be a valid enum value' },
-            property: 'on',
-            target: {},
-            value: undefined,
-          },
-        ],
-        property: 'pleaseAck',
-        target: {
-          id: undefined,
-          pleaseAck: {},
-          type: undefined,
-        },
-        value: {},
-      },
-    ])
-  })
-
-  test('successfully validates please ack decorator', async () => {
-    const transformedWithDefault = JsonTransformer.fromJSON(
-      {
-        '~please_ack': {},
-        '@id': '7517433f-1150-46f2-8495-723da61b872a',
-        '@type': 'https://didcomm.org/test-protocol/1.0/test-message',
-      },
-      TestMessage
-    )
-
-    await expect(MessageValidator.validate(transformedWithDefault)).resolves.toBeUndefined()
-
-    const transformedWithoutDefault = JsonTransformer.fromJSON(
-      {
-        '~please_ack': {
-          on: ['OUTCOME'],
-        },
-        '@id': '7517433f-1150-46f2-8495-723da61b872a',
-        '@type': 'https://didcomm.org/test-protocol/1.0/test-message',
-      },
-      TestMessage
-    )
-
-    await expect(MessageValidator.validate(transformedWithoutDefault)).resolves.toBeUndefined()
-
-    const transformedWithIncorrectValue = JsonTransformer.fromJSON(
-      {
-        '~please_ack': {
-          on: ['NOT_A_VALID_VALUE'],
-        },
-        '@id': '7517433f-1150-46f2-8495-723da61b872a',
-        '@type': 'https://didcomm.org/test-protocol/1.0/test-message',
-      },
-      TestMessage
-    )
-
-    await expect(MessageValidator.validate(transformedWithIncorrectValue)).rejects.toMatchObject([
-      {
-        children: [
-          {
-            children: [],
-            constraints: { isEnum: 'each value in on must be a valid enum value' },
-            property: 'on',
-            target: { on: ['NOT_A_VALID_VALUE'] },
-            value: ['NOT_A_VALID_VALUE'],
-          },
-        ],
-        property: 'pleaseAck',
       },
     ])
   })
