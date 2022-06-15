8916653ccd775906c286545e822638eb255533ba
Timo Glastra <timo@animo.id>
2022-06-15 10:40:51 +0200

    fix(w3c): add event emitter to repo
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/modules/vc/models/credential/W3cCredentialRepository.ts
+++ packages/core/src/modules/vc/models/credential/W3cCredentialRepository.ts
@@ -1,5 +1,6 @@
 import { inject, scoped, Lifecycle } from 'tsyringe'
 
+import { EventEmitter } from '../../../../agent/EventEmitter'
 import { InjectionSymbols } from '../../../../constants'
 import { Repository } from '../../../../storage/Repository'
 import { StorageService } from '../../../../storage/StorageService'
@@ -8,7 +9,10 @@ import { W3cCredentialRecord } from './W3cCredentialRecord'
 
 @scoped(Lifecycle.ContainerScoped)
 export class W3cCredentialRepository extends Repository<W3cCredentialRecord> {
-  public constructor(@inject(InjectionSymbols.StorageService) storageService: StorageService<W3cCredentialRecord>) {
-    super(W3cCredentialRecord, storageService)
+  public constructor(
+    @inject(InjectionSymbols.StorageService) storageService: StorageService<W3cCredentialRecord>,
+    eventEmitter: EventEmitter
+  ) {
+    super(W3cCredentialRecord, storageService, eventEmitter)
   }
 }
