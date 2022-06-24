2316bb596b81d49759391754d5b2d67197619334
KolbyRKunz <KolbyKunz@yahoo.com>
2022-06-24 13:56:43 -0600

    unit tests for shortenedUrls created and passing
    Signed-off-by: KolbyRKunz <KolbyKunz@yahoo.com>

--- packages/core/src/utils/__tests__/shortenedUrl.test.ts
+++ packages/core/src/utils/__tests__/shortenedUrl.test.ts
@@ -1,7 +1,11 @@
 import type { Response } from 'node-fetch'
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Headers } from 'node-fetch'
+
 import { ConnectionInvitationMessage } from '../../modules/connections'
 import { OutOfBandInvitation } from '../../modules/oob'
+import { convertToNewInvitation } from '../../modules/oob/helpers'
 import { JsonTransformer } from '../JsonTransformer'
 import { MessageValidator } from '../MessageValidator'
 import { fromShortUrl } from '../parseInvitation'
@@ -22,8 +26,15 @@ const mockConnectionInvite = {
   recipientKeys: ['5Gvpf9M4j7vWpHyeTyvBKbjYe7qWc72kGo6qZaLHkLrd'],
 }
 
+const header = new Headers()
+
+header.append('Content-Type', 'application/json')
+
+const dummyHeader = new Headers()
+
 const mockedResponseOobJson = {
   status: 200,
+  ok: true,
   json: async () => ({
     '@type': 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/out-of-band/1.0/invitation',
     '@id': '764af259-8bb4-4546-b91a-924c912d0bb8',
@@ -33,59 +44,71 @@ const mockedResponseOobJson = {
   }),
 } as Response
 
-mockedResponseOobJson.headers.append('Content-Type', 'application/json')
+mockedResponseOobJson['headers'] = header
 
 const mockedResponseOobUrl = {
   status: 200,
-  json: async () => ({
-    url: 'https://wonderful-rabbit-5.tun2.indiciotech.io?oob=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9vdXQtb2YtYmFuZC8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiNzY0YWYyNTktOGJiNC00NTQ2LWI5MWEtOTI0YzkxMmQwYmI4IiwgImxhYmVsIjogIkFsaWNlIiwgImhhbmRzaGFrZV9wcm90b2NvbHMiOiBbImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2Nvbm5lY3Rpb25zLzEuMCJdLCAic2VydmljZXMiOiBbImRpZDpzb3Y6TXZUcVZYQ0VtSjg3dXNMOXVRVG83diJdfQ====',
-  }),
+  ok: true,
+  url: 'https://wonderful-rabbit-5.tun2.indiciotech.io?oob=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9vdXQtb2YtYmFuZC8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiNzY0YWYyNTktOGJiNC00NTQ2LWI5MWEtOTI0YzkxMmQwYmI4IiwgImxhYmVsIjogIkFsaWNlIiwgImhhbmRzaGFrZV9wcm90b2NvbHMiOiBbImRpZDpzb3Y6QnpDYnNOWWhNcmpIaXFaRFRVQVNIZztzcGVjL2Nvbm5lY3Rpb25zLzEuMCJdLCAic2VydmljZXMiOiBbImRpZDpzb3Y6TXZUcVZYQ0VtSjg3dXNMOXVRVG83diJdfQ====',
 } as Response
+
+mockedResponseOobUrl['headers'] = dummyHeader
 
 const mockedResponseConnectionJson = {
   status: 200,
+  ok: true,
   json: async () => ({
-    '@type': 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/out-of-band/1.0/invitation',
-    '@id': '764af259-8bb4-4546-b91a-924c912d0bb8',
-    label: 'Alice',
-    handshake_protocols: ['did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0'],
-    services: ['did:sov:MvTqVXCEmJ87usL9uQTo7v'],
+    '@type': 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/invitation',
+    '@id': '20971ef0-1029-46db-a25b-af4c465dd16b',
+    label: 'test',
+    serviceEndpoint: 'http://sour-cow-15.tun1.indiciotech.io',
+    recipientKeys: ['5Gvpf9M4j7vWpHyeTyvBKbjYe7qWc72kGo6qZaLHkLrd'],
   }),
 } as Response
 
-mockedResponseConnectionJson.headers.append('Content-Type', 'application/json')
+mockedResponseConnectionJson['headers'] = header
 
 const mockedResponseConnectionUrl = {
   status: 200,
-  json: async () => ({
-    url: 'http://sour-cow-15.tun1.indiciotech.io?c_i=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiMjA5NzFlZjAtMTAyOS00NmRiLWEyNWItYWY0YzQ2NWRkMTZiIiwgImxhYmVsIjogInRlc3QiLCAic2VydmljZUVuZHBvaW50IjogImh0dHA6Ly9zb3VyLWNvdy0xNS50dW4xLmluZGljaW90ZWNoLmlvIiwgInJlY2lwaWVudEtleXMiOiBbIjVHdnBmOU00ajd2V3BIeWVUeXZCS2JqWWU3cVdjNzJrR282cVphTEhrTHJkIl19',
-  }),
+  ok: true,
+  url: 'http://sour-cow-15.tun1.indiciotech.io?c_i=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiMjA5NzFlZjAtMTAyOS00NmRiLWEyNWItYWY0YzQ2NWRkMTZiIiwgImxhYmVsIjogInRlc3QiLCAic2VydmljZUVuZHBvaW50IjogImh0dHA6Ly9zb3VyLWNvdy0xNS50dW4xLmluZGljaW90ZWNoLmlvIiwgInJlY2lwaWVudEtleXMiOiBbIjVHdnBmOU00ajd2V3BIeWVUeXZCS2JqWWU3cVdjNzJrR282cVphTEhrTHJkIl19',
 } as Response
+
+mockedResponseConnectionUrl['headers'] = dummyHeader
 
 let outOfBandInvitationMock: OutOfBandInvitation
 let connectionInvitationMock: ConnectionInvitationMessage
+let connectionInvitationToNew: OutOfBandInvitation
 
 beforeAll(async () => {
   outOfBandInvitationMock = await JsonTransformer.fromJSON(mockOobInvite, OutOfBandInvitation)
   await MessageValidator.validate(outOfBandInvitationMock)
   connectionInvitationMock = await JsonTransformer.fromJSON(mockConnectionInvite, ConnectionInvitationMessage)
   await MessageValidator.validate(connectionInvitationMock)
+  connectionInvitationToNew = convertToNewInvitation(connectionInvitationMock)
 })
 
-describe('shortUrl', () => {
-  test('oobToJson', () => {
-    expect(fromShortUrl(mockedResponseOobJson)).toEqual(outOfBandInvitationMock)
+describe('shortUrlOobJson', () => {
+  test('oobToJson', async () => {
+    const short = await fromShortUrl(mockedResponseOobJson)
+    expect(short).toEqual(outOfBandInvitationMock)
   })
-
-  test('oobFromUrl', () => {
-    expect(fromShortUrl(mockedResponseOobUrl)).toEqual(outOfBandInvitationMock)
+})
+describe('shortUrlOobUrl', () => {
+  test('oobFromUrl', async () => {
+    const short = await fromShortUrl(mockedResponseOobUrl)
+    expect(short).toEqual(outOfBandInvitationMock)
   })
-
-  test('connectionInvitationToJson', () => {
-    expect(fromShortUrl(mockedResponseConnectionJson)).toEqual(connectionInvitationMock)
+})
+describe('shortUrlConnectionJson', () => {
+  test('connectionInvitationToJson', async () => {
+    const short = await fromShortUrl(mockedResponseConnectionJson)
+    expect(short).toEqual(connectionInvitationToNew)
   })
-
-  test('connectionFromUrl', () => {
-    expect(fromShortUrl(mockedResponseConnectionUrl)).toEqual(connectionInvitationMock)
+})
+describe('shortUrlConnectionUrl', () => {
+  test('connectionFromUrl', async () => {
+    const short = await fromShortUrl(mockedResponseConnectionUrl)
+    expect(short).toEqual(connectionInvitationToNew)
   })
 })
