TimoGlastra/aries-framework-javascript

refactor!: add agent context
Signed-off-by: Timo Glastra <timo@animo.id>

packages/core/src/modules/credentials/protocol/v2/handlers/V2OfferCredentialHandler.ts
@@ -1,6 +1,6 @@
-import type { AgentConfig } from '../../../../../agent/AgentConfig'
 import type { Handler, HandlerInboundMessage } from '../../../../../agent/Handler'
 import type { InboundMessageContext } from '../../../../../agent/models/InboundMessageContext'
+import type { Logger } from '../../../../../logger'
 import type { DidCommMessageRepository } from '../../../../../storage'
 import type { MediationRecipientService } from '../../../../routing/services/MediationRecipientService'
 import type { CredentialExchangeRecord } from '../../../repository/CredentialExchangeRecord'
@@ -13,27 +13,28 @@ import { V2OfferCredentialMessage } from '../messages/V2OfferCredentialMessage'
 
 export class V2OfferCredentialHandler implements Handler {
   private credentialService: V2CredentialService
-  private agentConfig: AgentConfig
   private mediationRecipientService: MediationRecipientService
+  private logger: Logger
+
   public supportedMessages = [V2OfferCredentialMessage]
   private didCommMessageRepository: DidCommMessageRepository
 
   public constructor(
     credentialService: V2CredentialService,
-    agentConfig: AgentConfig,
     mediationRecipientService: MediationRecipientService,
-    didCommMessageRepository: DidCommMessageRepository
+    didCommMessageRepository: DidCommMessageRepository,
+    logger: Logger
   ) {
     this.credentialService = credentialService
-    this.agentConfig = agentConfig
     this.mediationRecipientService = mediationRecipientService
     this.didCommMessageRepository = didCommMessageRepository
+    this.logger = logger
   }
 
   public async handle(messageContext: InboundMessageContext<V2OfferCredentialMessage>) {
     const credentialRecord = await this.credentialService.processOffer(messageContext)
 
-    const shouldAutoRespond = await this.credentialService.shouldAutoRespondToOffer({
+    const shouldAutoRespond = await this.credentialService.shouldAutoRespondToOffer(messageContext.agentContext, {
       credentialRecord,
       offerMessage: messageContext.message,
     })
@@ -48,17 +49,17 @@ export class V2OfferCredentialHandler implements Handler {
     messageContext: HandlerInboundMessage<V2OfferCredentialHandler>,
     offerMessage?: V2OfferCredentialMessage
   ) {
-    this.agentConfig.logger.info(
-      `Automatically sending request with autoAccept on ${this.agentConfig.autoAcceptCredentials}`
+    this.logger.info(
+      `Automatically sending request with autoAccept on ${messageContext.agentContext.config.autoAcceptCredentials}`
     )
 
     if (messageContext.connection) {
-      const { message } = await this.credentialService.acceptOffer({
+      const { message } = await this.credentialService.acceptOffer(messageContext.agentContext, {
         credentialRecord,
       })
       return createOutboundMessage(messageContext.connection, message)
     } else if (offerMessage?.service) {
-      const routing = await this.mediationRecipientService.getRouting()
+      const routing = await this.mediationRecipientService.getRouting(messageContext.agentContext)
       const ourService = new ServiceDecorator({
         serviceEndpoint: routing.endpoints[0],
         recipientKeys: [routing.recipientKey.publicKeyBase58],
@@ -66,14 +67,14 @@ export class V2OfferCredentialHandler implements Handler {
       })
       const recipientService = offerMessage.service
 
-      const { message } = await this.credentialService.acceptOffer({
+      const { message } = await this.credentialService.acceptOffer(messageContext.agentContext, {
         credentialRecord,
       })
 
       // Set and save ~service decorator to record (to remember our verkey)
       message.service = ourService
 
-      await this.didCommMessageRepository.saveOrUpdateAgentMessage({
+      await this.didCommMessageRepository.saveOrUpdateAgentMessage(messageContext.agentContext, {
         agentMessage: message,
         role: DidCommMessageRole.Sender,
         associatedRecordId: credentialRecord.id,
@@ -86,6 +87,6 @@ export class V2OfferCredentialHandler implements Handler {
       })
     }
 
-    this.agentConfig.logger.error(`Could not automatically create credential request`)
+    this.logger.error(`Could not automatically create credential request`)
   }
 }

packages/core/src/modules/proofs/handlers/RequestPresentationHandler.ts
@@ -1,5 +1,5 @@
-import type { AgentConfig } from '../../../agent/AgentConfig'
 import type { Handler, HandlerInboundMessage } from '../../../agent/Handler'
+import type { Logger } from '../../../logger'
 import type { MediationRecipientService } from '../../routing'
 import type { ProofResponseCoordinator } from '../ProofResponseCoordinator'
 import type { ProofRecord } from '../repository'
@@ -11,27 +11,27 @@ import { RequestPresentationMessage } from '../messages'
 
 export class RequestPresentationHandler implements Handler {
   private proofService: ProofService
-  private agentConfig: AgentConfig
   private proofResponseCoordinator: ProofResponseCoordinator
   private mediationRecipientService: MediationRecipientService
+  private logger: Logger
   public supportedMessages = [RequestPresentationMessage]
 
   public constructor(
     proofService: ProofService,
-    agentConfig: AgentConfig,
     proofResponseCoordinator: ProofResponseCoordinator,
-    mediationRecipientService: MediationRecipientService
+    mediationRecipientService: MediationRecipientService,
+    logger: Logger
   ) {
     this.proofService = proofService
-    this.agentConfig = agentConfig
     this.proofResponseCoordinator = proofResponseCoordinator
     this.mediationRecipientService = mediationRecipientService
+    this.logger = logger
   }
 
   public async handle(messageContext: HandlerInboundMessage<RequestPresentationHandler>) {
     const proofRecord = await this.proofService.processRequest(messageContext)
 
-    if (this.proofResponseCoordinator.shouldAutoRespondToRequest(proofRecord)) {
+    if (this.proofResponseCoordinator.shouldAutoRespondToRequest(messageContext.agentContext, proofRecord)) {
       return await this.createPresentation(proofRecord, messageContext)
     }
   }
@@ -43,28 +43,36 @@ export class RequestPresentationHandler implements Handler {
     const indyProofRequest = record.requestMessage?.indyProofRequest
     const presentationProposal = record.proposalMessage?.presentationProposal
 
-    this.agentConfig.logger.info(
-      `Automatically sending presentation with autoAccept on ${this.agentConfig.autoAcceptProofs}`
+    this.logger.info(
+      `Automatically sending presentation with autoAccept on ${messageContext.agentContext.config.autoAcceptProofs}`
     )
 
     if (!indyProofRequest) {
-      this.agentConfig.logger.error('Proof request is undefined.')
+      this.logger.error('Proof request is undefined.')
       return
     }
 
-    const retrievedCredentials = await this.proofService.getRequestedCredentialsForProofRequest(indyProofRequest, {
-      presentationProposal,
-    })
+    const retrievedCredentials = await this.proofService.getRequestedCredentialsForProofRequest(
+      messageContext.agentContext,
+      indyProofRequest,
+      {
+        presentationProposal,
+      }
+    )
 
     const requestedCredentials = this.proofService.autoSelectCredentialsForProofRequest(retrievedCredentials)
 
-    const { message, proofRecord } = await this.proofService.createPresentation(record, requestedCredentials)
+    const { message, proofRecord } = await this.proofService.createPresentation(
+      messageContext.agentContext,
+      record,
+      requestedCredentials
+    )
 
     if (messageContext.connection) {
       return createOutboundMessage(messageContext.connection, message)
     } else if (proofRecord.requestMessage?.service) {
       // Create ~service decorator
-      const routing = await this.mediationRecipientService.getRouting()
+      const routing = await this.mediationRecipientService.getRouting(messageContext.agentContext)
       const ourService = new ServiceDecorator({
         serviceEndpoint: routing.endpoints[0],
         recipientKeys: [routing.recipientKey.publicKeyBase58],
@@ -76,7 +84,7 @@ export class RequestPresentationHandler implements Handler {
       // Set and save ~service decorator to record (to remember our verkey)
       message.service = ourService
       proofRecord.presentationMessage = message
-      await this.proofService.update(proofRecord)
+      await this.proofService.update(messageContext.agentContext, proofRecord)
 
       return createOutboundServiceMessage({
         payload: message,
@@ -85,6 +93,6 @@ export class RequestPresentationHandler implements Handler {
       })
     }
 
-    this.agentConfig.logger.error(`Could not automatically create presentation`)
+    this.logger.error(`Could not automatically create presentation`)
   }
 }

openwallet-foundation/credo-ts

packages/core/src/modules/credentials/protocol/v2/handlers/V2OfferCredentialHandler.ts
@@ -1,6 +1,6 @@
-import type { AgentConfig } from '../../../../../agent/AgentConfig'
 import type { Handler, HandlerInboundMessage } from '../../../../../agent/Handler'
 import type { InboundMessageContext } from '../../../../../agent/models/InboundMessageContext'
+import type { Logger } from '../../../../../logger'
 import type { DidCommMessageRepository } from '../../../../../storage'
 import type { RoutingService } from '../../../../routing/services/RoutingService'
 import type { CredentialExchangeRecord } from '../../../repository/CredentialExchangeRecord'
@@ -13,27 +13,28 @@ import { V2OfferCredentialMessage } from '../messages/V2OfferCredentialMessage'
 
 export class V2OfferCredentialHandler implements Handler {
   private credentialService: V2CredentialService
-  private agentConfig: AgentConfig
   private routingService: RoutingService
+  private logger: Logger
+
   public supportedMessages = [V2OfferCredentialMessage]
   private didCommMessageRepository: DidCommMessageRepository
 
   public constructor(
     credentialService: V2CredentialService,
-    agentConfig: AgentConfig,
     routingService: RoutingService,
-    didCommMessageRepository: DidCommMessageRepository
+    didCommMessageRepository: DidCommMessageRepository,
+    logger: Logger
   ) {
     this.credentialService = credentialService
-    this.agentConfig = agentConfig
     this.routingService = routingService
     this.didCommMessageRepository = didCommMessageRepository
+    this.logger = logger
   }
 
   public async handle(messageContext: InboundMessageContext<V2OfferCredentialMessage>) {
     const credentialRecord = await this.credentialService.processOffer(messageContext)
 
-    const shouldAutoRespond = await this.credentialService.shouldAutoRespondToOffer({
+    const shouldAutoRespond = await this.credentialService.shouldAutoRespondToOffer(messageContext.agentContext, {
       credentialRecord,
       offerMessage: messageContext.message,
     })
@@ -48,17 +49,17 @@ export class V2OfferCredentialHandler implements Handler {
     messageContext: HandlerInboundMessage<V2OfferCredentialHandler>,
     offerMessage?: V2OfferCredentialMessage
   ) {
-    this.agentConfig.logger.info(
-      `Automatically sending request with autoAccept on ${this.agentConfig.autoAcceptCredentials}`
+    this.logger.info(
+      `Automatically sending request with autoAccept on ${messageContext.agentContext.config.autoAcceptCredentials}`
     )
 
     if (messageContext.connection) {
-      const { message } = await this.credentialService.acceptOffer({
+      const { message } = await this.credentialService.acceptOffer(messageContext.agentContext, {
         credentialRecord,
       })
       return createOutboundMessage(messageContext.connection, message)
     } else if (offerMessage?.service) {
-      const routing = await this.routingService.getRouting()
+      const routing = await this.routingService.getRouting(messageContext.agentContext)
       const ourService = new ServiceDecorator({
         serviceEndpoint: routing.endpoints[0],
         recipientKeys: [routing.recipientKey.publicKeyBase58],
@@ -66,14 +67,14 @@ export class V2OfferCredentialHandler implements Handler {
       })
       const recipientService = offerMessage.service
 
-      const { message } = await this.credentialService.acceptOffer({
+      const { message } = await this.credentialService.acceptOffer(messageContext.agentContext, {
         credentialRecord,
       })
 
       // Set and save ~service decorator to record (to remember our verkey)
       message.service = ourService
 
-      await this.didCommMessageRepository.saveOrUpdateAgentMessage({
+      await this.didCommMessageRepository.saveOrUpdateAgentMessage(messageContext.agentContext, {
         agentMessage: message,
         role: DidCommMessageRole.Sender,
         associatedRecordId: credentialRecord.id,
@@ -86,6 +87,6 @@ export class V2OfferCredentialHandler implements Handler {
       })
     }
 
-    this.agentConfig.logger.error(`Could not automatically create credential request`)
+    this.logger.error(`Could not automatically create credential request`)
   }
 }

packages/core/src/modules/proofs/handlers/RequestPresentationHandler.ts
@@ -1,5 +1,5 @@
-import type { AgentConfig } from '../../../agent/AgentConfig'
 import type { Handler, HandlerInboundMessage } from '../../../agent/Handler'
+import type { Logger } from '../../../logger'
 import type { RoutingService } from '../../routing/services/RoutingService'
 import type { ProofResponseCoordinator } from '../ProofResponseCoordinator'
 import type { ProofRecord } from '../repository'
@@ -11,27 +11,27 @@ import { RequestPresentationMessage } from '../messages'
 
 export class RequestPresentationHandler implements Handler {
   private proofService: ProofService
-  private agentConfig: AgentConfig
   private proofResponseCoordinator: ProofResponseCoordinator
   private routingService: RoutingService
+  private logger: Logger
   public supportedMessages = [RequestPresentationMessage]
 
   public constructor(
     proofService: ProofService,
-    agentConfig: AgentConfig,
     proofResponseCoordinator: ProofResponseCoordinator,
-    routingService: RoutingService
+    routingService: RoutingService,
+    logger: Logger
   ) {
     this.proofService = proofService
-    this.agentConfig = agentConfig
     this.proofResponseCoordinator = proofResponseCoordinator
     this.routingService = routingService
+    this.logger = logger
   }
 
   public async handle(messageContext: HandlerInboundMessage<RequestPresentationHandler>) {
     const proofRecord = await this.proofService.processRequest(messageContext)
 
-    if (this.proofResponseCoordinator.shouldAutoRespondToRequest(proofRecord)) {
+    if (this.proofResponseCoordinator.shouldAutoRespondToRequest(messageContext.agentContext, proofRecord)) {
       return await this.createPresentation(proofRecord, messageContext)
     }
   }
@@ -43,28 +43,36 @@ export class RequestPresentationHandler implements Handler {
     const indyProofRequest = record.requestMessage?.indyProofRequest
     const presentationProposal = record.proposalMessage?.presentationProposal
 
-    this.agentConfig.logger.info(
-      `Automatically sending presentation with autoAccept on ${this.agentConfig.autoAcceptProofs}`
+    this.logger.info(
+      `Automatically sending presentation with autoAccept on ${messageContext.agentContext.config.autoAcceptProofs}`
     )
 
     if (!indyProofRequest) {
-      this.agentConfig.logger.error('Proof request is undefined.')
+      this.logger.error('Proof request is undefined.')
       return
     }
 
-    const retrievedCredentials = await this.proofService.getRequestedCredentialsForProofRequest(indyProofRequest, {
-      presentationProposal,
-    })
+    const retrievedCredentials = await this.proofService.getRequestedCredentialsForProofRequest(
+      messageContext.agentContext,
+      indyProofRequest,
+      {
+        presentationProposal,
+      }
+    )
 
     const requestedCredentials = this.proofService.autoSelectCredentialsForProofRequest(retrievedCredentials)
 
-    const { message, proofRecord } = await this.proofService.createPresentation(record, requestedCredentials)
+    const { message, proofRecord } = await this.proofService.createPresentation(
+      messageContext.agentContext,
+      record,
+      requestedCredentials
+    )
 
     if (messageContext.connection) {
       return createOutboundMessage(messageContext.connection, message)
     } else if (proofRecord.requestMessage?.service) {
       // Create ~service decorator
-      const routing = await this.routingService.getRouting()
+      const routing = await this.routingService.getRouting(messageContext.agentContext)
       const ourService = new ServiceDecorator({
         serviceEndpoint: routing.endpoints[0],
         recipientKeys: [routing.recipientKey.publicKeyBase58],
@@ -76,7 +84,7 @@ export class RequestPresentationHandler implements Handler {
       // Set and save ~service decorator to record (to remember our verkey)
       message.service = ourService
       proofRecord.presentationMessage = message
-      await this.proofService.update(proofRecord)
+      await this.proofService.update(messageContext.agentContext, proofRecord)
 
       return createOutboundServiceMessage({
         payload: message,
@@ -85,6 +93,6 @@ export class RequestPresentationHandler implements Handler {
       })
     }
 
-    this.agentConfig.logger.error(`Could not automatically create presentation`)
+    this.logger.error(`Could not automatically create presentation`)
   }
 }