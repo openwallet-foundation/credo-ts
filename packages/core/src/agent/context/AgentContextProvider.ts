22bf4a3620e529f6a173a2069fdfa883800b18e5
Timo Glastra <timo@animo.id>
2022-07-03 16:13:12 +0200

    feat: get agent context by correlation id
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/agent/context/AgentContextProvider.ts
+++ packages/core/src/agent/context/AgentContextProvider.ts
@@ -2,7 +2,7 @@ import type { AgentContext } from './AgentContext'
 
 export interface AgentContextProvider {
   /**
-   * Find the agent context based for an inbound message. It's possible to provide a contextCorrelationId to make it
+   * Get the agent context for an inbound message. It's possible to provide a contextCorrelationId to make it
    * easier for the context provider implementation to correlate inbound messages to the correct context. This can be useful if
    * a plaintext message is passed and the context provider can't determine the context based on the recipient public keys
    * of the inbound message.
@@ -14,4 +14,10 @@ export interface AgentContextProvider {
     inboundMessage: unknown,
     options?: { contextCorrelationId?: string }
   ): Promise<AgentContext>
+
+  /**
+   * Get the agent context for a context correlation id. Will throw an error if no AgentContext could be retrieved
+   * for the specified contextCorrelationId.
+   */
+  getAgentContextForContextCorrelationId(contextCorrelationId: string): Promise<AgentContext>
 }
