ba90926d4769310607481f00e9f4a9253c5088de
Timo Glastra <timo@animo.id>
2022-07-24 21:00:52 +0200

    refactor: do not log websocket data (#962)

--- packages/node/src/transport/WsInboundTransport.ts
+++ packages/node/src/transport/WsInboundTransport.ts
@@ -61,7 +61,7 @@ export class WsInboundTransport implements InboundTransport {
   private listenOnWebSocketMessages(agent: Agent, socket: WebSocket, session: TransportSession) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     socket.addEventListener('message', async (event: any) => {
-      this.logger.debug('WebSocket message event received.', { url: event.target.url, data: event.data })
+      this.logger.debug('WebSocket message event received.', { url: event.target.url })
       try {
         await agent.receiveMessage(JSON.parse(event.data), session)
       } catch (error) {
