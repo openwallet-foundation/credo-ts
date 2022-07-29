c800e6299d56e57f05a461a4b18b684f3fc164b2
Moriarty <moritz@animo.id>
2022-07-29 15:12:42 +0200

    chore: refactor
    Signed-off-by: Moriarty <moritz@animo.id>

--- packages/core/src/modules/indy/repository/AnonCredsCredentialDefinitionRecord.ts
+++ packages/core/src/modules/indy/repository/AnonCredsCredentialDefinitionRecord.ts
@@ -2,7 +2,7 @@ import type { DidIndyNamespace } from '../../../utils/indyIdentifiers'
 import type { CredDef } from 'indy-sdk'
 
 import { BaseRecord } from '../../../storage/BaseRecord'
-import { isQualifiedIdentifier, getQualifiedIdentifier } from '../../../utils/indyIdentifiers'
+import { getQualifiedIdentifier } from '../../../utils/indyIdentifiers'
 import { uuid } from '../../../utils/uuid'
 
 export interface AnonCredsCredentialDefinitionRecordProps {
@@ -24,12 +24,10 @@ export class AnonCredsCredentialDefinitionRecord extends BaseRecord {
     if (props) {
       this.credentialDefinition = props.credentialDefinition
       this.schemaSeqNo = props.schemaSeqNo
-      this._tags.id = isQualifiedIdentifier(this._tags.id as string)
-        ? (this._tags.id as string)
-        : getQualifiedIdentifier(props.didIndyNamespace, {
-            ...this.credentialDefinition,
-            schemaSeqNo: props.schemaSeqNo,
-          })
+      this._tags.id = getQualifiedIdentifier(props.didIndyNamespace, {
+        ...this.credentialDefinition,
+        schemaSeqNo: props.schemaSeqNo,
+      })
     }
   }
 
--- packages/core/src/modules/indy/repository/AnonCredsSchemaRecord.ts
+++ packages/core/src/modules/indy/repository/AnonCredsSchemaRecord.ts
@@ -2,7 +2,7 @@ import type { DidIndyNamespace } from '../../../utils/indyIdentifiers'
 import type { Schema } from 'indy-sdk'
 
 import { BaseRecord } from '../../../storage/BaseRecord'
-import { isQualifiedIdentifier, getQualifiedIdentifier } from '../../../utils/indyIdentifiers'
+import { getQualifiedIdentifier } from '../../../utils/indyIdentifiers'
 import { uuid } from '../../../utils/uuid'
 
 export interface AnonCredsSchemaRecordProps {
@@ -21,9 +21,7 @@ export class AnonCredsSchemaRecord extends BaseRecord {
     this.id = uuid()
     if (props) {
       this.schema = props.schema
-      this._tags.id = isQualifiedIdentifier(this._tags.id as string)
-        ? this._tags.id
-        : getQualifiedIdentifier(props.didIndyNamespace, this.schema)
+      this._tags.id = getQualifiedIdentifier(props.didIndyNamespace, this.schema)
     }
   }
 
