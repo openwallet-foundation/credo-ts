e379c9ed065bc1486d284b0e524bf858a682c695
Mike Richardson <mike.richardson@ontario.ca>
2022-08-15 10:26:44 +0300

    Put Config back into ProofsModule
    Signed-off-by: Mike Richardson <mike.richardson@ontario.ca>

--- packages/core/src/modules/proofs/ProofsModule.ts
+++ packages/core/src/modules/proofs/ProofsModule.ts
@@ -1,20 +1,31 @@
 import type { DependencyManager, Module } from '../../plugins'
+import type { ProofsModuleConfigOptions } from './ProofsModuleConfig'
 
 import { RevocationNotificationService } from '../credentials/protocol/revocation-notification/services'
 
 import { ProofsApi } from './ProofsApi'
+import { ProofsModuleConfig } from './ProofsModuleConfig'
 import { IndyProofFormatService } from './formats/indy/IndyProofFormatService'
 import { V1ProofService } from './protocol/v1'
 import { V2ProofService } from './protocol/v2'
 import { ProofRepository } from './repository'
 
 export class ProofsModule implements Module {
+  public readonly config: ProofsModuleConfig
+
+  public constructor(config?: ProofsModuleConfigOptions) {
+    this.config = new ProofsModuleConfig(config)
+  }
+
   /**
    * Registers the dependencies of the proofs module on the dependency manager.
    */
   public register(dependencyManager: DependencyManager) {
     // Api
     dependencyManager.registerContextScoped(ProofsApi)
+
+    // Config
+    dependencyManager.registerInstance(ProofsModuleConfig, this.config)
 
     // Services
     dependencyManager.registerSingleton(V1ProofService)
