642d4a2d202230f8f7e97232cb1f6537997e3496
DenisRybas <denis.rybas@dsr-corporation.com>
2022-09-14 16:33:47 +0300

    helper method for parsing added

--- packages/core/src/utils/JWE.ts
+++ packages/core/src/utils/JWE.ts
@@ -1,6 +1,16 @@
 import type { EncryptedMessage } from '../agent/didcomm'
-
+import { DidDocument } from '../../../peer-did-ts/src/did-doc/DidDocument'
+import { DecryptedMessageContext } from '../../src/agent/didcomm/types'
+import { isDid } from './did'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isValidJweStructure(message: any): message is EncryptedMessage {
   return message && typeof message === 'object' && message.protected && message.iv && message.ciphertext && message.tag
+}
+
+export function didFromKid(decryptedMessageContext: DecryptedMessageContext): string | undefined {
+  const kid = decryptedMessageContext.recipient
+  if (!kid) return undefined
+
+  const did = DidDocument.extractDidFromKid(kid)
+  return isDid(did) ? did : undefined
 }
