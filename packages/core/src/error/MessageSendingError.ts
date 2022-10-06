df66ef8c07c4b18fc95ab8d2c468026695e7af57
Ariel Gentile <gentilester@gmail.com>
2022-10-06 09:00:29 -0500

    refactor: remove unused parameter
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/core/src/error/MessageSendingError.ts
+++ packages/core/src/error/MessageSendingError.ts
@@ -4,10 +4,7 @@ import { AriesFrameworkError } from './AriesFrameworkError'
 
 export class MessageSendingError extends AriesFrameworkError {
   public outboundMessage: OutboundMessage
-  public constructor(
-    message: string,
-    { outboundMessage, cause }: { outboundMessage: OutboundMessage; associatedRecordId?: string; cause?: Error }
-  ) {
+  public constructor(message: string, { outboundMessage, cause }: { outboundMessage: OutboundMessage; cause?: Error }) {
     super(message, { cause })
     this.outboundMessage = outboundMessage
   }
