1db637a11d07e625c9d353533cf382ba8282b54c
Karim Stekelenburg <karim@animo.id>
2022-11-15 20:04:02 +0100

    feat: copy indy-sdk related files to indy-sdk pkg
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- packages/core/src/modules/vc/signature-suites/index.ts
+++ packages/core/src/modules/vc/signature-suites/index.ts
@@ -1,2 +1,3 @@
 export * from './ed25519/Ed25519Signature2018'
+export * from './ed25519/constants'
 export * from './JwsLinkedDataSignature'
---
+++ packages/indy-sdk/src/ledger/services/index.ts
@@ -0,0 +1,2 @@
+export * from './IndyLedgerService'
+export * from './IndyPoolService'
