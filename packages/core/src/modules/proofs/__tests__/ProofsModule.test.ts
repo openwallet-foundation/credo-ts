87a126bfebbf9ce3a69cf79e1aef3cc5d1120140
Mike Richardson <mike.richardson@ontario.ca>
2022-11-24 09:54:15 +0200

    Fix test
    Signed-off-by: Mike Richardson <mike.richardson@ontario.ca>

--- packages/core/src/modules/proofs/__tests__/ProofsModule.test.ts
+++ packages/core/src/modules/proofs/__tests__/ProofsModule.test.ts
@@ -1,27 +1,31 @@
+import { FeatureRegistry } from '../../../agent/FeatureRegistry'
 import { DependencyManager } from '../../../plugins/DependencyManager'
 import { ProofsApi } from '../ProofsApi'
 import { ProofsModule } from '../ProofsModule'
-import { IndyProofFormatService } from '../formats/indy/IndyProofFormatService'
 import { V1ProofService } from '../protocol/v1/V1ProofService'
 import { V2ProofService } from '../protocol/v2/V2ProofService'
 import { ProofRepository } from '../repository'
 
 jest.mock('../../../plugins/DependencyManager')
-const DependencyManagerMock = DependencyManager as jest.Mock<DependencyManager>
+jest.mock('../../../agent/FeatureRegistry')
 
+const DependencyManagerMock = DependencyManager as jest.Mock<DependencyManager>
 const dependencyManager = new DependencyManagerMock()
+const FeatureRegistryMock = FeatureRegistry as jest.Mock<FeatureRegistry>
+const featureRegistry = new FeatureRegistryMock()
 
 describe('ProofsModule', () => {
   test('registers dependencies on the dependency manager', () => {
-    new ProofsModule().register(dependencyManager)
+    new ProofsModule().register(dependencyManager, featureRegistry)
 
     expect(dependencyManager.registerContextScoped).toHaveBeenCalledTimes(1)
     expect(dependencyManager.registerContextScoped).toHaveBeenCalledWith(ProofsApi)
 
-    expect(dependencyManager.registerSingleton).toHaveBeenCalledTimes(4)
+    expect(dependencyManager.registerSingleton).toHaveBeenCalledTimes(3)
     expect(dependencyManager.registerSingleton).toHaveBeenCalledWith(V1ProofService)
     expect(dependencyManager.registerSingleton).toHaveBeenCalledWith(V2ProofService)
     expect(dependencyManager.registerSingleton).toHaveBeenCalledWith(ProofRepository)
-    expect(dependencyManager.registerSingleton).toHaveBeenCalledWith(IndyProofFormatService)
+
+    expect(featureRegistry.register).toHaveBeenCalledTimes(2)
   })
 })
