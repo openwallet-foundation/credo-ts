d59abc77e6717fc128c65c2e6935cffa019aefba
Mike Richardson <mike.richardson@ontario.ca>
2022-11-24 09:16:29 +0200

    minor tweaks
    Signed-off-by: Mike Richardson <mike.richardson@ontario.ca>

--- tests/transport/SubjectInboundTransport.ts
+++ tests/transport/SubjectInboundTransport.ts
@@ -16,8 +16,6 @@ export class SubjectInboundTransport implements InboundTransport {
   private subscription?: Subscription
 
   public constructor(ourSubject = new Subject<SubjectMessage>()) {
-
-    console.trace()
     this.ourSubject = ourSubject
   }
 
