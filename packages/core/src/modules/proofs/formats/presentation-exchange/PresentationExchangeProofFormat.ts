12f7690949e7f83a8ccd496aa9dba57573bd0959
Mike Richardson <mike.richardson@ontario.ca>
2022-12-06 12:35:46 +0200

    Changes from PR Comments
    Signed-off-by: Mike Richardson <mike.richardson@ontario.ca>

--- packages/core/src/modules/proofs/formats/presentation-exchange/PresentationExchangeProofFormat.ts
+++ packages/core/src/modules/proofs/formats/presentation-exchange/PresentationExchangeProofFormat.ts
@@ -1,10 +1,4 @@
-import type { LinkedAttachment } from '../../../../utils/LinkedAttachment'
-import type { CredentialPreviewAttributeOptions } from '../../../credentials'
 import type { RequestProofFormats } from '../../models/SharedOptions'
-import type {
-  PresentationPreviewAttribute,
-  PresentationPreviewPredicate,
-} from '../../protocol/v1/models/V1PresentationPreview'
 import type { ProofExchangeRecord } from '../../repository'
 import type { ProofFormat } from '../ProofFormat'
 import type { ProofAttachmentFormat } from '../models/ProofAttachmentFormat'
@@ -15,27 +9,6 @@ import type { IVerifiableCredential } from '@sphereon/ssi-types'
 export const V2_PRESENTATION_EXCHANGE_PRESENTATION_PROPOSAL = 'dif/presentation-exchange/definitions@v1.0'
 export const V2_PRESENTATION_EXCHANGE_PRESENTATION_REQUEST = 'dif/presentation-exchange/definitions@v1.0'
 export const V2_PRESENTATION_EXCHANGE_PRESENTATION = 'dif/presentation-exchange/submission@v1.0'
-
-export interface IndyProposeProofFormat {
-  attributes?: PresentationPreviewAttribute[]
-  predicates?: PresentationPreviewPredicate[]
-  nonce: string
-  name: string
-  version: string
-}
-
-/**
- * This defines the module payload for calling CredentialsApi.acceptProposal
- */
-export interface IndyAcceptProposalFormat {
-  credentialDefinitionId?: string
-  attributes?: CredentialPreviewAttributeOptions[]
-  linkedAttachments?: LinkedAttachment[]
-}
-
-export interface IndyAcceptOfferFormat {
-  holderDid?: string
-}
 
 export interface PresentationExchangeProposalFormat {
   presentationDefinition: PresentationDefinitionV1
