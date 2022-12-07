9aa4cc486d61d79b2426abd9086128bcba019b49
Ariel Gentile <gentilester@gmail.com>
2022-12-07 18:20:38 -0300

    fix: missing exports
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/core/src/agent/models/OutboundMessageContext.ts
+++ packages/core/src/agent/models/OutboundMessageContext.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Key } from '../../crypto'
 import type { ConnectionRecord } from '../../modules/connections'
 import type { ResolvedDidCommService } from '../../modules/didcomm'
