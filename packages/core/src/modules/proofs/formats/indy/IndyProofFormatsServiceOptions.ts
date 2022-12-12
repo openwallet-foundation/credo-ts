bb0c2b2ec5977ed116b146682f0776607966390c
Tipu Singh <tipu.singh@ayanworks.com>
2022-12-12 13:36:04 +0530

    chore: ran prettier
    Signed-off-by: Tipu Singh <tipu.singh@ayanworks.com>

--- packages/core/src/modules/proofs/formats/indy/IndyProofFormatsServiceOptions.ts
+++ packages/core/src/modules/proofs/formats/indy/IndyProofFormatsServiceOptions.ts
@@ -1,11 +1,11 @@
 import type { Attachment } from '../../../../decorators/attachment/Attachment'
-import type { GetRequestedCredentialsConfig } from '../../models/GetRequestedCredentialsConfig'
-import type { IndyRequestedCredentialsFormat } from './IndyProofFormat'
 import type { IndyRevocationInterval } from '../../../credentials'
+import type { GetRequestedCredentialsConfig } from '../../models/GetRequestedCredentialsConfig'
 import type { PresentationPreview } from '../../protocol/v1/models/V1PresentationPreview'
-import type { ProofAttributeInfo } from '.././indy/models/ProofAttributeInfo'
 import type { ProofExchangeRecord } from '../../repository/ProofExchangeRecord'
+import type { ProofAttributeInfo } from '.././indy/models/ProofAttributeInfo'
 import type { ProofPredicateInfo } from '.././indy/models/ProofPredicateInfo'
+import type { IndyRequestedCredentialsFormat } from './IndyProofFormat'
 
 export type IndyPresentationProofFormat = IndyRequestedCredentialsFormat
 
