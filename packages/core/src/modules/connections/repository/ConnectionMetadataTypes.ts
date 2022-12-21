c0be64eb45629ec12397a385f2b93060309ecb28
Akiff Manji <akiff.manji@gmail.com>
2022-12-20 19:52:59 -0800

    feat(connections): add internal metadata keys
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- packages/core/src/modules/connections/repository/ConnectionMetadataTypes.ts
+++ packages/core/src/modules/connections/repository/ConnectionMetadataTypes.ts
@@ -1,9 +1,13 @@
+import type { ConnectionType } from '../models'
+
 export enum ConnectionMetadataKeys {
   UseDidKeysForProtocol = '_internal/useDidKeysForProtocol',
+  ConnectionTypes = '_internal/connectionTypes',
 }
 
 export type ConnectionMetadata = {
   [ConnectionMetadataKeys.UseDidKeysForProtocol]: {
     [protocolUri: string]: boolean
   }
+  [ConnectionMetadataKeys.ConnectionTypes]: Array<ConnectionType | string>
 }
