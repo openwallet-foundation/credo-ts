206d8ec556307ceb230e29acc7eeb50590884321
Akiff Manji <akiff.manji@gmail.com>
2022-12-20 21:50:04 -0800

    refactor(connection): remove metadata connection types
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- packages/core/src/modules/connections/repository/ConnectionMetadataTypes.ts
+++ packages/core/src/modules/connections/repository/ConnectionMetadataTypes.ts
@@ -1,13 +1,9 @@
-import type { ConnectionType } from '../models'
-
 export enum ConnectionMetadataKeys {
   UseDidKeysForProtocol = '_internal/useDidKeysForProtocol',
-  ConnectionTypes = '_internal/connectionTypes',
 }
 
 export type ConnectionMetadata = {
   [ConnectionMetadataKeys.UseDidKeysForProtocol]: {
     [protocolUri: string]: boolean
   }
-  [ConnectionMetadataKeys.ConnectionTypes]: Array<ConnectionType | string>
 }
