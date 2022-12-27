460510db43a7c63fd8dc1c3614be03fd8772f63c
Ariel Gentile <gentilester@gmail.com>
2022-12-27 19:49:20 -0300

    fix: missing migration script and exports (#1184)
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/core/src/storage/migration/updates/0.3-0.3.1/__tests__/did.test.ts
+++ packages/core/src/storage/migration/updates/0.3-0.3.1/__tests__/did.test.ts
@@ -7,7 +7,7 @@ import { JsonTransformer } from '../../../../../utils'
 import { Metadata } from '../../../../Metadata'
 import * as testModule from '../did'
 
-const agentConfig = getAgentConfig('Migration DidRecord 0.2-0.3')
+const agentConfig = getAgentConfig('Migration DidRecord 0.3-0.3.1')
 const agentContext = getAgentContext()
 
 jest.mock('../../../../../modules/dids/repository/DidRepository')
@@ -29,20 +29,20 @@ jest.mock('../../../../../agent/Agent', () => {
 // Mock typed object
 const AgentMock = Agent as jest.Mock<Agent>
 
-describe('0.2-0.3 | Did', () => {
+describe('0.3-0.3.1 | Did', () => {
   let agent: Agent
 
   beforeEach(() => {
     agent = new AgentMock()
   })
 
-  describe('migrateDidRecordToV0_3()', () => {
+  describe('migrateDidRecordToV0_3_1()', () => {
     it('should fetch all records and apply the needed updates ', async () => {
       const records: DidRecord[] = [getDid({ id: 'did:peer:123' })]
 
       mockFunction(didRepository.getAll).mockResolvedValue(records)
 
-      await testModule.migrateDidRecordToV0_3(agent)
+      await testModule.migrateDidRecordToV0_3_1(agent)
 
       expect(didRepository.getAll).toHaveBeenCalledTimes(1)
       expect(didRepository.save).toHaveBeenCalledTimes(1)
