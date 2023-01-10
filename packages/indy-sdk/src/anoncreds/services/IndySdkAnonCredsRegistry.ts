f5cca638ff461ead01d59a9bf20229c519810686
Timo Glastra <timo@animo.id>
2023-01-10 22:37:31 +0800

    feat(anoncreds): add anoncreds registry service
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/indy-sdk/src/anoncreds/services/IndySdkAnonCredsRegistry.ts
+++ packages/indy-sdk/src/anoncreds/services/IndySdkAnonCredsRegistry.ts
@@ -1,3 +1,4 @@
+import type { IndySdk } from '../../types'
 import type {
   AnonCredsRegistry,
   GetCredentialDefinitionReturn,
@@ -12,17 +13,19 @@ import type {
 import type { AgentContext } from '@aries-framework/core'
 import type { Schema as IndySdkSchema } from 'indy-sdk'
 
-import { inject } from '@aries-framework/core'
-
 import { IndySdkError, isIndyError } from '../../error'
 import { IndySdkPoolService } from '../../ledger'
-import { IndySdk, IndySdkSymbol } from '../../types'
+import { IndySdkSymbol } from '../../types'
 import {
   didFromCredentialDefinitionId,
   didFromRevocationRegistryDefinitionId,
   didFromSchemaId,
   getLegacyCredentialDefinitionId,
   getLegacySchemaId,
+  legacyIndyCredentialDefinitionIdRegex,
+  legacyIndyIssuerIdRegex,
+  legacyIndyRevocationRegistryIdRegex,
+  legacyIndySchemaIdRegex,
 } from '../utils/identifiers'
 import {
   anonCredsRevocationListFromIndySdk,
@@ -33,32 +36,39 @@ import {
  * TODO: validation of the identifiers. The Indy SDK classes only support the legacy (unqualified) identifiers.
  */
 export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
-  private indySdk: IndySdk
-  private indySdkPoolService: IndySdkPoolService
-
-  public constructor(@inject(IndySdkSymbol) indySdk: IndySdk, indySdkPoolService: IndySdkPoolService) {
-    this.indySdk = indySdk
-    this.indySdkPoolService = indySdkPoolService
-  }
+  /**
+   * This class only supports resoling and registering objects with legacy indy identifiers.
+   * It needs to include support for the schema, credential definition, revocation registry as well
+   * as the issuer id (which is needed when registering objects).
+   */
+  public readonly supportedMethods = [
+    legacyIndyIssuerIdRegex,
+    legacyIndySchemaIdRegex,
+    legacyIndyCredentialDefinitionIdRegex,
+    legacyIndyRevocationRegistryIdRegex,
+  ]
 
   public async getSchema(agentContext: AgentContext, schemaId: string): Promise<GetSchemaReturn> {
     try {
+      const indySdkPoolService = agentContext.dependencyManager.resolve(IndySdkPoolService)
+      const indySdk = agentContext.dependencyManager.resolve<IndySdk>(IndySdkSymbol)
+
       const did = didFromSchemaId(schemaId)
-      const { pool } = await this.indySdkPoolService.getPoolForDid(agentContext, did)
+      const { pool } = await indySdkPoolService.getPoolForDid(agentContext, did)
       agentContext.config.logger.debug(`Getting schema '${schemaId}' from ledger '${pool.didIndyNamespace}'`)
 
-      const request = await this.indySdk.buildGetSchemaRequest(null, schemaId)
+      const request = await indySdk.buildGetSchemaRequest(null, schemaId)
 
       agentContext.config.logger.trace(
         `Submitting get schema request for schema '${schemaId}' to ledger '${pool.didIndyNamespace}'`
       )
-      const response = await this.indySdkPoolService.submitReadRequest(pool, request)
+      const response = await indySdkPoolService.submitReadRequest(pool, request)
 
       agentContext.config.logger.trace(`Got un-parsed schema '${schemaId}' from ledger '${pool.didIndyNamespace}'`, {
         response,
       })
 
-      const [, schema] = await this.indySdk.parseGetSchemaResponse(response)
+      const [, schema] = await indySdk.parseGetSchemaResponse(response)
       agentContext.config.logger.debug(`Got schema '${schemaId}' from ledger '${pool.didIndyNamespace}'`, {
         schema,
       })
@@ -117,7 +127,10 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
     }
 
     try {
-      const pool = this.indySdkPoolService.getPoolForNamespace(options.options.didIndyNamespace)
+      const indySdkPoolService = agentContext.dependencyManager.resolve(IndySdkPoolService)
+      const indySdk = agentContext.dependencyManager.resolve<IndySdk>(IndySdkSymbol)
+
+      const pool = indySdkPoolService.getPoolForNamespace(options.options.didIndyNamespace)
       agentContext.config.logger.debug(
         `Register schema on ledger '${pool.didIndyNamespace}' with did '${options.schema.issuerId}'`,
         options.schema
@@ -133,14 +146,9 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
         // buildSchemaRequest (seqNo is not yet known)
       } as IndySdkSchema
 
-      const request = await this.indySdk.buildSchemaRequest(options.schema.issuerId, schema)
+      const request = await indySdk.buildSchemaRequest(options.schema.issuerId, schema)
 
-      const response = await this.indySdkPoolService.submitWriteRequest(
-        agentContext,
-        pool,
-        request,
-        options.schema.issuerId
-      )
+      const response = await indySdkPoolService.submitWriteRequest(agentContext, pool, request, options.schema.issuerId)
       agentContext.config.logger.debug(`Registered schema '${schema.id}' on ledger '${pool.didIndyNamespace}'`, {
         response,
         schema,
@@ -189,19 +197,22 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
     credentialDefinitionId: string
   ): Promise<GetCredentialDefinitionReturn> {
     try {
+      const indySdkPoolService = agentContext.dependencyManager.resolve(IndySdkPoolService)
+      const indySdk = agentContext.dependencyManager.resolve<IndySdk>(IndySdkSymbol)
+
       const did = didFromCredentialDefinitionId(credentialDefinitionId)
-      const { pool } = await this.indySdkPoolService.getPoolForDid(agentContext, did)
+      const { pool } = await indySdkPoolService.getPoolForDid(agentContext, did)
 
       agentContext.config.logger.debug(
         `Using ledger '${pool.didIndyNamespace}' to retrieve credential definition '${credentialDefinitionId}'`
       )
-      const request = await this.indySdk.buildGetCredDefRequest(null, credentialDefinitionId)
+      const request = await indySdk.buildGetCredDefRequest(null, credentialDefinitionId)
 
       agentContext.config.logger.trace(
         `Submitting get credential definition request for credential definition '${credentialDefinitionId}' to ledger '${pool.didIndyNamespace}'`
       )
 
-      const response = await this.indySdkPoolService.submitReadRequest(pool, request)
+      const response = await indySdkPoolService.submitReadRequest(pool, request)
       agentContext.config.logger.trace(
         `Got un-parsed credential definition '${credentialDefinitionId}' from ledger '${pool.didIndyNamespace}'`,
         {
@@ -209,7 +220,7 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
         }
       )
 
-      const [, credentialDefinition] = await this.indySdk.parseGetCredDefResponse(response)
+      const [, credentialDefinition] = await indySdk.parseGetCredDefResponse(response)
       agentContext.config.logger.debug(
         `Got credential definition '${credentialDefinitionId}' from ledger '${pool.didIndyNamespace}'`,
         {
@@ -267,7 +278,10 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
     }
 
     try {
-      const pool = this.indySdkPoolService.getPoolForNamespace(options.options.didIndyNamespace)
+      const indySdkPoolService = agentContext.dependencyManager.resolve(IndySdkPoolService)
+      const indySdk = agentContext.dependencyManager.resolve<IndySdk>(IndySdkSymbol)
+
+      const pool = indySdkPoolService.getPoolForNamespace(options.options.didIndyNamespace)
       agentContext.config.logger.debug(
         `Registering credential definition on ledger '${pool.didIndyNamespace}' with did '${options.credentialDefinition.issuerId}'`,
         options.credentialDefinition
@@ -296,7 +310,7 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
         options.credentialDefinition.tag
       )
 
-      const request = await this.indySdk.buildCredDefRequest(options.credentialDefinition.issuerId, {
+      const request = await indySdk.buildCredDefRequest(options.credentialDefinition.issuerId, {
         id: credentialDefinitionId,
         schemaId: options.credentialDefinition.schemaId,
         tag: options.credentialDefinition.tag,
@@ -305,7 +319,7 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
         ver: '1.0',
       })
 
-      const response = await this.indySdkPoolService.submitWriteRequest(
+      const response = await indySdkPoolService.submitWriteRequest(
         agentContext,
         pool,
         request,
@@ -350,18 +364,21 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
     revocationRegistryDefinitionId: string
   ): Promise<GetRevocationRegistryDefinitionReturn> {
     try {
+      const indySdkPoolService = agentContext.dependencyManager.resolve(IndySdkPoolService)
+      const indySdk = agentContext.dependencyManager.resolve<IndySdk>(IndySdkSymbol)
+
       const did = didFromRevocationRegistryDefinitionId(revocationRegistryDefinitionId)
-      const { pool } = await this.indySdkPoolService.getPoolForDid(agentContext, did)
+      const { pool } = await indySdkPoolService.getPoolForDid(agentContext, did)
 
       agentContext.config.logger.debug(
         `Using ledger '${pool.didIndyNamespace}' to retrieve revocation registry definition '${revocationRegistryDefinitionId}'`
       )
-      const request = await this.indySdk.buildGetRevocRegDefRequest(null, revocationRegistryDefinitionId)
+      const request = await indySdk.buildGetRevocRegDefRequest(null, revocationRegistryDefinitionId)
 
       agentContext.config.logger.trace(
         `Submitting get revocation registry definition request for revocation registry definition '${revocationRegistryDefinitionId}' to ledger`
       )
-      const response = await this.indySdkPoolService.submitReadRequest(pool, request)
+      const response = await indySdkPoolService.submitReadRequest(pool, request)
       agentContext.config.logger.trace(
         `Got un-parsed revocation registry definition '${revocationRegistryDefinitionId}' from ledger '${pool.didIndyNamespace}'`,
         {
@@ -369,7 +386,7 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
         }
       )
 
-      const [, revocationRegistryDefinition] = await this.indySdk.parseGetRevocRegDefResponse(response)
+      const [, revocationRegistryDefinition] = await indySdk.parseGetRevocRegDefResponse(response)
 
       agentContext.config.logger.debug(
         `Got revocation registry definition '${revocationRegistryDefinitionId}' from ledger`,
@@ -415,21 +432,24 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
     timestamp: number
   ): Promise<GetRevocationListReturn> {
     try {
+      const indySdkPoolService = agentContext.dependencyManager.resolve(IndySdkPoolService)
+      const indySdk = agentContext.dependencyManager.resolve<IndySdk>(IndySdkSymbol)
+
       const did = didFromRevocationRegistryDefinitionId(revocationRegistryId)
-      const { pool } = await this.indySdkPoolService.getPoolForDid(agentContext, did)
+      const { pool } = await indySdkPoolService.getPoolForDid(agentContext, did)
 
       agentContext.config.logger.debug(
         `Using ledger '${pool.id}' to retrieve revocation registry deltas with revocation registry definition id '${revocationRegistryId}' until ${timestamp}`
       )
 
       // TODO: implement caching for returned deltas
-      const request = await this.indySdk.buildGetRevocRegDeltaRequest(null, revocationRegistryId, 0, timestamp)
+      const request = await indySdk.buildGetRevocRegDeltaRequest(null, revocationRegistryId, 0, timestamp)
 
       agentContext.config.logger.trace(
         `Submitting get revocation registry delta request for revocation registry '${revocationRegistryId}' to ledger`
       )
 
-      const response = await this.indySdkPoolService.submitReadRequest(pool, request)
+      const response = await indySdkPoolService.submitReadRequest(pool, request)
       agentContext.config.logger.trace(
         `Got revocation registry delta unparsed-response '${revocationRegistryId}' from ledger`,
         {
@@ -437,7 +457,7 @@ export class IndySdkAnonCredsRegistry implements AnonCredsRegistry {
         }
       )
 
-      const [, revocationRegistryDelta, deltaTimestamp] = await this.indySdk.parseGetRevocRegDeltaResponse(response)
+      const [, revocationRegistryDelta, deltaTimestamp] = await indySdk.parseGetRevocRegDeltaResponse(response)
 
       agentContext.config.logger.debug(
         `Got revocation registry deltas '${revocationRegistryId}' until timestamp ${timestamp} from ledger`,
