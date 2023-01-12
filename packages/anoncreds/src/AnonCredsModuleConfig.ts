86647e7f55c9a362f6ab500538c4de2112e42206
Timo Glastra <timo@animo.id>
2023-01-12 19:05:19 +0800

    feat(anoncreds): add anoncreds registry service (#1204)
    Signed-off-by: Timo Glastra <timo@animo.id>

---
+++ packages/anoncreds/src/AnonCredsModuleConfig.ts
@@ -0,0 +1,28 @@
+import type { AnonCredsRegistry } from './services'
+
+/**
+ * @public
+ * AnonCredsModuleConfigOptions defines the interface for the options of the AnonCredsModuleConfig class.
+ */
+export interface AnonCredsModuleConfigOptions {
+  /**
+   * A list of AnonCreds registries to make available to the AnonCreds module.
+   */
+  registries: [AnonCredsRegistry, ...AnonCredsRegistry[]]
+}
+
+/**
+ * @public
+ */
+export class AnonCredsModuleConfig {
+  private options: AnonCredsModuleConfigOptions
+
+  public constructor(options: AnonCredsModuleConfigOptions) {
+    this.options = options
+  }
+
+  /** See {@link AnonCredsModuleConfigOptions.registries} */
+  public get registries() {
+    return this.options.registries
+  }
+}
