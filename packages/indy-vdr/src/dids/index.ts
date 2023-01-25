e113e1e11226c306939de13a9288fed7d7fad344
Ariel Gentile <gentilester@gmail.com>
2023-01-25 07:32:34 -0300

    import initial sov resolver
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

---
+++ packages/indy-vdr/src/dids/index.ts
@@ -0,0 +1,7 @@
+export {
+  IndyVdrSovDidRegistrar,
+  IndyVdrSovDidCreateOptions,
+  IndyVdrSovDidDeactivateOptions,
+  IndyVdrSovDidUpdateOptions,
+} from './IndyVdrSovDidRegistrar'
+export { IndySdkSovDidResolver } from './IndyVdrSovDidResolver'
