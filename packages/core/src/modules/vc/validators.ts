9f20a68fa01913cb67c61884729e10dff73b4b00
Karim Stekelenburg <karim@animo.id>
2023-01-28 21:28:29 +0100

    fix: improve JSON-LD context validator
    Co-authored-by: Timo Glastra <timo@animo.id>
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- packages/core/src/modules/vc/validators.ts
+++ packages/core/src/modules/vc/validators.ts
@@ -13,14 +13,7 @@ export function IsJsonLdContext(validationOptions?: ValidationOptions): Property
           // If value is an array, check if all items are strings, are URLs and that
           // the first entry is a verifiable credential context
           if (Array.isArray(value)) {
-            if (value.every((v) => isString(v) && isURL(v)) && value[0] === CREDENTIALS_CONTEXT_V1_URL) {
-              return true
-            }
-
-            if (value.every((v) => isString(v) || typeof v === 'object')) {
-              // TODO add more validation on the properties of the JSON object
-              return true
-            }
+            return value.every((v) => (isString(v) && isURL(v)) || isJsonObject(v)) && value[0] === CREDENTIALS_CONTEXT_V1_URL)
           }
 
           // If value is not an array, check if it is an object (assuming it's a JSON-LD context definition)
