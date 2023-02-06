a683599f459e426910c4bbf816ffc1fedcac5d4f
Ariel Gentile <gentilester@gmail.com>
2023-02-06 18:03:19 -0300

    remove temp files
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/askar/postgres.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-docker container stop postgres
-docker container rm postgres
-docker run --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
--- stop-ledger.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-docker container stop indy-pool
-docker container rm indy-pool
