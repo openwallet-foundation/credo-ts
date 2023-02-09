c8bf9f2d301621a7c41c1de968d8974b9fdd8172
Pritam Singh <pkspritam16@gmail.com>
2023-02-09 12:59:55 +0530

    fix: add processMessagesConcurrently config for processing message concurrently
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -83,6 +83,7 @@ export interface InitConfig {
   connectionImageUrl?: string
   autoUpdateStorageOnStartup?: boolean
   backupBeforeStorageUpdate?: boolean
+  processMessagesConcurrently?: boolean
 }
 
 export type ProtocolVersion = `${number}.${number}`
