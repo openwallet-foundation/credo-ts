1925ece268a0b1749636dfa145a99f3b2bb80f69
Ariel Gentile <gentilester@gmail.com>
2023-02-10 13:04:56 -0300

    test: throw error when import fails
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/anoncreds-rs/tests/setup.ts
+++ packages/anoncreds-rs/tests/setup.ts
@@ -1,6 +1,3 @@
-try {
-  // eslint-disable-next-line import/no-extraneous-dependencies
-  require('@hyperledger/anoncreds-nodejs')
-} catch (error) {
-  throw new Error('Could not load anoncreds bindings')
-}
+import '@hyperledger/anoncreds-nodejs'
+
+jest.setTimeout(60000)
