b570e0f923fc46adef3ce20ee76a683a867b85f4
Karim Stekelenburg <karim@animo.id>
2023-02-13 19:49:21 +0100

    fix(indy-vdr): export relevant packages from root (#1291)
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- packages/indy-vdr/src/index.ts
+++ packages/indy-vdr/src/index.ts
@@ -1,8 +1,4 @@
 export { IndyVdrSovDidResolver } from './dids'
-
-try {
-  // eslint-disable-next-line import/no-extraneous-dependencies
-  require('@hyperledger/indy-vdr-nodejs')
-} catch (error) {
-  throw new Error('Error registering nodejs bindings for Indy VDR')
-}
+export * from './IndyVdrModule'
+export * from './IndyVdrModuleConfig'
+export * from './anoncreds'
