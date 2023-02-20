4dc49cc12643ea37d200c1cacb4f7ddd9d8bd8e9
Victor Anene <victor@animo.id>
2023-02-20 17:08:45 +0100

    feat: IndyVdrAnonCredsRegistry revocation methods Work Funded by the Government of Ontario
    Signed-off-by: Victor Anene <victor@animo.id>

---
+++ packages/indy-vdr/src/anoncreds/utils/transform.ts
@@ -0,0 +1,42 @@
+import type { AnonCredsRevocationStatusList, AnonCredsRevocationRegistryDefinition } from '@aries-framework/anoncreds'
+
+export function anonCredsRevocationStatusListFromIndyVdr(
+  revocationRegistryDefinitionId: string,
+  revocationRegistryDefinition: AnonCredsRevocationRegistryDefinition,
+  delta: RevocRegDelta,
+  timestamp: number,
+  isIssuanceByDefault: boolean
+): AnonCredsRevocationStatusList {
+  // 0 means unrevoked, 1 means revoked
+  const defaultState = isIssuanceByDefault ? 0 : 1
+
+  // Fill with default value
+  const revocationList = new Array(revocationRegistryDefinition.value.maxCredNum).fill(defaultState)
+
+  // Set all `issuer` indexes to 0 (not revoked)
+  for (const issued of delta.value.issued ?? []) {
+    revocationList[issued] = 0
+  }
+
+  // Set all `revoked` indexes to 1 (revoked)
+  for (const revoked of delta.value.revoked ?? []) {
+    revocationList[revoked] = 1
+  }
+
+  return {
+    issuerId: revocationRegistryDefinition.issuerId,
+    currentAccumulator: delta.value.accum,
+    revRegId: revocationRegistryDefinitionId,
+    revocationList,
+    timestamp,
+  }
+}
+
+interface RevocRegDelta {
+  value: {
+    accum: string
+    issued: number[]
+    revoked: number[]
+  }
+  ver: string
+}
