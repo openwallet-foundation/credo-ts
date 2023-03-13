66578f3975e68804c052471c7f70d19b3fa147e2
blu3beri <blu3beri@proton.me>
2023-03-13 16:29:58 +0100

    feat: working backups
    Signed-off-by: blu3beri <blu3beri@proton.me>

--- packages/indy-sdk-to-askar-migration/src/IndySdkToAskarMigrationUpdater.ts
+++ packages/indy-sdk-to-askar-migration/src/IndySdkToAskarMigrationUpdater.ts
@@ -3,58 +3,226 @@ import type { Agent, FileSystem } from '@aries-framework/core'
 import type { EntryObject } from '@hyperledger/aries-askar-shared'
 
 import { AnonCredsCredentialRecord, AnonCredsLinkSecretRecord } from '@aries-framework/anoncreds'
+import { AskarWallet } from '@aries-framework/askar'
 import { InjectionSymbols, KeyDerivationMethod, JsonTransformer, TypedArrayEncoder } from '@aries-framework/core'
 import { Key, KeyAlgs, Store, StoreKeyMethod } from '@hyperledger/aries-askar-shared'
 
 import { IndySdkToAskarMigrationError } from './errors/IndySdkToAskarMigrationError'
 import { transformFromRecordTagValues } from './utils'
 
+/**
+ *
+ * Migration class to move a wallet form the indy-sdk structure to the new
+ * askar wallet structure.
+ *
+ * Right now, this is ONLY supported within React Native environments AND only sqlite.
+ *
+ * The reason it only works within React Native is that we ONLY update the
+ * keys, masterSecret and credentials for now. If you have an agent in Node.JS
+ * where it only contains these records, it may be used but we cannot
+ * guarantee a successful migration.
+ *
+ */
 export class IndySdkToAskarMigrationUpdater {
   private store: Store
-  private walletName: string
+  private walletId: string
   private defaultLinkSecretId: string
   private agent: Agent
+  private dbPath: string
+  private fs: FileSystem
+  private deleteOnFinish: boolean
 
-  private constructor(store: Store, walletName: string, agent: Agent, defaultLinkSecretId?: string) {
+  private constructor(
+    store: Store,
+    walletId: string,
+    agent: Agent,
+    dbPath: string,
+    deleteOnFinish = false,
+    defaultLinkSecretId?: string
+  ) {
     this.store = store
-    this.walletName = walletName
+    this.walletId = walletId
+    this.dbPath = dbPath
     this.agent = agent
-    this.defaultLinkSecretId = defaultLinkSecretId ?? walletName
+    this.fs = this.agent.dependencyManager.resolve<FileSystem>(InjectionSymbols.FileSystem)
+    this.defaultLinkSecretId = defaultLinkSecretId ?? walletId
+    this.deleteOnFinish = deleteOnFinish
   }
 
-  public static async initialize({ uri, agent }: { uri: string; agent: Agent }) {
+  public static async initialize({
+    dbPath,
+    agent,
+    deleteOnFinish,
+    defaultLinkSecretId,
+  }: {
+    dbPath: string
+    agent: Agent
+    deleteOnFinish?: boolean
+    defaultLinkSecretId?: string
+  }) {
     const {
       config: { walletConfig },
     } = agent
     if (!walletConfig) throw new IndySdkToAskarMigrationError('Wallet config is required for updating the wallet')
 
+    if (agent.isInitialized)
+      throw new IndySdkToAskarMigrationError('Wallet migration can not be done on an initialized agent')
+
+    if (!(agent.dependencyManager.resolve(InjectionSymbols.Wallet) instanceof AskarWallet)) {
+      throw new IndySdkToAskarMigrationError("Wallet on the agent must be of instance 'AskarWallet'")
+    }
+
     const keyMethod =
       walletConfig.keyDerivationMethod == KeyDerivationMethod.Raw ? StoreKeyMethod.Raw : StoreKeyMethod.Kdf
-    const store = await Store.open({ uri, passKey: walletConfig.key, keyMethod })
-    return new IndySdkToAskarMigrationUpdater(store, walletConfig.id, agent)
+    const store = await Store.open({ uri: `sqlite://${dbPath}`, passKey: walletConfig.key, keyMethod })
+    return new IndySdkToAskarMigrationUpdater(
+      store,
+      walletConfig.id,
+      agent,
+      dbPath,
+      deleteOnFinish,
+      defaultLinkSecretId
+    )
   }
 
+  /*
+   * Checks whether the destination locations are allready used. This might
+   * happen if you want to migrate a wallet when you already have a new wallet
+   * with the same id.
+   */
+  private async assertDestinationsAreFree() {
+    const areAllDestinationsTaken =
+      (await this.fs.exists(this.backupFile)) || (await this.fs.exists(this.newWalletPath))
+
+    if (areAllDestinationsTaken) {
+      throw new IndySdkToAskarMigrationError(
+        `Files already exist at paths that will be used for backing up. Please remove them manually. Backup path: '${this.backupFile}' and new wallet path: ${this.newWalletPath} `
+      )
+    }
+  }
+
+  /**
+   * Location of the new wallet
+   */
+  private get newWalletPath() {
+    return `${this.fs.dataPath}/wallet/${this.walletId}/sqlite.db`
+  }
+
+  /**
+   * Temporary backup location of the pre-migrated script
+   */
+  private get backupFile() {
+    return `${this.fs.tempPath}/${this.walletId}.bak.db`
+  }
+
+  /**
+   * Backup the database file. This function makes sure that the the indy-sdk
+   * database file is backed up within our temporary directory path. If some
+   * error occurs, `this.revertDatbase()` will be called to revert the backup.
+   */
   private async backupDatabase() {
-    const fs = this.agent.dependencyManager.resolve<FileSystem>(InjectionSymbols.FileSystem)
+    const src = this.dbPath
+    const dest = this.backupFile
+    this.agent.config.logger?.trace(`Creating backup from '${src}' to '${dest}'`)
+
+    // Create the directories for the backup
+    await this.fs.createDirectory(dest)
+
+    // Copy the supplied database to the backup destination
+    await this.fs.copyFile(src, dest)
+
+    if (!(await this.fs.exists(dest))) {
+      throw new IndySdkToAskarMigrationError('Could not locate the new backup file')
+    }
   }
 
+  /**
+   * Reverts backed up database file to the original path, if its missing, and
+   * deletes the backup. We do some additional, possible redundant, exists checks
+   * here to be extra sure that only a happy flow occurs.
+   */
   private async revertDatabase() {
-    const fs = this.agent.dependencyManager.resolve<FileSystem>(InjectionSymbols.FileSystem)
+    // "Impossible" state. Since we do not continue if `this.backupDatabase()`
+    // fails, this file should always be there. If this error is thrown, we
+    // cannot correctly revert the state.
+    if (!(await this.fs.exists(this.backupFile))) {
+      throw new IndySdkToAskarMigrationError('Backup file could not be found while trying to revert the state')
+    }
+
+    /**
+     * Since we used `copy` to get the file, it should still be there. We
+     * double-check here to be sure.
+     */
+    if (!(await this.fs.exists(this.dbPath))) {
+      return
+    } else {
+      this.agent.config.logger?.trace(`Moving '${this.backupFile}' back to the original path: '${this.dbPath}`)
+
+      // Move the backedup file back to the original path
+      await this.fs.copyFile(this.backupFile, this.dbPath)
+
+      this.agent.config.logger?.trace(`Cleaned up the backed up file at '${this.backupFile}'`)
+    }
   }
 
+  // Delete the backup as `this.fs.copyFile` only copies and no deletion
+  // Since we use `tempPath` which is cleared when certain events happen,
+  // e.g. cron-job and system restart (depending on the os) we could omit
+  // this call `await this.fs.delete(this.backupFile)`.
   private async cleanBackup() {
-    const fs = this.agent.dependencyManager.resolve<FileSystem>(InjectionSymbols.FileSystem)
+    this.agent.config.logger?.trace(`Deleting the backup file at '${this.backupFile}'`)
+    await this.fs.delete(this.backupFile)
   }
 
+  /**
+   * Move the migrated and updated database file to the new location according
+   * to the `FileSystem.dataPath`.
+   */
+  private async moveToNewLocation() {
+    const src = this.dbPath
+    // New path for the database
+    const dest = this.newWalletPath
+
+    // create the wallet directory
+    await this.fs.createDirectory(dest)
+
+    this.agent.config.logger?.trace(`Moving upgraded database from ${src} to ${dest}`)
+
+    // Copy the file from the database path to the new location
+    await this.fs.copyFile(src, dest)
+
+    // Delete the original, only if specified by the user
+    if (this.deleteOnFinish) await this.fs.delete(this.dbPath)
+  }
+
+  /**
+   * Function that updates the values from an indy-sdk structure to the new askar stucture.
+   *
+   * - Assert that the paths that will be used are free
+   * - Create a backup of the database
+   * - Update the Keys
+   * - Update the Master Secret (Link Sercet)
+   * - Update the credentials
+   * If any of those failed:
+   *   - Revert the database
+   * - Clear the backup from the temporary directory
+   */
   public async update() {
+    await this.assertDestinationsAreFree()
+
+    await this.backupDatabase()
     try {
       await this.updateKeys()
       await this.updateMasterSecret()
       await this.updateCredentials()
-    } catch (e) {
+
+      await this.moveToNewLocation()
+    } catch (cause) {
       this.agent.config.logger?.error('Migration failed. Reverting state.')
 
       await this.revertDatabase()
+
+      throw new IndySdkToAskarMigrationError('Migration failed. State has been reverted.', { cause })
     } finally {
       await this.cleanBackup()
     }
@@ -77,7 +245,7 @@ export class IndySdkToAskarMigrationUpdater {
 
       for (const row of keys) {
         await txn.remove({ category, name: row.name })
-        const signKey: string = (typeof row.value === 'string' ? JSON.parse(row.value) : row.value).signkey
+        const signKey: string = JSON.parse(row.value as string).signkey
         const keySk = TypedArrayEncoder.fromBase58(signKey)
         const key = Key.fromSecretBytes({
           algorithm: KeyAlgs.Ed25519,
@@ -111,11 +279,11 @@ export class IndySdkToAskarMigrationUpdater {
       }
 
       if (!masterSecrets.some((ms: EntryObject) => ms.name === this.defaultLinkSecretId)) {
-        throw new IndySdkToAskarMigrationError('defaultLinkSecretId can not be established')
+        throw new IndySdkToAskarMigrationError('defaultLinkSecretId can not be established.')
       }
 
       for (const row of masterSecrets) {
-        const isDefault = masterSecrets.length === 0 ?? row.name === this.walletName
+        const isDefault = masterSecrets.length === 0 ?? row.name === this.walletId
         await txn.remove({ category, name: row.name })
 
         const {
--- packages/indy-sdk-to-askar-migration/tests/migrate.test.ts
+++ packages/indy-sdk-to-askar-migration/tests/migrate.test.ts
@@ -1,28 +1,23 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import type { InitConfig, WalletConfig } from '@aries-framework/core'
-
 import { AskarModule } from '@aries-framework/askar'
-import { utils, ConsoleLogger, LogLevel, KeyDerivationMethod, Agent } from '@aries-framework/core'
+import { ConsoleLogger, LogLevel, utils, KeyDerivationMethod, Agent } from '@aries-framework/core'
 import { IndySdkModule } from '@aries-framework/indy-sdk'
 import { agentDependencies } from '@aries-framework/node'
 import { ariesAskar, Migration } from '@hyperledger/aries-askar-nodejs'
 import { registerAriesAskar } from '@hyperledger/aries-askar-shared'
-import fs from 'fs'
 import indy from 'indy-sdk'
 import { homedir } from 'os'
-import path from 'path'
 
 import { IndySdkToAskarMigrationUpdater } from '../src'
 
 describe('Migrate', () => {
-  const config: InitConfig = {
+  const config = {
     label: 'test-agent',
     walletConfig: {
       id: `walletwallet.0-${utils.uuid()}`,
       key: 'GfwU1DC7gEZNs3w41tjBiZYj7BNToDoFEqKY6wZXqs1A',
       keyDerivationMethod: KeyDerivationMethod.Raw,
     },
-    // logger: new ConsoleLogger(LogLevel.trace),
+    logger: new ConsoleLogger(LogLevel.trace),
   }
 
   const oldAgent = new Agent({
@@ -38,7 +33,6 @@ describe('Migrate', () => {
   })
 
   const oldDbPath = `${homedir()}/.indy_client/wallet/${oldAgent.config.walletConfig?.id}/sqlite.db`
-  const newDbPath = `${homedir()}/.afj/data/wallet/${newAgent.config.walletConfig?.id}/sqlite.db`
 
   beforeAll(() => {
     registerAriesAskar({ askar: ariesAskar })
@@ -46,40 +40,27 @@ describe('Migrate', () => {
 
   // TODO: update with an aca-py issued revokable credential
   // community agent MIGHT have revocrevoc
-  // TODO: should take uninitialized agent
   test('indy-sdk sqlite to aries-askar sqlite', async () => {
-    const genericRecord = { foo: 'bar' }
+    const genericRecordContent = { foo: 'bar' }
 
     await oldAgent.initialize()
 
-    const record = await oldAgent.genericRecords.save({ content: genericRecord })
-
-    const walletConfig: WalletConfig = {
-      key: oldAgent.config.walletConfig!.key,
-      id: oldAgent.config.walletConfig!.id,
-      keyDerivationMethod: oldAgent.config.walletConfig!.keyDerivationMethod,
-    }
+    const record = await oldAgent.genericRecords.save({ content: genericRecordContent })
 
     await oldAgent.shutdown()
 
     await Migration.migrate({
-      walletName: walletConfig.id,
-      walletKey: walletConfig.key,
-      kdfLevel: walletConfig.keyDerivationMethod?.toString() ?? 'ARGON2I_MOD',
+      walletName: config.walletConfig.id,
+      walletKey: config.walletConfig.key,
+      kdfLevel: config.walletConfig.keyDerivationMethod,
       specUri: oldDbPath,
     })
 
-    const updater = await IndySdkToAskarMigrationUpdater.initialize({ uri: `sqlite://${oldDbPath}`, agent: oldAgent })
+    const updater = await IndySdkToAskarMigrationUpdater.initialize({ dbPath: oldDbPath, agent: newAgent })
     await updater.update()
 
-    fs.mkdirSync(path.dirname(newDbPath), { recursive: true })
-
     await newAgent.initialize()
-    // await newAgent.wallet.import(oldAgent.config.walletConfig!, {
-    //   path: oldDbPath,
-    //   key: oldAgent.config.walletConfig!.key,
-    // })
 
-    await expect(newAgent.genericRecords.findById(record.id)).resolves.toMatchObject(genericRecord)
+    await expect(newAgent.genericRecords.findById(record.id)).resolves.toMatchObject({ content: genericRecordContent })
   })
 })
