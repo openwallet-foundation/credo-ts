5bc18855a68b4f73858adf0f295f8e7211168bae
Karim Stekelenburg <karim@animo.id>
2023-03-31 15:53:20 +0200

    test(vc): add tests for W3cVcApi
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- packages/core/src/modules/vc/__tests__/W3cVcApi.test.ts
+++ packages/core/src/modules/vc/__tests__/W3cVcApi.test.ts
@@ -1,9 +1,14 @@
+import type { AgentContext } from '../../../agent'
+
 import { IndySdkModule } from '../../../../../indy-sdk/src'
 import { getAgentOptions, indySdk } from '../../../../tests'
 import { Agent } from '../../../agent/Agent'
+import { AgentConfig } from '../../../agent/AgentConfig'
 import { JsonTransformer } from '../../../utils'
+import { W3cCredentialService } from '../W3cCredentialService'
 import { W3cVcModule } from '../W3cVcModule'
 import { W3cVerifiableCredential } from '../models'
+import { W3cCredentialRepository } from '../repository'
 
 import { customDocumentLoader } from './documentLoader'
 import { Ed25519Signature2018Fixtures } from './fixtures'
@@ -21,29 +26,74 @@ const agentOptions = getAgentOptions<typeof modules>('W3cVcApi', {}, modules)
 
 const agent = new Agent(agentOptions)
 
+let w3cCredentialRepository: W3cCredentialRepository
+let w3cCredentialService: W3cCredentialService
+
+const testCredential = JsonTransformer.fromJSON(
+  Ed25519Signature2018Fixtures.TEST_LD_DOCUMENT_SIGNED,
+  W3cVerifiableCredential
+)
+
 describe('W3cVcApi', () => {
-  beforeAll(async () => {
+  beforeAll(() => {
+    w3cCredentialRepository = agent.dependencyManager.resolve(W3cCredentialRepository)
+    w3cCredentialService = agent.dependencyManager.resolve(W3cCredentialService)
+  })
+
+  beforeEach(async () => {
     await agent.initialize()
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     await agent.shutdown()
     await agent.wallet.delete()
   })
 
   it('Should successfully store a credential', async () => {
-    const credential = JsonTransformer.fromJSON(
-      Ed25519Signature2018Fixtures.TEST_LD_DOCUMENT_SIGNED,
-      W3cVerifiableCredential
-    )
+    const repoSpy = jest.spyOn(w3cCredentialRepository, 'save')
+    const serviceSpy = jest.spyOn(w3cCredentialService, 'storeCredential')
 
-    const storedCredential = await agent.modules.w3cVc.storeCredential({
-      credential,
+    await agent.modules.w3cVc.storeCredential({
+      credential: testCredential,
     })
 
-    const retrievedCredentials = await agent.modules.w3cVc.getAllCredentialRecords()
+    expect(repoSpy).toHaveBeenCalledTimes(1)
+    expect(serviceSpy).toHaveBeenCalledTimes(1)
+  })
 
-    expect(retrievedCredentials.length).toBe(1)
-    expect(retrievedCredentials[0].id).toStrictEqual(storedCredential.id)
+  it('Should successfully retrieve a credential by id', async () => {
+    const repoSpy = jest.spyOn(w3cCredentialRepository, 'getById')
+    const serviceSpy = jest.spyOn(w3cCredentialService, 'getCredentialRecordById')
+
+    const storedCredential = await agent.modules.w3cVc.storeCredential({
+      credential: testCredential,
+    })
+
+    const retrievedCredential = await agent.modules.w3cVc.getCredentialRecordById(storedCredential.id)
+    expect(storedCredential.id).toEqual(retrievedCredential.id)
+
+    expect(repoSpy).toHaveBeenCalledTimes(1)
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(repoSpy).toHaveBeenCalledWith((agent as any).agentContext, storedCredential.id)
+    expect(serviceSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should successfully remove a credential by id', async () => {
+    const repoSpy = jest.spyOn(w3cCredentialRepository, 'delete')
+    const serviceSpy = jest.spyOn(w3cCredentialService, 'removeCredentialRecord')
+
+    const storedCredential = await agent.modules.w3cVc.storeCredential({
+      credential: testCredential,
+    })
+
+    await agent.modules.w3cVc.removeCredentialRecord(storedCredential.id)
+
+    expect(repoSpy).toHaveBeenCalledTimes(1)
+    expect(serviceSpy).toHaveBeenCalledTimes(1)
+    expect(serviceSpy).toHaveBeenCalledWith((agent as any).agentContext, storedCredential.id)
+
+    const allCredentials = await agent.modules.w3cVc.getAllCredentialRecords()
+    expect(allCredentials).toHaveLength(0)
   })
 })
