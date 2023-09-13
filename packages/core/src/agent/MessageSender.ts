168291441c4064425a71e7fe69426107ad4f0104
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-09-13 21:24:47 +0200

    Fix spelling
    Signed-off-by: Eric Vergnaud <eric.vergnaud@wanadoo.fr>

--- packages/core/src/agent/MessageSender.ts
+++ packages/core/src/agent/MessageSender.ts
@@ -99,7 +99,7 @@ export class MessageSender {
   }
 
   private async sendMessageToSession(agentContext: AgentContext, session: TransportSession, message: AgentMessage) {
-    this.logger.debug(`Packing message end sending it via existing session ${session.type}...`)
+    this.logger.debug(`Packing message and sending it via existing session ${session.type}...`)
     if (!session.keys) {
       throw new AriesFrameworkError(`There are no keys for the given ${session.type} transport session.`)
     }
