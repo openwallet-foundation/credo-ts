ee39749b09186935effaf2fcbfda6274637398ba
ericvergnaud <eric.vergnaud@wanadoo.fr>
2023-09-18 15:03:39 +0200

    Update packages/core/src/modules/message-pìckup/MessagePickupApi.ts
    Co-authored-by: Ariel Gentile <gentilester@gmail.com>
    Signed-off-by: ericvergnaud <eric.vergnaud@wanadoo.fr>

--- packages/core/src/modules/message-pìckup/MessagePickupApi.ts
+++ packages/core/src/modules/message-pìckup/MessagePickupApi.ts
@@ -65,7 +65,7 @@ export class MessagePickupApi<MPPs extends MessagePickupProtocol[] = [V1MessageP
    * @param options: connectionId associated to the message and the encrypted message itself
    */
   public async queueMessage(options: QueueMessageOptions): Promise<QueueMessageReturnType> {
-    this.logger.debug(`Queuing message...`)
+    this.logger.debug('Queuing message...')
     const connectionRecord = await this.connectionService.getById(this.agentContext, options.connectionId)
 
     const messageRepository = this.agentContext.dependencyManager.resolve<MessageRepository>(
