11050afc7965adfa9b00107ba34abfbe3afaf874
ericvergnaud <eric.vergnaud@wanadoo.fr>
2023-09-19 13:31:24 +0200

    fix: log and throw on WebSocket sending errors (#1573)
    Signed-off-by: Eric Vergnaud <eric.vergnaud@wanadoo.fr>

--- packages/core/tests/logger.ts
+++ packages/core/tests/logger.ts
@@ -28,7 +28,7 @@ export class TestLogger extends BaseLogger {
   } as const
 
   // Map our log levels to tslog levels
-  private tsLogLevelNumgerMap = {
+  private tsLogLevelNumberMap = {
     [LogLevel.test]: 0,
     [LogLevel.trace]: 1,
     [LogLevel.debug]: 2,
@@ -48,12 +48,12 @@ export class TestLogger extends BaseLogger {
     if (logger) {
       this.logger = logger.getSubLogger({
         name,
-        minLevel: this.logLevel == LogLevel.off ? undefined : this.tsLogLevelNumgerMap[this.logLevel],
+        minLevel: this.logLevel == LogLevel.off ? undefined : this.tsLogLevelNumberMap[this.logLevel],
       })
     } else {
       this.logger = new Logger({
         name,
-        minLevel: this.logLevel == LogLevel.off ? undefined : this.tsLogLevelNumgerMap[this.logLevel],
+        minLevel: this.logLevel == LogLevel.off ? undefined : this.tsLogLevelNumberMap[this.logLevel],
         attachedTransports: [logToTransport],
       })
     }
