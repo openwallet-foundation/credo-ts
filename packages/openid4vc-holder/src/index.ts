5a3fd6c2494161cbbdb31a7bc865a669a65e163f
Martin Auer <martin.auer97@gmail.com>
2023-11-16 17:43:40 +0100

    fix: export all service options

--- packages/openid4vc-holder/src/index.ts
+++ packages/openid4vc-holder/src/index.ts
@@ -1,14 +1,6 @@
 export * from './OpenId4VcHolderApi'
 export * from './issuance/OpenId4VciHolderModule'
 export * from './issuance/OpenId4VciHolderService'
-// Contains internal types, so we don't export everything
-export {
-  AuthCodeFlowOptions,
-  AcceptCredentialOfferOptions,
-  ProofOfPossessionVerificationMethodResolver,
-  ProofOfPossessionVerificationMethodResolverOptions,
-  RequestCredentialOptions,
-  SupportedCredentialFormats,
-} from './OpenId4VcHolderServiceOptions'
+export * from './OpenId4VcHolderServiceOptions'
 export * from './presentations'
 export { OpenIdCredentialFormatProfile } from './issuance/utils'
