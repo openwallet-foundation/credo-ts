cd0c3ecf908531ae0bcf5b8d07eaf9f391706c7c
Timo Glastra <timo@animo.id>
2023-12-22 13:36:17 +0700

    refactor(oob)!: make label optional
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/utils/parseInvitation.ts
+++ packages/core/src/utils/parseInvitation.ts
@@ -140,9 +140,6 @@ export const parseInvitationShortUrl = async (
 
     // transform into out of band invitation
     const invitation = new OutOfBandInvitation({
-      // The label is currently required by the OutOfBandInvitation class, but not according to the specification.
-      // FIXME: In 0.5.0 we will make this optional: https://github.com/hyperledger/aries-framework-javascript/issues/1524
-      label: '',
       services: [OutOfBandDidCommService.fromResolvedDidCommService(agentMessage.service.resolvedDidCommService)],
     })
 
