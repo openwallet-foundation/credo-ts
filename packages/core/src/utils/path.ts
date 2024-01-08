fd355cd6adf26023e01b1ab1b5aca6d46f296011
Timo Glastra <timo@animo.id>
2024-01-08 13:46:35 +0700

    feat: add joinUriParts method
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/utils/path.ts
+++ packages/core/src/utils/path.ts
@@ -7,3 +7,19 @@
 export function getDirFromFilePath(path: string) {
   return path.substring(0, Math.max(path.lastIndexOf('/'), path.lastIndexOf('\\')))
 }
+
+/**
+ * Combine multiple uri parts into a single uri taking into account slashes.
+ *
+ * @param parts the parts to combine
+ * @returns the combined url
+ */
+export function joinUriParts(parts: string[]) {
+  let combined = ''
+
+  for (const part of parts) {
+    combined += part.endsWith('/') ? part : `${part}/`
+  }
+
+  return combined
+}
