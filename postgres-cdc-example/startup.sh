d83e8a7111526db4d8667d2fc5f588a9ce14bca8
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-01-08 17:58:17 +0100

    use env variable and improve comments
    Signed-off-by: Eric Vergnaud <eric.vergnaud@wanadoo.fr>

--- postgres-cdc-example/startup.sh
+++ postgres-cdc-example/startup.sh
@@ -2,15 +2,15 @@
 
 # run postgres in the background so we can run pg_recvlogical
 postgres -c wal_level=logical -c max_wal_senders=1 -c shared_preload_libraries=wal2json &
-# ensure postgres is fully initialized
+# ensure postgres is fully initialized before running pg_recvlogical
 sleep 10s
-# create replication slot
-pg_recvlogical -d test_db --slot messages_slot --create-slot --if-not-exists -P wal2json
+# create replication slot if required
+pg_recvlogical -d test_db --slot ${CDC_DB_NAME}_slot --create-slot --if-not-exists -P wal2json
 # need a function for publishing otherwise bash shouts
 publish_to_redis() {
   redis-cli -h $REDIS_HOST -p $REDIS_PORT PUBLISH $REDIS_CHANNEL $1
 }
 # listen to replication messages and publish them to redis
-pg_recvlogical -d test_db --slot messages_slot --start -o pretty-print=0 -f - | while read message; do publish_to_redis $message ; done
+pg_recvlogical -d ${CDC_DB_NAME} --slot ${CDC_DB_NAME}_slot --start -o pretty-print=0 -f - | while read message; do publish_to_redis $message ; done
 # uncomment the following if you comment the previous to keep the container running
 # tail -f /dev/null
