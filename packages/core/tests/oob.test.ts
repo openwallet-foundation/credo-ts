c96aec5677aff44e86b735bc8b14d969d8377bcf
Timo Glastra <timo@animo.id>
2024-02-01 18:03:54 +0700

    fix test
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/tests/oob.test.ts
+++ packages/core/tests/oob.test.ts
@@ -687,7 +687,7 @@ describe('out of band', () => {
 
       await expect(aliceAgent.oob.receiveInvitation(outOfBandInvitation, receiveInvitationConfig)).rejects.toEqual(
         new CredoError(
-          `Handshake protocols [${unsupportedProtocol}] are not supported. Supported protocols are [https://didcomm.org/didexchange/1.1,https://didcomm.org/connections/1.0]`
+          `Handshake protocols [${unsupportedProtocol}] are not supported. Supported protocols are [https://didcomm.org/didexchange/1.x,https://didcomm.org/connections/1.x]`
         )
       )
     })
