f666a3bbbc8b162cf37531524b279e36f2d34202
Timo Glastra <timo@animo.id>
2024-02-05 16:26:16 +0700

    fix: updates and bug fixes
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/storage/migration/__tests__/0.4.test.ts
+++ packages/core/src/storage/migration/__tests__/0.4.test.ts
@@ -1,30 +1,35 @@
-import {
-  DependencyManager,
-  InjectionSymbols,
-  Agent,
-  UpdateAssistant,
-  utils,
-  MediatorRoutingRecord,
-} from '@credo-ts/core'
-import { agentDependencies } from '@credo-ts/node'
 import { readFileSync } from 'fs'
 import path from 'path'
 
-import { InMemoryStorageService } from '../../../../../tests/InMemoryStorageService'
-import { RegisteredAskarTestWallet } from '../../../../askar/tests/helpers'
-import { TenantsModule } from '../../TenantsModule'
+import { InMemoryStorageService } from '../../../../../../tests/InMemoryStorageService'
+import { RegisteredAskarTestWallet } from '../../../../../askar/tests/helpers'
+import { agentDependencies } from '../../../../tests/helpers'
+import { Agent } from '../../../agent/Agent'
+import { InjectionSymbols } from '../../../constants'
+import { W3cCredentialsModule } from '../../../modules/vc'
+import { customDocumentLoader } from '../../../modules/vc/data-integrity/__tests__/documentLoader'
+import { DependencyManager } from '../../../plugins'
+import * as uuid from '../../../utils/uuid'
+import { UpdateAssistant } from '../UpdateAssistant'
 
-// Backup date / time is the unique identifier for a backup, needs to be unique for every test
-const backupDate = new Date('2023-11-23T22:50:20.522Z')
+const backupDate = new Date('2024-02-05T22:50:20.522Z')
 jest.useFakeTimers().setSystemTime(backupDate)
 
-describe('UpdateAssistant | Tenants | v0.4 - v0.5', () => {
-  it(`should correctly update the tenant records`, async () => {
+const walletConfig = {
+  id: `Wallet: 0.5 Update`,
+  key: `Key: 0.5 Update`,
+}
+
+describe('UpdateAssistant | v0.4 - v0.5', () => {
+  it(`should correctly add 'type' tag to w3c records`, async () => {
     // We need to mock the uuid generation to make sure we generate consistent uuids for the new records created.
     let uuidCounter = 1
-    const uuidSpy = jest.spyOn(utils, 'uuid').mockImplementation(() => `${uuidCounter++}-4e4f-41d9-94c4-f49351b811f1`)
+    const uuidSpy = jest.spyOn(uuid, 'uuid').mockImplementation(() => `${uuidCounter++}-4e4f-41d9-94c4-f49351b811f1`)
 
-    const tenantRecordsString = readFileSync(path.join(__dirname, '__fixtures__/tenants-no-label-tag-0.4.json'), 'utf8')
+    const aliceW3cCredentialRecordsString = readFileSync(
+      path.join(__dirname, '__fixtures__/alice-2-w3c-credential-records-0.4.json'),
+      'utf8'
+    )
 
     const dependencyManager = new DependencyManager()
     const storageService = new InMemoryStorageService()
@@ -36,15 +41,13 @@ describe('UpdateAssistant | Tenants | v0.4 - v0.5', () => {
       {
         config: {
           label: 'Test Agent',
-          walletConfig: {
-            id: `Wallet: 0.5 Update Tenants`,
-            key: `Key: 0.5 Update Tenants`,
-          },
+          walletConfig,
         },
         dependencies: agentDependencies,
         modules: {
-          // We need to include the TenantsModule to run the updates
-          tenants: new TenantsModule(),
+          w3cCredentials: new W3cCredentialsModule({
+            documentLoader: customDocumentLoader,
+          }),
         },
       },
       dependencyManager
@@ -62,7 +65,7 @@ describe('UpdateAssistant | Tenants | v0.4 - v0.5', () => {
     // is opened as an existing wallet instead of a new wallet
     storageService.contextCorrelationIdToRecords = {
       default: {
-        records: JSON.parse(tenantRecordsString),
+        records: JSON.parse(aliceW3cCredentialRecordsString),
         creationDate: new Date(),
       },
     }
@@ -76,12 +79,11 @@ describe('UpdateAssistant | Tenants | v0.4 - v0.5', () => {
       },
     ])
 
-    await updateAssistant.update('0.5')
+    await updateAssistant.update()
 
-    expect(await updateAssistant.isUpToDate('0.5')).toBe(true)
+    expect(await updateAssistant.isUpToDate()).toBe(true)
     expect(await updateAssistant.getNeededUpdates('0.5')).toEqual([])
 
-    await storageService.deleteById(agent.context, MediatorRoutingRecord, 'MEDIATOR_ROUTING_RECORD')
     expect(storageService.contextCorrelationIdToRecords[agent.context.contextCorrelationId].records).toMatchSnapshot()
 
     await agent.shutdown()
