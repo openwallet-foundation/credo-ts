90b661f9f452c195d072576e8b8e10b307721ebc
Timo Glastra <timo@animo.id>
2024-02-12 15:59:03 +0700

    debug log
    Signed-off-by: Timo Glastra <timo@animo.id>

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,6 @@ import base from './jest.config.base'
 const config: Config.InitialOptions = {
   ...base,
   roots: ['<rootDir>'],
-  verbose: true,
   coveragePathIgnorePatterns: ['/build/', '/node_modules/', '/__tests__/', 'tests'],
   coverageDirectory: '<rootDir>/coverage/',
   projects: [
--- tests/e2e-http.test.ts
+++ tests/e2e-http.test.ts
@@ -1,6 +1,7 @@
 import type { AnonCredsTestsAgent } from '../packages/anoncreds/tests/legacyAnonCredsSetup'
 
 import { getAnonCredsIndyModules } from '../packages/anoncreds/tests/legacyAnonCredsSetup'
+import { TestLogger } from '../packages/core/tests'
 import { getInMemoryAgentOptions } from '../packages/core/tests/helpers'
 
 import { e2eTest } from './e2e-test'
@@ -12,12 +13,15 @@ import {
   MediatorPickupStrategy,
   MediationRecipientModule,
   MediatorModule,
+  LogLevel,
 } from '@credo-ts/core'
 import { HttpInboundTransport } from '@credo-ts/node'
 
 const recipientAgentOptions = getInMemoryAgentOptions(
   'E2E HTTP Recipient',
-  {},
+  {
+    logger: new TestLogger(LogLevel.trace),
+  },
   {
     ...getAnonCredsIndyModules({
       autoAcceptCredentials: AutoAcceptCredential.ContentApproved,
@@ -34,6 +38,7 @@ const mediatorAgentOptions = getInMemoryAgentOptions(
   'E2E HTTP Mediator',
   {
     endpoints: [`http://localhost:${mediatorPort}`],
+    logger: new TestLogger(LogLevel.trace),
   },
   {
     ...getAnonCredsIndyModules({
@@ -50,6 +55,7 @@ const senderAgentOptions = getInMemoryAgentOptions(
   'E2E HTTP Sender',
   {
     endpoints: [`http://localhost:${senderPort}`],
+    logger: new TestLogger(LogLevel.trace),
   },
   getAnonCredsIndyModules({
     autoAcceptCredentials: AutoAcceptCredential.ContentApproved,
