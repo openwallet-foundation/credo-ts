8c467337263ac9f0f26552920ab06b8c1c5e6e65
Timo Glastra <timo@animo.id>
2024-04-25 15:55:27 +0100

    feat: did rotate event (#1840)
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/core/src/modules/connections/ConnectionEvents.ts
+++ packages/core/src/modules/connections/ConnectionEvents.ts
@@ -4,6 +4,7 @@ import type { BaseEvent } from '../../agent/Events'
 
 export enum ConnectionEventTypes {
   ConnectionStateChanged = 'ConnectionStateChanged',
+  ConnectionDidRotated = 'ConnectionDidRotated',
 }
 
 export interface ConnectionStateChangedEvent extends BaseEvent {
@@ -11,5 +12,21 @@ export interface ConnectionStateChangedEvent extends BaseEvent {
   payload: {
     connectionRecord: ConnectionRecord
     previousState: DidExchangeState | null
+  }
+}
+
+export interface ConnectionDidRotatedEvent extends BaseEvent {
+  type: typeof ConnectionEventTypes.ConnectionDidRotated
+  payload: {
+    connectionRecord: ConnectionRecord
+
+    ourDid?: {
+      from: string
+      to: string
+    }
+    theirDid?: {
+      from: string
+      to: string
+    }
   }
 }
