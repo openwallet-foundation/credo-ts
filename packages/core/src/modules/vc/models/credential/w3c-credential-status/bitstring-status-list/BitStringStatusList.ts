fdd2eb4f6c1d555e0d0da9457b2ef832fcce88d7
Krishna Waske <krishna.waske@ayanworks.com>
2025-01-22 12:48:21 +0530

    refactor: separate bitstring statuslist and bitstring status list credential
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- packages/core/src/modules/vc/models/credential/w3c-credential-status/bitstring-status-list/BitStringStatusList.ts
+++ packages/core/src/modules/vc/models/credential/w3c-credential-status/bitstring-status-list/BitStringStatusList.ts
@@ -1,8 +1,6 @@
 import { Type } from 'class-transformer'
 import { IsEnum, IsNumber, IsString } from 'class-validator'
 
-import { JsonLdCredentialDetail, W3cCredentialSubject } from '../../../../../..'
-import { W3cCredential, W3cCredentialOptions } from '../../W3cCredential'
 import { W3cCredentialStatus } from '../W3cCredentialStatus'
 
 // The purpose can be anything apart from this as well
@@ -42,8 +40,10 @@ export class BitStringStatusListMessage {
  * @see https://www.w3.org/TR/vc-bitstring-status-list/#bitstringstatuslistentry
  */
 export class BitStringStatusListEntry extends W3cCredentialStatus {
+  public static type = 'BitstringStatusListEntry'
+
   public constructor(options: IBitStringStatusListEntryOptions) {
-    super({ ...options, type: BitStringStatusListEntry.type })
+    super({ id: options.id, type: BitStringStatusListEntry.type })
 
     if (options) {
       this.statusPurpose = options.statusPurpose
@@ -54,7 +54,6 @@ export class BitStringStatusListEntry extends W3cCredentialStatus {
       if (options.statusMessage) this.statusMessage = options.statusMessage
     }
   }
-  public static type = 'BitstringStatusListEntry'
 
   @IsEnum(BitstringStatusListCredentialStatusPurpose)
   @IsString()
@@ -103,54 +102,3 @@ export interface IBitStringStatusListEntryOptions {
   statusReference?: string | string[]
 }
 
-export class BitStringStatusListCredentialStatus extends W3cCredential {
-  public credentialStatus?: BitStringStatusListEntry | undefined
-}
-
-export class BitStringStatusListCredentialDetail extends JsonLdCredentialDetail {
-  public credential!: BitStringStatusListCredentialStatus
-}
-
-/**
- * StatusListCredential describes the format of the verifiable credential that encapsulates the status list.
- *
- * @see https://www.w3.org/TR/vc-bitstring-status-list/#bitstringstatuslistcredential
- */
-export class BitStringStatusListCredential extends W3cCredential {
-  public constructor(options: IBitStringStatusListCredentialOptions) {
-    super({
-      ...options,
-      type: BitStringStatusListCredential.type,
-    })
-
-    if (options) {
-      this.credentialSubject = options.credentialSubject
-    }
-  }
-  public static type = ['VerifiableCredential', 'BitstringStatusListCredential']
-
-  @IsString()
-  public credentialSubject!: BitStringStatusListCredentialSubject
-}
-
-export interface IBitStringStatusListCredentialOptions extends Omit<W3cCredentialOptions, 'credentialStatus'> {
-  type: ['VerifiableCredential', 'BitstringStatusListCredential']
-  credentialSubject: BitStringStatusListCredentialSubject
-}
-
-// Define an interface for `credentialSubject`
-export interface BitStringStatusListCredentialSubject extends W3cCredentialSubject {
-  type: 'BitstringStatusList'
-  statusPurpose: BitstringStatusListCredentialStatusPurpose
-  encodedList: string
-}
-
-// // Define an interface for the `credential` object that uses `CredentialSubject`
-// export interface Credential {
-//   credentialSubject: CredentialSubject
-// }
-
-// // Use the `Credential` interface within `BitStringStatusListCredential`
-// export interface BitStringStatusListCredential {
-//   credential: Credential
-// }
