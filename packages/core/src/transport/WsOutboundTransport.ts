53c7cb4d01c8fac84dc7c463966bfc5cdaca2ffa
Timo Glastra <timo@animo.id>
2025-01-23 13:41:54 +0800

    style: format
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/transport/WsOutboundTransport.ts
+++ packages/core/src/transport/WsOutboundTransport.ts
@@ -41,15 +41,17 @@ export class WsOutboundTransport implements OutboundTransport {
     this.transportTable.forEach((socket) => {
       socket.removeEventListener('message', this.handleMessageEvent)
       if (socket.readyState !== this.WebSocketClass.CLOSED) {
-        stillOpenSocketClosingPromises.push(new Promise((resolve) => {
-          const closeHandler = () => {
-            socket.removeEventListener('close', closeHandler)
-            resolve()
-          }
-          
-          socket.addEventListener('close', closeHandler)
-        }))
-        
+        stillOpenSocketClosingPromises.push(
+          new Promise((resolve) => {
+            const closeHandler = () => {
+              socket.removeEventListener('close', closeHandler)
+              resolve()
+            }
+
+            socket.addEventListener('close', closeHandler)
+          })
+        )
+
         socket.close()
       }
     })
