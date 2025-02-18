18467205915db58a9256d6b722706d1beaac965d
Timo Glastra <timo@animo.id>
2025-02-18 12:12:10 +0800

    fix typing dcql selection
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/src/modules/dcql/models/index.ts
+++ packages/core/src/modules/dcql/models/index.ts
@@ -20,21 +20,21 @@ export type DcqlMdocCredential = _DcqlMdocCredential.Model['Input']
 export type DcqlSdJwtVcCredential = _DcqlSdJwtVcCredential.Model['Input']
 export type DcqlW3cVcCredential = _DcqlW3cVcCredential.Model['Input']
 
+type DcqlEntrySuccess = _DcqlQueryResult['credential_matches'][string]['all'][number][number] & {
+  success: true
+  record: MdocRecord | SdJwtVcRecord | W3cCredentialRecord
+}
+
+type DcqlEntryNoSuccess = _DcqlQueryResult['credential_matches'][string]['all'][number][number] & {
+  success: false
+  record?: undefined
+}
+
 export type DcqlMatchWithRecord =
   | (_DcqlQueryResult['credential_matches'][string] & {
       success: true
       record: MdocRecord | SdJwtVcRecord | W3cCredentialRecord
-      all: Array<
-        Array<
-          | (_DcqlQueryResult['credential_matches'][string]['all'][number][number] & {
-              success: true
-              record: MdocRecord | SdJwtVcRecord | W3cCredentialRecord
-            })
-          | (_DcqlQueryResult['credential_matches'][string]['all'][number][number] & {
-              success: false
-            })
-        >
-      >
+      all: Array<Array<DcqlEntrySuccess | DcqlEntryNoSuccess>>
     })
   | (_DcqlQueryResult['credential_matches'][string] & {
       success: false
