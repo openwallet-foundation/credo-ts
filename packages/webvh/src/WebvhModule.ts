3fd41d1a1eca69ac292fd66e970b33e041d0cb17
Brian Richter <brian@aviary.tech>
2025-05-22 10:00:14 -0700

    refactor(webvh): remove WebvhModuleConfig and simplify WebvhModule initialization
    - Deleted WebvhModuleConfig class and its related exports to streamline the module configuration.
    - Updated WebvhModule to remove dependency on WebvhModuleConfig, simplifying its constructor.
    - Adjusted test setup to reflect the removal of configuration options.
    
    Signed-off-by: Brian Richter <brian@aviary.tech>

--- packages/webvh/src/WebvhModule.ts
+++ packages/webvh/src/WebvhModule.ts
@@ -1,20 +1,12 @@
 import type { DependencyManager, Module } from '@credo-ts/core'
 
-import { WebvhModuleConfig } from './WebvhModuleConfig'
 import { WebvhDidResolver } from './dids/WebvhDidResolver'
 
 export class WebvhModule implements Module {
-  public readonly config: WebvhModuleConfig
-
-  public constructor(config?: WebvhModuleConfig) {
-    this.config = config ?? new WebvhModuleConfig()
-  }
-
   /**
    * Registers the dependencies of the WebVH module on the dependency manager.
    */
   public register(dependencyManager: DependencyManager) {
-    dependencyManager.registerInstance(WebvhModuleConfig, this.config)
     dependencyManager.registerSingleton(WebvhDidResolver)
   }
 }
--- packages/webvh/src/index.ts
+++ packages/webvh/src/index.ts
@@ -1,5 +1,4 @@
 export { WebvhDidResolver } from './dids'
 
 export { WebvhModule } from './WebvhModule'
-export { WebvhModuleConfig, WebvhModuleConfigOptions } from './WebvhModuleConfig'
 export { WebvhApi } from './WebvhApi'
