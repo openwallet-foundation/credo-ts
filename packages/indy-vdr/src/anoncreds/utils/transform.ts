2c59162648dc6aa3820d0ca90faa19a2f5557b99
Tarun Vadde <142969534+tarunvaddeSoul@users.noreply.github.com>
2025-07-22 17:28:37 +0530

    fix: correct indyVdrCreateLatestRevocationDelta to allow individual credential revocation/unrevocation (#2319)
    Signed-off-by: tarunvaddeSoul <tarun@soulverse.com>
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/indy-vdr/src/anoncreds/utils/transform.ts
+++ packages/indy-vdr/src/anoncreds/utils/transform.ts
@@ -94,8 +94,8 @@ export function indyVdrCreateLatestRevocationDelta(
 
   if (previousDelta) {
     revocationStatusList.forEach((revocationStatus, idx) => {
-      // Check whether the revocationStatusList entry is not included in the previous delta issued indices
-      if (revocationStatus === RevocationState.Active && !previousDelta.issued.includes(idx)) {
+      // If the current status is Active and this index was previously revoked, it means the credential was just unrevoked, so add it to the issued list
+      if (revocationStatus === RevocationState.Active && previousDelta.revoked.includes(idx)) {
         issued.push(idx)
       }
 
