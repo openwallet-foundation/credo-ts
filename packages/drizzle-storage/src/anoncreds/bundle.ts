4b97160c36cefa2ccccfe15eb8db7127f9ab2bf9
Timo Glastra <timo@animo.id>
2025-08-12 17:20:15 +0200

    chore: address feedback
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/drizzle-storage/src/anoncreds/bundle.ts
+++ packages/drizzle-storage/src/anoncreds/bundle.ts
@@ -1,12 +1,12 @@
 import type { DrizzleRecordBundle } from '../DrizzleRecord'
 import { bundleMigrationDefinition } from '../util'
-import { anonCredsCredentialDrizzleRecord } from './credential-record'
-import { anonCredsCredentialDefinitionDrizzleRecord } from './credential-definition-record'
 import { anonCredsCredentialDefinitionPrivateDrizzleRecord } from './credential-definition-private-record'
+import { anonCredsCredentialDefinitionDrizzleRecord } from './credential-definition-record'
+import { anonCredsCredentialDrizzleRecord } from './credential-record'
 import { anonCredsKeyCorrectnessProofDrizzleRecord } from './key-correctness-proof-record'
 import { anonCredsLinkSecretDrizzleRecord } from './link-secret-record'
-import { anonCredsRevocationRegistryDefinitionDrizzleRecord } from './revocation-registry-definition-record'
 import { anonCredsRevocationRegistryDefinitionPrivateDrizzleRecord } from './revocation-registry-definition-private-record'
+import { anonCredsRevocationRegistryDefinitionDrizzleRecord } from './revocation-registry-definition-record'
 import { anonCredsSchemaDrizzleRecord } from './schema-record'
 
 export default {
