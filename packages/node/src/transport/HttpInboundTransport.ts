ec539d800c2154817cf5fefc38231fff5395dcb5
jamshale <31809382+jamshale@users.noreply.github.com>
2025-08-20 13:06:38 -0700

    fix(didcomm): prevent buildup of AgentMessageProcessed listeners under load (#2374)
    Signed-off-by: jamshale <jamiehalebc@gmail.com>
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/node/src/transport/HttpInboundTransport.ts
+++ packages/node/src/transport/HttpInboundTransport.ts
@@ -12,7 +12,7 @@ import type { Express, Request, Response } from 'express'
 import { CredoError, EventEmitter, utils } from '@credo-ts/core'
 import { AgentEventTypes, DidCommMimeType, DidCommModuleConfig, TransportService } from '@credo-ts/didcomm'
 import express, { text } from 'express'
-import { ReplaySubject, filter, firstValueFrom, timeout } from 'rxjs'
+import { ReplaySubject, filter, firstValueFrom, take, timeout } from 'rxjs'
 
 const supportedContentTypes: string[] = [DidCommMimeType.V0, DidCommMimeType.V1]
 
@@ -84,7 +84,8 @@ export class HttpInboundTransport implements InboundTransport {
             timeout({
               first: this.processedMessageListenerTimeoutMs,
               meta: 'HttpInboundTransport.start',
-            })
+            }),
+            take(1) // automatically unsubscribe after the first matching event
           )
           .subscribe(subject)
 
