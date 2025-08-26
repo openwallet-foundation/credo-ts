fc04df2ee6348e05d7984ab21983613331480c2c
jamshale <jamiehalebc@gmail.com>
2025-08-26 15:57:15 -0700

    feat: pickupV2 backwards compatibility with implicit pickup
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- packages/core/src/modules/connections/services/TrustPingService.ts
+++ packages/core/src/modules/connections/services/TrustPingService.ts
@@ -4,8 +4,17 @@ import type { TrustPingMessage } from '../messages'
 import type { ConnectionRecord } from '../repository/ConnectionRecord'
 
 import { EventEmitter } from '../../../agent/EventEmitter'
+import { MessageSender } from '../../../agent/MessageSender'
+import { AgentContext } from '../../../agent/context/AgentContext'
 import { OutboundMessageContext } from '../../../agent/models'
+import { InjectionSymbols } from '../../../constants'
 import { injectable } from '../../../plugins'
+import { MessagePickupSessionRole } from '../../message-pickup/MessagePickupSession'
+import { MessagePickupSessionService } from '../../message-pickup/services/MessagePickupSessionService'
+import { MessagePickupRepository } from '../../message-pickup/storage/MessagePickupRepository'
+import { QueuedMessage } from '../../message-pickup/storage/QueuedMessage'
+import { MediatorModuleConfig } from '../../routing/MediatorModuleConfig'
+import { MessageForwardingStrategy } from '../../routing/MessageForwardingStrategy'
 import { TrustPingEventTypes } from '../TrustPingEvents'
 import { TrustPingResponseMessage } from '../messages'
 
@@ -25,6 +34,8 @@ export class TrustPingService {
         message: message,
       },
     })
+
+    this.handleBackwardsCompatibilityImplicitPickup(agentContext, connection)
 
     if (message.responseRequested) {
       const response = new TrustPingResponseMessage({
@@ -47,5 +58,44 @@ export class TrustPingService {
         message: message,
       },
     })
+  }
+
+  private handleBackwardsCompatibilityImplicitPickup(agentContext: AgentContext, connection: ConnectionRecord) {
+    if (
+      agentContext.dependencyManager.resolve(MediatorModuleConfig).messageForwardingStrategy ===
+      MessageForwardingStrategy.QueueAndLiveModeDelivery
+    ) {
+      const pickupLiveSessionService = agentContext.dependencyManager.resolve(MessagePickupSessionService)
+      const messagePickupRepository = agentContext.dependencyManager.resolve<MessagePickupRepository>(
+        InjectionSymbols.MessagePickupRepository
+      )
+      const messageSender = agentContext.dependencyManager.resolve(MessageSender)
+
+      // We need to allow the session to be created before creating the live pickup session and sending messages
+      setTimeout(async () => {
+        if (!pickupLiveSessionService.getLiveSessionByConnectionId(agentContext, { connectionId: connection.id })) {
+          pickupLiveSessionService.saveLiveSession(agentContext, {
+            connectionId: connection.id,
+            protocolVersion: 'v2',
+            role: MessagePickupSessionRole.MessageHolder,
+          })
+        }
+        const messagesToDeliver = (await messagePickupRepository.takeFromQueue({
+          connectionId: connection.id,
+          limit: 10, // Default limit, can be adjusted
+          deleteMessages: true,
+        })) as QueuedMessage[]
+        if (messagesToDeliver.length > 0) {
+          for (const message of messagesToDeliver) {
+            await messageSender?.sendPackage(agentContext, {
+              connection: connection,
+              recipientKey: connection.did || connection.id,
+              encryptedMessage: message.encryptedMessage,
+              options: { transportPriority: { schemes: ['ws', 'wss'], restrictive: true } },
+            })
+          }
+        }
+      }, 1000)
+    }
   }
 }
