14dd7bdba7916aa4dc706a9916fd9107e140e57d
jamshale <jamiehalebc@gmail.com>
2025-08-27 15:28:39 -0700

    Add tests and small refactor
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- packages/core/src/modules/connections/services/TrustPingService.ts
+++ packages/core/src/modules/connections/services/TrustPingService.ts
@@ -12,7 +12,6 @@ import { injectable } from '../../../plugins'
 import { MessagePickupSessionRole } from '../../message-pickup/MessagePickupSession'
 import { MessagePickupSessionService } from '../../message-pickup/services/MessagePickupSessionService'
 import { MessagePickupRepository } from '../../message-pickup/storage/MessagePickupRepository'
-import { QueuedMessage } from '../../message-pickup/storage/QueuedMessage'
 import { MediatorModuleConfig } from '../../routing/MediatorModuleConfig'
 import { MessageForwardingStrategy } from '../../routing/MessageForwardingStrategy'
 import { TrustPingEventTypes } from '../TrustPingEvents'
@@ -61,18 +60,21 @@ export class TrustPingService {
   }
 
   private handleBackwardsCompatibilityImplicitPickup(agentContext: AgentContext, connection: ConnectionRecord) {
-    if (
-      agentContext.dependencyManager.resolve(MediatorModuleConfig).messageForwardingStrategy ===
-      MessageForwardingStrategy.QueueAndLiveModeDelivery
-    ) {
-      const pickupLiveSessionService = agentContext.dependencyManager.resolve(MessagePickupSessionService)
-      const messagePickupRepository = agentContext.dependencyManager.resolve<MessagePickupRepository>(
-        InjectionSymbols.MessagePickupRepository
-      )
-      const messageSender = agentContext.dependencyManager.resolve(MessageSender)
+    const config = agentContext.dependencyManager.resolve(MediatorModuleConfig)
 
-      // We need to allow the session to be created before creating the live pickup session and sending messages
-      setTimeout(async () => {
+    if (config.messageForwardingStrategy !== MessageForwardingStrategy.QueueAndLiveModeDelivery) {
+      return
+    }
+
+    const pickupLiveSessionService = agentContext.dependencyManager.resolve(MessagePickupSessionService)
+    const messagePickupRepository = agentContext.dependencyManager.resolve<MessagePickupRepository>(
+      InjectionSymbols.MessagePickupRepository
+    )
+    const messageSender = agentContext.dependencyManager.resolve(MessageSender)
+
+    // Allow session creation before creating the live pickup session
+    setTimeout(async () => {
+      try {
         if (!pickupLiveSessionService.getLiveSessionByConnectionId(agentContext, { connectionId: connection.id })) {
           pickupLiveSessionService.saveLiveSession(agentContext, {
             connectionId: connection.id,
@@ -80,22 +82,26 @@ export class TrustPingService {
             role: MessagePickupSessionRole.MessageHolder,
           })
         }
-        const messagesToDeliver = (await messagePickupRepository.takeFromQueue({
+
+        const messagesToDeliver = await messagePickupRepository.takeFromQueue({
           connectionId: connection.id,
-          limit: 10, // Default limit, can be adjusted
+          limit: 10,
           deleteMessages: true,
-        })) as QueuedMessage[]
-        if (messagesToDeliver.length > 0) {
-          for (const message of messagesToDeliver) {
-            await messageSender?.sendPackage(agentContext, {
-              connection: connection,
-              recipientKey: connection.did || connection.id,
-              encryptedMessage: message.encryptedMessage,
-              options: { transportPriority: { schemes: ['ws', 'wss'], restrictive: true } },
-            })
-          }
+        })
+
+        if (messagesToDeliver.length === 0) return
+
+        for (const message of messagesToDeliver) {
+          await messageSender.sendPackage(agentContext, {
+            connection,
+            recipientKey: connection.did ?? connection.id,
+            encryptedMessage: message.encryptedMessage,
+            options: { transportPriority: { schemes: ['ws', 'wss'], restrictive: true } },
+          })
         }
-      }, 1000)
-    }
+      } catch (error) {
+        agentContext.config.logger?.error(`Error during implicit pickup for connection ${connection.id}`, { error })
+      }
+    }, 100)
   }
 }
