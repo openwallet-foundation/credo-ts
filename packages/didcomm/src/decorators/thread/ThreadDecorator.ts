6eb4ed0c23a11ef2ddc67f992afc7b9d1a1b678d
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2025-10-03 13:00:35 +0100

    feat: allow DID URLs in pthid
    Signed-off-by: Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>

--- packages/didcomm/src/decorators/thread/ThreadDecorator.ts
+++ packages/didcomm/src/decorators/thread/ThreadDecorator.ts
@@ -1,7 +1,16 @@
-import { Expose } from 'class-transformer'
-import { IsInt, IsOptional, Matches } from 'class-validator'
+import {Expose} from "class-transformer";
+import {IsInt, IsOptional, Matches} from "class-validator";
 
-import { MessageIdRegExp } from '../../BaseDidCommMessage'
+import {MessageIdRegExp} from "../../BaseDidCommMessage";
+
+// ^did:[a-z0-9]+:(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9.-])+  # domain (no ':', allows %XX)
+// (?::(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9._~!$&'()*+,;=:@/?-])*)*  # extra colon-separated segments
+// (?:\?(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9._~!$&'()*+,;=:@/?-])*)?  # optional query
+// (?:#(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9._~!$&'()*+,;=:@/?-])*)?   # optional fragment
+// $
+
+const PthidRegExp =
+  /^([-_./A-Za-z0-9]{8,64}|did:[a-z0-9]+:(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9.-])+(?::(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9._~!$&'()*+,;=:@/?-])*)*(?:\?(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9._~!$&'()*+,;=:@/?-])*)?(?:#(?:%[0-9A-Fa-f]{2}|[A-Za-z0-9._~!$&'()*+,;=:@/?-])*)?)$/;
 
 /**
  * Represents `~thread` decorator
@@ -9,41 +18,41 @@ import { MessageIdRegExp } from '../../BaseDidCommMessage'
  */
 export class ThreadDecorator {
   public constructor(partial?: Partial<ThreadDecorator>) {
-    this.threadId = partial?.threadId
-    this.parentThreadId = partial?.parentThreadId
-    this.senderOrder = partial?.senderOrder
-    this.receivedOrders = partial?.receivedOrders
+    this.threadId = partial?.threadId;
+    this.parentThreadId = partial?.parentThreadId;
+    this.senderOrder = partial?.senderOrder;
+    this.receivedOrders = partial?.receivedOrders;
   }
 
   /**
    * The ID of the message that serves as the thread start.
    */
-  @Expose({ name: 'thid' })
-  @Matches(MessageIdRegExp)
+  @Expose({name: "thid"})
+  @Matches(PthidRegExp)
   @IsOptional()
-  public threadId?: string
+  public threadId?: string;
 
   /**
    * An optional parent `thid`. Used when branching or nesting a new interaction off of an existing one.
    */
-  @Expose({ name: 'pthid' })
-  @Matches(MessageIdRegExp)
+  @Expose({name: "pthid"})
+  @Matches(PthidRegExp)
   @IsOptional()
-  public parentThreadId?: string
+  public parentThreadId?: string;
 
   /**
    * A number that tells where this message fits in the sequence of all messages that the current sender has contributed to this thread.
    */
-  @Expose({ name: 'sender_order' })
+  @Expose({name: "sender_order"})
   @IsOptional()
   @IsInt()
-  public senderOrder?: number
+  public senderOrder?: number;
 
   /**
    * Reports the highest `sender_order` value that the sender has seen from other sender(s) on the thread.
    * This value is often missing if it is the first message in an interaction, but should be used otherwise, as it provides an implicit ACK.
    */
-  @Expose({ name: 'received_orders' })
+  @Expose({name: "received_orders"})
   @IsOptional()
-  public receivedOrders?: { [key: string]: number }
+  public receivedOrders?: {[key: string]: number};
 }
