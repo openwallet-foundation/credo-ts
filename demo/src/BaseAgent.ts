321b2e8cd7dbf6a4a1321757ae4b7b2167ee2f63
Alexander Shenshin <93187809+AlexanderShenshin@users.noreply.github.com>
2025-10-06 13:23:08 +0300

    feat: Add Hedera module (#2405)
    * feat: Add Hedera module
    
    Signed-off-by: Andrey Kononov <andrey.kononov@dsr-corporation.com>
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    Co-authored-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * docs(changeset): Add Hedera module
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * feat(hedera): Use DID SDK Signer interface
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * chore(hedera): Updates after merging changes from 'main'
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * chore(hedera): Update Hiero DID SDK to 0.1.5
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * fix(jest): Update snapshot guide links
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * feat(hedera): Support DID record keys removal + refactor PublicJwk usage
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    * chore(hedera): Update package exports
    
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    
    ---------
    
    Signed-off-by: Andrey Kononov <andrey.kononov@dsr-corporation.com>
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
    Co-authored-by: Andrey Kononov <andrey.kononov@dsr-corporation.com>

--- demo/src/BaseAgent.ts
+++ demo/src/BaseAgent.ts
@@ -32,11 +32,13 @@ import {
 } from '@credo-ts/didcomm'
 import { IndyVdrAnonCredsRegistry, IndyVdrIndyDidResolver, IndyVdrModule } from '@credo-ts/indy-vdr'
 import { DidCommHttpInboundTransport, agentDependencies } from '@credo-ts/node'
+import { HederaNetwork } from '@hiero-did-sdk/client'
 import { anoncreds } from '@hyperledger/anoncreds-nodejs'
 import { indyVdr } from '@hyperledger/indy-vdr-nodejs'
 import { askar } from '@openwallet-foundation/askar-nodejs'
 
 import { AskarModuleConfigStoreOptions } from '@credo-ts/askar'
+import { HederaAnonCredsRegistry, HederaDidRegistrar, HederaDidResolver, HederaModule } from '@credo-ts/hedera'
 import { greenText } from './OutputClass'
 
 const bcovrin = `{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node1","blskey":"4N8aUNHSgjQVgkpm8nhNEfDf6txHznoYREg9kirmJrkivgL4oSEimFF6nsQ6M41QvhM2Z33nves5vfSn9n1UwNFJBYtWVnHYMATn76vLuL3zU88KyeAYcHfsih3He6UHcXDxcaecHVz6jhCYz1P2UZn2bDVruL5wXpehgBfBaLKm3Ba","blskey_pop":"RahHYiCvoNCtPTrVtP7nMC5eTYrsUA8WjXbdhNc8debh1agE9bGiJxWBXYNFbnJXoXhWFMvyqhqhRoq737YQemH5ik9oL7R4NTTCz2LEZhkgLJzB3QRQqJyBNyv7acbdHrAT8nQ9UkLbaVL9NBpnWXBTw4LEMePaSHEw66RzPNdAX1","client_ip":"138.197.138.255","client_port":9702,"node_ip":"138.197.138.255","node_port":9701,"services":["VALIDATOR"]},"dest":"Gw6pDLhcBcoQesN72qfotTgFa7cbuqZpkX3Xo6pLhPhv"},"metadata":{"from":"Th7MpTaRZVRYnPiabds81Y"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"fea82e10e894419fe2bea7d96296a6d46f50f93f9eeda954ec461b2ed2950b62"},"ver":"1"}
@@ -113,7 +115,7 @@ function getAskarAnonCredsIndyModules(
       ],
     }),
     anoncreds: new AnonCredsModule({
-      registries: [new IndyVdrAnonCredsRegistry(), new CheqdAnonCredsRegistry()],
+      registries: [new IndyVdrAnonCredsRegistry(), new CheqdAnonCredsRegistry(), new HederaAnonCredsRegistry()],
       anoncreds,
     }),
     indyVdr: new IndyVdrModule({
@@ -132,12 +134,23 @@ function getAskarAnonCredsIndyModules(
       })
     ),
     dids: new DidsModule({
-      resolvers: [new IndyVdrIndyDidResolver(), new CheqdDidResolver()],
-      registrars: [new CheqdDidRegistrar()],
+      resolvers: [new IndyVdrIndyDidResolver(), new CheqdDidResolver(), new HederaDidResolver()],
+      registrars: [new CheqdDidRegistrar(), new HederaDidRegistrar()],
     }),
     askar: new AskarModule({
       askar,
       store: askarStoreConfig,
+    }),
+    hedera: new HederaModule({
+      networks: [
+        {
+          network: (process.env.HEDERA_NETWORK as HederaNetwork) ?? 'testnet',
+          operatorId: process.env.HEDERA_OPERATOR_ID ?? '0.0.5489553',
+          operatorKey:
+            process.env.HEDERA_OPERATOR_KEY ??
+            '302e020100300506032b6570042204209f54b75b6238ced43e41b1463999cb40bf2f7dd2c9fd4fd3ef780027c016a138',
+        },
+      ],
     }),
   } as const
 }
