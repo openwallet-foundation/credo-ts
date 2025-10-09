5b2506dbe0da8d4714d473631b6052af02433b61
Ariel Gentile <gentilester@gmail.com>
2025-10-09 09:14:06 -0300

    test: update
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- packages/didcomm/src/updates/0.5-0.6/__tests__/connectionRecord.test.ts
+++ packages/didcomm/src/updates/0.5-0.6/__tests__/connectionRecord.test.ts
@@ -39,9 +39,9 @@ describe('0.5-0.6 | Migration | DIDComm Connection Record', () => {
   describe('migrateConnectionRecordToV0_6()', () => {
     it('should fetch all records and apply the needed updates ', async () => {
       const records: DidCommConnectionRecord[] = [
-        getConnectionRecord({ alias: 'alias-1', theirDid: 'theirDid-1' }),
+        getConnectionRecord({ alias: 'alias-1', theirLabel: 'theirLabel-1' }),
         getConnectionRecord({}),
-        getConnectionRecord({ alias: 'alias-3', theirDid: 'theirDid-3' }),
+        getConnectionRecord({ alias: 'alias-3', theirLabel: 'theirLabel-3' }),
       ]
 
       mockFunction(connectionRepository.getAll).mockResolvedValue(records)
@@ -49,12 +49,20 @@ describe('0.5-0.6 | Migration | DIDComm Connection Record', () => {
       await testModule.migrateConnectionRecordToV0_6(agent)
 
       expect(connectionRepository.getAll).toHaveBeenCalledTimes(1)
-      expect(connectionRepository.update).toHaveBeenCalledTimes(1)
+      expect(connectionRepository.update).toHaveBeenCalledTimes(3)
 
-      const [, connectionRecord] = mockFunction(connectionRepository.update).mock.calls[0]
-      expect(connectionRecord.toJSON()).toMatchObject({
-        tags: { alias: 'alias-1', theirDid: 'theirDid' },
+      const [, connectionRecord1] = mockFunction(connectionRepository.update).mock.calls[0]
+      expect(connectionRecord1.toJSON()).toMatchObject({
+        _tags: expect.objectContaining({ alias: 'alias-1', theirLabel: 'theirLabel-1' }),
       })
+      const [, connectionRecord2] = mockFunction(connectionRepository.update).mock.calls[1]
+      expect(connectionRecord2.toJSON()).toMatchObject({
+        _tags: expect.objectContaining({ alias: undefined, theirLabel: undefined }),
+      })
+      const [, connectionRecord3] = mockFunction(connectionRepository.update).mock.calls[2]
+      expect(connectionRecord3.toJSON()).toMatchObject({
+        _tags: expect.objectContaining({ alias: 'alias-3', theirLabel: 'theirLabel-3' }),
+      })            
     })
   })
 
@@ -62,19 +70,19 @@ describe('0.5-0.6 | Migration | DIDComm Connection Record', () => {
     id,
     metadata,
     alias,
-    theirDid,
+    theirLabel,
   }: {
     id?: string
     metadata?: Record<string, unknown>
     alias?: string
-    theirDid?: string
+    theirLabel?: string
   }) {
     return JsonTransformer.fromJSON(
       {
         id: id ?? 'connection-id',
         metadata,
         alias,
-        theirDid,
+        theirLabel,
       },
       DidCommConnectionRecord
     )
