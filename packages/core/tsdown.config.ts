4981d7f120bacf5680a9f7b994b68f27b466256f
Timo Glastra <timo@animo.id>
2025-10-09 21:59:09 +0200

    fix: import at top
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -1,4 +1,23 @@
 import { defineConfig } from 'tsdown'
 import config from '../../tsdown.config.base'
 
-export default defineConfig(config)
+export default defineConfig(
+  config.map((entry) => ({
+    ...entry,
+    plugins: [
+      ...(entry.plugins ?? []),
+      {
+        // Adds reflect-metadata import to top of core package src/index file
+        // because the imports are reordered and that causes issues since
+        // reflect metadata needs to be imported first
+        // See: https://github.com/rolldown/rolldown/issues/6436
+        name: 'import-reflect-metadata-at-top',
+        banner: (chunk) => {
+          if (chunk.fileName === 'index.js') return "require('reflect-metadata')"
+          if (chunk.fileName === 'index.mjs') return "import 'reflect-metadata'"
+          return ''
+        },
+      },
+    ],
+  }))
+)
--- tsdown.config.base.ts
+++ tsdown.config.base.ts
@@ -38,4 +38,4 @@ export default [
       },
     ],
   },
-] as const satisfies UserConfig
+] satisfies UserConfig
