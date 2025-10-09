2b2fefaa73fc7136ee91fc22517ff5f3411076dc
Timo Glastra <timo@animo.id>
2025-10-09 23:21:08 +0200

    fix: core bundle entry
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/drizzle-storage/tsdown.config.ts
+++ packages/drizzle-storage/tsdown.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsdown'
 import config from '../../tsdown.config.base'
 
-const bundles = ['didcomm', 'action-menu', 'core', 'drpc', 'tenants', 'openid4vc', 'anoncreds', 'question-answer']
+// NOTE: core is exlcuded since we already import it, so we don't have to add it as entry.
+const bundles = ['didcomm', 'action-menu', 'drpc', 'tenants', 'openid4vc', 'anoncreds', 'question-answer']
 const bundleEntry = (bundle: string) => [
   `src/${bundle}/bundle.ts`,
   `src/${bundle}/sqlite.ts`,
