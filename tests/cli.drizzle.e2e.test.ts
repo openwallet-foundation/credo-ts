ac5fd302134fa72ae486f257193d2ac97a83d951
Timo Glastra <timo@animo.id>
2025-11-13 14:54:50 +0100

    fix: drizzle setup
    Signed-off-by: Timo Glastra <timo@animo.id>

--- tests/cli.drizzle.e2e.test.ts
+++ tests/cli.drizzle.e2e.test.ts
@@ -1,6 +1,16 @@
 import { cli } from '../packages/drizzle-storage/cli/cli-definition'
 import { createDrizzlePostgresTestDatabase } from '../packages/drizzle-storage/tests/testDatabase'
 
+// Since we use dynamic imports vite(st) won't recognize that these files
+// need to be transpiled and result in errors. We import them manually here
+import '@credo-ts/drizzle-storage/didcomm'
+import '@credo-ts/drizzle-storage/anoncreds'
+import '@credo-ts/drizzle-storage/action-menu'
+import '@credo-ts/drizzle-storage/drpc'
+import '@credo-ts/drizzle-storage/openid4vc'
+import '@credo-ts/drizzle-storage/tenants'
+import '@credo-ts/drizzle-storage/question-answer'
+
 describe('Credo Drizzle CLI', () => {
   test('generate migrations', async () => {
     await cli.route([
