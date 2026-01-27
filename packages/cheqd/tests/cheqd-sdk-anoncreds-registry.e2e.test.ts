f85e70832968d44c7341ada7d13326dad0454c19
DaevMithran <daevmithran1999@gmail.com>
2026-01-27 17:14:43 +0530

    fix: Run cache and uncached tests sequentially
    Signed-off-by: DaevMithran <daevmithran1999@gmail.com>

--- packages/cheqd/tests/cheqd-sdk-anoncreds-registry.e2e.test.ts
+++ packages/cheqd/tests/cheqd-sdk-anoncreds-registry.e2e.test.ts
@@ -1,4 +1,13 @@
 import { cheqdAnonCredsRegistryTest } from './cheqd-sdk-anoncreds-registry-base'
 import { cheqdPayerSeeds } from './setupCheqdModule'
 
-cheqdAnonCredsRegistryTest(false, cheqdPayerSeeds[2])
+describe('Cheqd AnonCreds Registry Sequential Tests', () => {
+  describe('Without Cache', () => {
+    cheqdAnonCredsRegistryTest(false, cheqdPayerSeeds[2])
+  })
+
+  describe('With Cache', () => {
+    // This will only start after the "Without Cache" suite finishes
+    cheqdAnonCredsRegistryTest(true, cheqdPayerSeeds[4])
+  })
+})
