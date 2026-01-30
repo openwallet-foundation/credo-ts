39404bf6eb2b6c1ee569fdc3e4114d3baf51d37d
Tshering Dawa <tsheringdawa@bhutanndi.bt>
2026-01-30 12:06:38 +0600

    formate code
    Signed-off-by: Tshering Dawa <tsheringdawa@bhutanndi.bt>

--- packages/core/src/storage/migration/updates/0.3-0.3.1/did.ts
+++ packages/core/src/storage/migration/updates/0.3-0.3.1/did.ts
@@ -19,34 +19,36 @@ export async function migrateDidRecordToV0_3_1<Agent extends BaseAgent>(agent: A
   const allDids = await didRepository.getAll(agent.context)
 
   agent.config.logger.debug(`Found a total of ${allDids.length} did records to update.`)
-    for (const didRecord of allDids) {
-        agent.config.logger.debug(`Migrating did record with id ${didRecord.id} to storage version 0.3.1`);
+  for (const didRecord of allDids) {
+    agent.config.logger.debug(`Migrating did record with id ${didRecord.id} to storage version 0.3.1`)
 
-        // Save old DID or ID for reference/deletion
-        const oldId = didRecord.id;
+    // Save old DID or ID for reference/deletion
+    const oldId = didRecord.id
 
-        // Generate new storage ID
-        const newId = uuid();
+    // Generate new storage ID
+    const newId = uuid()
 
-        agent.config.logger.debug(`Updating id ${oldId} to ${newId} for did record`);
+    agent.config.logger.debug(`Updating id ${oldId} to ${newId} for did record`)
 
-        // Preserve the actual DID in didRecord.did
-        if (!didRecord.did) {
-            didRecord.did = oldId; // fallback if didRecord.did was empty
-        }
-
-        didRecord.id = newId;
-
-        // Save new did record
-        await didRepository.save(agent.context, didRecord);
-
-        // Delete old did record
-        await didRepository.deleteById(agent.context, oldId);
-
-        if (!didRecord.did.startsWith('did:')) {
-            throw new Error(`Invalid DID after migration: ${didRecord.did}`);
-        }
-
-        agent.config.logger.debug(`Successfully migrated did record with old id ${oldId} to new id ${newId} to storage version 0.3.1`);
+    // Preserve the actual DID in didRecord.did
+    if (!didRecord.did) {
+      didRecord.did = oldId // fallback if didRecord.did was empty
     }
+
+    didRecord.id = newId
+
+    // Save new did record
+    await didRepository.save(agent.context, didRecord)
+
+    // Delete old did record
+    await didRepository.deleteById(agent.context, oldId)
+
+    if (!didRecord.did.startsWith('did:')) {
+      throw new Error(`Invalid DID after migration: ${didRecord.did}`)
+    }
+
+    agent.config.logger.debug(
+      `Successfully migrated did record with old id ${oldId} to new id ${newId} to storage version 0.3.1`
+    )
+  }
 }
