f34dd3a05dec9559a62b4cfd204c0422fa534ae5
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-05-07 19:09:40 +0100

    feat(webvh): expose module API
    Signed-off-by: rmlearney-digicatapult <robert.learney@digicatapult.org.uk>

--- packages/webvh/src/WebVhModule.ts
+++ packages/webvh/src/WebVhModule.ts
@@ -1,13 +1,17 @@
 import type { DependencyManager, Module } from '@credo-ts/core'
 
+import { WebVhApi } from './WebVhApi'
 import { WebVhDidRegistrar, WebVhDidResolver } from './dids'
 
 export class WebVhModule implements Module {
+  public readonly api = WebVhApi
+
   /**
    * Registers the dependencies of the WebVH module on the dependency manager.
    */
   public register(dependencyManager: DependencyManager) {
     dependencyManager.registerSingleton(WebVhDidResolver)
     dependencyManager.registerSingleton(WebVhDidRegistrar)
+    dependencyManager.registerContextScoped(WebVhApi)
   }
 }
