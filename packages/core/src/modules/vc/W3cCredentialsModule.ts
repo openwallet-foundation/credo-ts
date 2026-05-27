57f2f9f4db513e7c7be3cb3f7d9ef31d6699b2e8
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-05-27 18:13:51 +0100

    linting
    Signed-off-by: rmlearney-digicatapult <robert.learney@digicatapult.org.uk>

--- packages/core/src/modules/vc/W3cCredentialsModule.ts
+++ packages/core/src/modules/vc/W3cCredentialsModule.ts
@@ -4,10 +4,14 @@ import {
   VERIFICATION_METHOD_TYPE_ED25519_VERIFICATION_KEY_2020,
 } from '../dids'
 import { Ed25519PublicJwk } from '../kms'
-import { SignatureSuiteRegistry, SignatureSuiteToken, type SuiteInfo } from './linked-data-proofs/SignatureSuiteRegistry'
+import { W3cJwtCredentialService } from './jwt-vc'
+import {
+  SignatureSuiteRegistry,
+  SignatureSuiteToken,
+  type SuiteInfo,
+} from './linked-data-proofs/SignatureSuiteRegistry'
 import { Ed25519Signature2018, Ed25519Signature2020 } from './linked-data-proofs/signature-suites'
 import { W3cJsonLdCredentialService } from './linked-data-proofs/W3cJsonLdCredentialService'
-import { W3cJwtCredentialService } from './jwt-vc'
 import { W3cCredentialRepository } from './repository/W3cCredentialRepository'
 import { W3cCredentialService } from './W3cCredentialService'
 import { W3cCredentialsApi } from './W3cCredentialsApi'
