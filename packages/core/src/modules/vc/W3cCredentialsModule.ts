338576997a18fb5d2e644d476e12ffa613b6a61c
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-05-27 18:13:01 +0100

    reduce import diff churn
    Signed-off-by: rmlearney-digicatapult <robert.learney@digicatapult.org.uk>

--- packages/core/src/modules/vc/W3cCredentialsModule.ts
+++ packages/core/src/modules/vc/W3cCredentialsModule.ts
@@ -4,9 +4,9 @@ import {
   VERIFICATION_METHOD_TYPE_ED25519_VERIFICATION_KEY_2020,
 } from '../dids'
 import { Ed25519PublicJwk } from '../kms'
-import { SignatureSuiteRegistry, SignatureSuiteToken, type SuiteInfo } from './data-integrity/SignatureSuiteRegistry'
-import { Ed25519Signature2018, Ed25519Signature2020 } from './data-integrity/signature-suites'
-import { W3cJsonLdCredentialService } from './data-integrity/W3cJsonLdCredentialService'
+import { SignatureSuiteRegistry, SignatureSuiteToken, type SuiteInfo } from './linked-data-proofs/SignatureSuiteRegistry'
+import { Ed25519Signature2018, Ed25519Signature2020 } from './linked-data-proofs/signature-suites'
+import { W3cJsonLdCredentialService } from './linked-data-proofs/W3cJsonLdCredentialService'
 import { W3cJwtCredentialService } from './jwt-vc'
 import { W3cCredentialRepository } from './repository/W3cCredentialRepository'
 import { W3cCredentialService } from './W3cCredentialService'
