063523db68ce30d69e0ed5bf04c86638d19d8e4a
Tarun Vadde <vaddeofficial@gmail.com>
2026-06-09 22:56:47 +0530

    fix(drizzle-storage): regenerate didcomm migrations (mediation drift + connection didcommVersion)
    Signed-off-by: Tarun Vadde <vaddeofficial@gmail.com>

---
+++ packages/drizzle-storage/migrations/didcomm/sqlite/0002_open_killmonger.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `DidcommMediation` ADD `routing_did` text;--> statement-breakpoint
+ALTER TABLE `DidcommMediation` ADD `recipient_dids` text;--> statement-breakpoint
+ALTER TABLE `DidcommMediation` ADD `mediation_protocol_version` text;
--- packages/drizzle-storage/migrations/didcomm/sqlite/migrations.js
+++ packages/drizzle-storage/migrations/didcomm/sqlite/migrations.js
@@ -2,6 +2,8 @@
 
 import m0000 from './0000_initial.sql'
 import m0001 from './0001_remove_linked_attachments.sql'
+import m0002 from './0002_open_killmonger.sql'
+import m0003 from './0003_low_killer_shrike.sql'
 import journal from './meta/_journal.json'
 
 export default {
@@ -9,5 +11,7 @@ export default {
   migrations: {
     m0000,
     m0001,
+    m0002,
+    m0003,
   },
 }
