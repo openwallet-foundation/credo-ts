6fb882a19b71d319b24bb659feca1dba3d55291c
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-06-11 10:49:26 +0100

    vc2 model routing
    Signed-off-by: rmlearney-digicatapult <robert.learney@digicatapult.org.uk>

--- packages/core/src/modules/vc/W3cV2CredentialServiceOptions.ts
+++ packages/core/src/modules/vc/W3cV2CredentialServiceOptions.ts
@@ -1,44 +1,63 @@
 import type { HashName } from '../../crypto'
 import type { KnownJwaSignatureAlgorithm } from '../kms'
 import type { IDisclosureFrame, IPresentationFrame, SdJwtVcHolderBinding } from '../sd-jwt-vc'
+import type {
+  W3cV2DataIntegrityVerifiableCredential,
+  W3cV2DataIntegrityVerifiablePresentation,
+} from './data-integrity-v1'
 import { W3cV2JwtVerifiableCredential, W3cV2JwtVerifiablePresentation } from './jwt-vc'
 import type { ClaimFormat, W3cV2Credential, W3cV2Presentation } from './models'
 import type { W3cV2CredentialRecord } from './repository'
 import { W3cV2SdJwtVerifiableCredential, W3cV2SdJwtVerifiablePresentation } from './sd-jwt-vc'
 
+type W3cV2CredentialSignOptionsByFormat = {
+  [ClaimFormat.JwtW3cVc]: W3cV2JwtSignCredentialOptions
+  [ClaimFormat.SdJwtW3cVc]: W3cV2SdJwtSignCredentialOptions
+  [ClaimFormat.DiVc]: W3cV2DiSignCredentialOptions
+}
+
+type W3cV2CredentialVerifyOptionsByFormat = {
+  [ClaimFormat.JwtW3cVc]: W3cV2JwtVerifyCredentialOptions
+  [ClaimFormat.SdJwtW3cVc]: W3cV2SdJwtVerifyCredentialOptions
+  [ClaimFormat.DiVc]: W3cV2DiVerifyCredentialOptions
+}
+
+type W3cV2PresentationSignOptionsByFormat = {
+  [ClaimFormat.JwtW3cVp]: W3cV2JwtSignPresentationOptions
+  [ClaimFormat.SdJwtW3cVp]: W3cV2SdJwtSignPresentationOptions
+  [ClaimFormat.DiVp]: W3cV2DiSignPresentationOptions
+}
+
 export type W3cV2SignCredentialOptions<
-  Format extends ClaimFormat.JwtW3cVc | ClaimFormat.SdJwtW3cVc | undefined = undefined,
-> = Format extends ClaimFormat.JwtW3cVc
-  ? W3cV2JwtSignCredentialOptions
-  : Format extends ClaimFormat.SdJwtW3cVc
-    ? W3cV2SdJwtSignCredentialOptions
-    : W3cV2JwtSignCredentialOptions | W3cV2SdJwtSignCredentialOptions
+  Format extends keyof W3cV2CredentialSignOptionsByFormat | undefined = undefined,
+> = Format extends keyof W3cV2CredentialSignOptionsByFormat
+  ? W3cV2CredentialSignOptionsByFormat[Format]
+  : W3cV2CredentialSignOptionsByFormat[keyof W3cV2CredentialSignOptionsByFormat]
 
 export type W3cV2VerifyCredentialOptions<
-  Format extends ClaimFormat.JwtW3cVc | ClaimFormat.SdJwtW3cVc | undefined = undefined,
-> = Format extends ClaimFormat.JwtW3cVc
-  ? W3cV2JwtVerifyCredentialOptions
-  : Format extends ClaimFormat.SdJwtW3cVc
-    ? W3cV2SdJwtVerifyCredentialOptions
-    : W3cV2JwtVerifyCredentialOptions | W3cV2SdJwtVerifyCredentialOptions
+  Format extends keyof W3cV2CredentialVerifyOptionsByFormat | undefined = undefined,
+> = Format extends keyof W3cV2CredentialVerifyOptionsByFormat
+  ? W3cV2CredentialVerifyOptionsByFormat[Format]
+  : W3cV2CredentialVerifyOptionsByFormat[keyof W3cV2CredentialVerifyOptionsByFormat]
 
 export type W3cV2SignPresentationOptions<
-  Format extends ClaimFormat.JwtW3cVp | ClaimFormat.SdJwtW3cVp | undefined = undefined,
-> = Format extends ClaimFormat.JwtW3cVp
-  ? W3cV2JwtSignPresentationOptions
-  : Format extends ClaimFormat.SdJwtW3cVp
-    ? W3cV2SdJwtSignPresentationOptions
-    : W3cV2JwtSignPresentationOptions | W3cV2SdJwtSignPresentationOptions
+  Format extends keyof W3cV2PresentationSignOptionsByFormat | undefined = undefined,
+> = Format extends keyof W3cV2PresentationSignOptionsByFormat
+  ? W3cV2PresentationSignOptionsByFormat[Format]
+  : W3cV2PresentationSignOptionsByFormat[keyof W3cV2PresentationSignOptionsByFormat]
 
-export type W3cV2VerifyPresentationOptions = W3cV2JwtVerifyPresentationOptions | W3cV2SdJwtVerifyPresentationOptions
+export type W3cV2VerifyPresentationOptions =
+  | W3cV2JwtVerifyPresentationOptions
+  | W3cV2SdJwtVerifyPresentationOptions
+  | W3cV2DiVerifyPresentationOptions
 
-interface W3cV2SignCredentialOptionsBase {
+interface W3cV2SignCredentialOptionsBase<Format extends ClaimFormat = ClaimFormat> {
   /**
    * The format of the credential to be signed.
    *
    * @see https://identity.foundation/claim-format-registry
    */
-  format: ClaimFormat
+  format: Format
 
   /**
    * The credential to be signed.
@@ -53,9 +72,7 @@ interface W3cV2SignCredentialOptionsBase {
   verificationMethod: string
 }
 
-export interface W3cV2JwtSignCredentialOptions extends W3cV2SignCredentialOptionsBase {
-  format: ClaimFormat.JwtW3cVc
-
+export interface W3cV2JwtSignCredentialOptions extends W3cV2SignCredentialOptionsBase<ClaimFormat.JwtW3cVc> {
   /**
    * The algorithm used to sign the JWT VC.
    */
@@ -68,9 +85,7 @@ export interface W3cV2JwtSignCredentialOptions extends W3cV2SignCredentialOption
   holder?: SdJwtVcHolderBinding
 }
 
-export interface W3cV2SdJwtSignCredentialOptions extends W3cV2SignCredentialOptionsBase {
-  format: ClaimFormat.SdJwtW3cVc
-
+export interface W3cV2SdJwtSignCredentialOptions extends W3cV2SignCredentialOptionsBase<ClaimFormat.SdJwtW3cVc> {
   /**
    * The algorithm used to sign the SD-JWT VC.
    */
@@ -96,25 +111,30 @@ export interface W3cV2SdJwtSignCredentialOptions extends W3cV2SignCredentialOpti
   disclosureFrame?: IDisclosureFrame
 }
 
-interface W3cV2VerifyCredentialOptionsBase {
-  credential: unknown
+interface W3cV2VerifyCredentialOptionsBase<Credential = unknown> {
+  credential: Credential
+
+  /**
+   * Whether to verify credentialStatus, if present.
+   *
+   * When false/undefined, VC2 verification does not fail on credentialStatus presence.
+   */
+  verifyCredentialStatus?: boolean
 }
 
-export interface W3cV2JwtVerifyCredentialOptions extends W3cV2VerifyCredentialOptionsBase {
-  credential: W3cV2JwtVerifiableCredential | string // string must be encoded VC JWT
-}
+export interface W3cV2JwtVerifyCredentialOptions
+  extends W3cV2VerifyCredentialOptionsBase<W3cV2JwtVerifiableCredential | string> {}
 
-export interface W3cV2SdJwtVerifyCredentialOptions extends W3cV2VerifyCredentialOptionsBase {
-  credential: W3cV2SdJwtVerifiableCredential | string // string must be encoded VC SD-JWT
-}
+export interface W3cV2SdJwtVerifyCredentialOptions
+  extends W3cV2VerifyCredentialOptionsBase<W3cV2SdJwtVerifiableCredential | string> {}
 
-interface W3cV2SignPresentationOptionsBase {
+interface W3cV2SignPresentationOptionsBase<Format extends ClaimFormat = ClaimFormat> {
   /**
    * The format of the presentation to be signed.
    *
    * @see https://identity.foundation/claim-format-registry
    */
-  format: ClaimFormat
+  format: Format
 
   /**
    * The presentation to be signed.
@@ -132,13 +152,9 @@ interface W3cV2SignPresentationOptionsBase {
   domain?: string
 }
 
-export interface W3cV2JwtSignPresentationOptions extends W3cV2SignPresentationOptionsBase {
-  format: ClaimFormat.JwtW3cVp
-}
+export interface W3cV2JwtSignPresentationOptions extends W3cV2SignPresentationOptionsBase<ClaimFormat.JwtW3cVp> {}
 
-export interface W3cV2SdJwtSignPresentationOptions extends W3cV2SignPresentationOptionsBase {
-  format: ClaimFormat.SdJwtW3cVp
-
+export interface W3cV2SdJwtSignPresentationOptions extends W3cV2SignPresentationOptionsBase<ClaimFormat.SdJwtW3cVp> {
   /**
    * The hashing algorithm to use for creating digests of the disclosures.
    *
@@ -153,11 +169,11 @@ export interface W3cV2SdJwtSignPresentationOptions extends W3cV2SignPresentation
   disclosureFrame?: IDisclosureFrame
 }
 
-interface W3cV2VerifyPresentationOptionsBase {
+interface W3cV2VerifyPresentationOptionsBase<Presentation = unknown> {
   /**
    * The presentation to be verified.
    */
-  presentation: unknown
+  presentation: Presentation
 
   /**
    * The challenge / nonce that must present in the presentation prevent replay attacks.
@@ -170,13 +186,11 @@ interface W3cV2VerifyPresentationOptionsBase {
   domain?: string
 }
 
-export interface W3cV2JwtVerifyPresentationOptions extends W3cV2VerifyPresentationOptionsBase {
-  presentation: W3cV2JwtVerifiablePresentation | string // string must be encoded VP JWT
-}
+export interface W3cV2JwtVerifyPresentationOptions
+  extends W3cV2VerifyPresentationOptionsBase<W3cV2JwtVerifiablePresentation | string> {}
 
-export interface W3cV2SdJwtVerifyPresentationOptions extends W3cV2VerifyPresentationOptionsBase {
-  presentation: W3cV2SdJwtVerifiablePresentation | string // string must be encoded VP SD-JWT
-}
+export interface W3cV2SdJwtVerifyPresentationOptions
+  extends W3cV2VerifyPresentationOptionsBase<W3cV2SdJwtVerifiablePresentation | string> {}
 
 export interface W3cV2StoreCredentialOptions {
   record: W3cV2CredentialRecord
@@ -190,3 +204,42 @@ export interface W3cV2SdJwtVcPresentOptions {
    */
   presentationFrame?: IPresentationFrame | true
 }
+
+/**
+ * Data Integrity (di_vc) credential signing options
+ */
+export interface W3cV2DiSignCredentialOptions extends W3cV2SignCredentialOptionsBase<ClaimFormat.DiVc> {
+  /**
+   * The cryptosuite to use for signing (e.g., 'eddsa-jcs-2022')
+   */
+  cryptosuite: string
+}
+
+/**
+ * Data Integrity (di_vc) credential verification options
+ */
+export interface W3cV2DiVerifyCredentialOptions
+  extends W3cV2VerifyCredentialOptionsBase<W3cV2DataIntegrityVerifiableCredential> {}
+
+/**
+ * Data Integrity (di_vp) presentation signing options
+ */
+export interface W3cV2DiSignPresentationOptions extends W3cV2SignPresentationOptionsBase<ClaimFormat.DiVp> {
+  /**
+   * URI of the verificationMethod to be used for signing the presentation proof.
+   *
+   * Must be a valid did url pointing to a key.
+   */
+  verificationMethod: string
+
+  /**
+   * The cryptosuite to use for signing (e.g., 'eddsa-jcs-2022')
+   */
+  cryptosuite: string
+}
+
+/**
+ * Data Integrity (di_vp) presentation verification options
+ */
+export interface W3cV2DiVerifyPresentationOptions
+  extends W3cV2VerifyPresentationOptionsBase<W3cV2DataIntegrityVerifiablePresentation> {}
--- packages/core/src/modules/vc/models/presentation/W3cV2JsonPresentation.ts
+++ packages/core/src/modules/vc/models/presentation/W3cV2JsonPresentation.ts
@@ -1,11 +1,40 @@
-import type { JsonObject } from '../../../../types'
+import type { JsonObject, SingleOrArray } from '../../../../types'
 import type { W3cV2JsonCredential } from '../credential/W3cV2JsonCredential'
+
+export interface W3cV2JsonEnvelopedVerifiableCredentialEntry {
+  '@context': string | Array<string | JsonObject>
+  id: string
+  type: string | Array<string>
+  [key: string]: unknown
+}
+
+export interface W3cV2JsonEnvelopedVerifiablePresentationEntry {
+  '@context': string | Array<string | JsonObject>
+  id: string
+  type: string | Array<string>
+  [key: string]: unknown
+}
+
+export interface W3cV2JsonDataIntegrityPresentationEntry {
+  '@context': string | Array<string | JsonObject>
+  type: string | Array<string>
+  proof: unknown
+  verifiableCredential?: SingleOrArray<W3cV2JsonPresentationCredentialEntry>
+  [key: string]: unknown
+}
+
+export type W3cV2JsonPresentationCredentialEntry =
+  | W3cV2JsonCredential
+  | W3cV2JsonDataIntegrityPresentationEntry
+  | W3cV2JsonEnvelopedVerifiableCredentialEntry
+  | W3cV2JsonEnvelopedVerifiablePresentationEntry
+  | string
 
 export interface W3cV2JsonPresentation {
   '@context': string | Array<string | JsonObject>
   id?: string
   type: string | Array<string>
   holder?: string | { id: string; [property: string]: unknown }
-  verifiableCredential: Array<W3cV2JsonCredential | string>
+  verifiableCredential: SingleOrArray<W3cV2JsonPresentationCredentialEntry>
   [key: string]: unknown
 }
