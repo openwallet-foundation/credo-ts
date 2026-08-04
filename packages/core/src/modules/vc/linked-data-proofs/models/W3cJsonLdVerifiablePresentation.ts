907cc540bfe430a88fcde791fbcb51917ab64490
Robert M. Learney <39194639+rmlearney-digicatapult@users.noreply.github.com>
2026-08-04 11:21:38 +0100

    refactor(vc): anoncreds data integrity namespace change (#2813)
    Signed-off-by: rmlearney-digicatapult <robert.learney@digicatapult.org.uk>

--- packages/core/src/modules/vc/linked-data-proofs/models/W3cJsonLdVerifiablePresentation.ts
+++ packages/core/src/modules/vc/linked-data-proofs/models/W3cJsonLdVerifiablePresentation.ts
@@ -1,41 +1,62 @@
+import { CredoError } from '../../../../error'
 import type { SingleOrArray } from '../../../../types'
 import { asArray, IsInstanceOrArrayOfInstances, JsonTransformer } from '../../../../utils'
+import type { AnonCredsW3cCredentialProofOptions } from '../../anoncreds-w3c-credential'
+import { ANONCREDS_W3C_CREDENTIAL_CRYPTOSUITE, AnonCredsW3cCredentialProof } from '../../anoncreds-w3c-credential'
 import { ClaimFormat } from '../../models'
 import type { W3cPresentationOptions } from '../../models/presentation/W3cPresentation'
 import { W3cPresentation } from '../../models/presentation/W3cPresentation'
 import { ProofTransformer } from '../proof-ops/ProofTransformer'
-import type { DataIntegrityProofOptions } from './DataIntegrityProof'
-import { DataIntegrityProof } from './DataIntegrityProof'
 import type { LinkedDataProofOptions } from './LinkedDataProof'
 import { LinkedDataProof } from './LinkedDataProof'
 
 export interface W3cJsonLdVerifiablePresentationOptions extends W3cPresentationOptions {
-  proof: LinkedDataProofOptions | DataIntegrityProofOptions
+  proof: LinkedDataProofOptions | AnonCredsW3cCredentialProofOptions
+}
+
+const mapProofOptionToProofClass = (proof: LinkedDataProofOptions | AnonCredsW3cCredentialProofOptions) => {
+  if ('cryptosuite' in proof) {
+    if (proof.type !== 'DataIntegrityProof' || proof.cryptosuite !== ANONCREDS_W3C_CREDENTIAL_CRYPTOSUITE) {
+      throw new CredoError(
+        `W3C credential proofs only support DataIntegrityProof with cryptosuite ${ANONCREDS_W3C_CREDENTIAL_CRYPTOSUITE}`
+      )
+    }
+
+    return new AnonCredsW3cCredentialProof(proof)
+  }
+
+  return new LinkedDataProof(proof)
 }
 
 export class W3cJsonLdVerifiablePresentation extends W3cPresentation {
   public constructor(options: W3cJsonLdVerifiablePresentationOptions) {
     super(options)
     if (options) {
-      if (options.proof.cryptosuite) this.proof = new DataIntegrityProof(options.proof)
-      else this.proof = new LinkedDataProof(options.proof as LinkedDataProofOptions)
+      this.proof = mapProofOptionToProofClass(options.proof)
     }
   }
 
   @ProofTransformer()
-  @IsInstanceOrArrayOfInstances({ classType: [LinkedDataProof, DataIntegrityProof] })
-  public proof!: SingleOrArray<LinkedDataProof | DataIntegrityProof>
+  @IsInstanceOrArrayOfInstances({ classType: [LinkedDataProof, AnonCredsW3cCredentialProof] })
+  public proof!: SingleOrArray<LinkedDataProof | AnonCredsW3cCredentialProof>
 
   public get proofTypes(): Array<string> {
     const proofArray = asArray(this.proof) ?? []
     return proofArray.map((proof) => proof.type)
   }
 
-  public get dataIntegrityCryptosuites(): Array<string> {
+  public get anonCredsW3cCredentialCryptosuites(): Array<string> {
     const proofArray = asArray(this.proof) ?? []
     return proofArray
-      .filter((proof): proof is DataIntegrityProof => proof.type === 'DataIntegrityProof' && 'cryptosuite' in proof)
+      .filter(
+        (proof): proof is AnonCredsW3cCredentialProof => proof.type === 'DataIntegrityProof' && 'cryptosuite' in proof
+      )
       .map((proof) => proof.cryptosuite)
+  }
+
+  /** @deprecated Use anonCredsW3cCredentialCryptosuites */
+  public get dataIntegrityCryptosuites(): Array<string> {
+    return this.anonCredsW3cCredentialCryptosuites
   }
 
   public toJson() {
