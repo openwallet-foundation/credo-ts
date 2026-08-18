9956d506f2ed519d44bfc0f7c48efd591bbaa5ed
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2026-08-18 20:00:59 +0100

    use helper in PAR flow
    Signed-off-by: rmlearney-digicatapult <robert.learney@digicatapult.org.uk>

--- packages/openid4vc/src/openid4vc-issuer/router/pushedAuthorizationRequestEndpoint.ts
+++ packages/openid4vc/src/openid4vc-issuer/router/pushedAuthorizationRequestEndpoint.ts
@@ -206,30 +206,22 @@ export async function handlePushedAuthorizationRequest(
     additionalRequestPayload = dynamicConfiguration.additionalPayload
     redirectUris = dynamicConfiguration.redirectUris
   } else {
-    for (const scope of requestedScopes) {
-      if (!authorizationServerConfig.scopesMapping) {
-        throw new Oauth2ServerErrorResponseError(
-          {
-            error: Oauth2ErrorCodes.ServerError,
-          },
-          {
-            internalMessage: `Issuer '${issuer.issuerId}' does not have a scope mapping for scope '${scope}' for external authorization server '${authorizationServerConfig.issuer}'`,
-          }
-        )
-      }
-
-      if (scope in authorizationServerConfig.scopesMapping) {
-        scopes.push(...authorizationServerConfig.scopesMapping[scope])
-      } else {
-        throw new Oauth2ServerErrorResponseError(
-          {
-            error: Oauth2ErrorCodes.ServerError,
-          },
-          {
-            internalMessage: `Issuer '${issuer.issuerId}' does not have a scope mapping for scope '${scope}' for external authorization server '${authorizationServerConfig.issuer}'`,
-          }
-        )
-      }
+    try {
+      scopes = openId4VcIssuerService.getStaticChainedAuthorizationScopes({
+        authorizationServerConfig,
+        requestedScopes,
+      })
+    } catch (error) {
+      throw new Oauth2ServerErrorResponseError(
+        {
+          error: Oauth2ErrorCodes.ServerError,
+          error_description: 'Invalid chained authorization server scope mapping.',
+        },
+        {
+          internalMessage: `Invalid scope mapping for chained authorization server '${authorizationServerConfig.issuer}'.`,
+          cause: error,
+        }
+      )
     }
   }
 
