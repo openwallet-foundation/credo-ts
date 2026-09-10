3e5034cf02458d972ab99179e2520c9d6c61651b
TimoGlastra <timo@animo.id>
2026-09-10 13:22:44 +0200

    feat: allow setting postgres connection params in askar
    Signed-off-by: TimoGlastra <timo@animo.id>

--- packages/askar/src/AskarStorageConfig.ts
+++ packages/askar/src/AskarStorageConfig.ts
@@ -1,9 +1,39 @@
+/**
+ * Connection parameters that are passed to the underlying postgres driver (sqlx).
+ *
+ * @see https://docs.rs/sqlx/latest/sqlx/postgres/struct.PgConnectOptions.html#parameters
+ */
+export interface AskarPostgresConnectionParameters {
+  sslmode?: 'disable' | 'allow' | 'prefer' | 'require' | 'verify-ca' | 'verify-full'
+  sslrootcert?: string
+  sslcert?: string
+  sslkey?: string
+  'statement-cache-capacity'?: number
+  application_name?: string
+  options?: string
+
+  [key: string]: string | number | boolean | undefined
+}
+
 export interface AskarPostgresConfig {
   host: string
   connectTimeout?: number
   idleTimeout?: number
   maxConnections?: number
   minConnections?: number
+
+  /**
+   * Additional connection parameters that will be added as query parameters to the
+   * postgres connection url. This allows configuring e.g. ssl for the connection
+   * without having to set environment variables.
+   *
+   * Parameters that are derived from the other config and credentials options (`connect_timeout`,
+   * `idle_timeout`, `max_connections`, `min_connections`, `admin_account`, `admin_password`, `host`,
+   * `port`, `user`, `password` and `dbname`) are not allowed.
+   *
+   * @see https://docs.rs/sqlx/latest/sqlx/postgres/struct.PgConnectOptions.html#parameters
+   */
+  connectionParameters?: AskarPostgresConnectionParameters
 }
 
 export interface AskarSqliteConfig {
