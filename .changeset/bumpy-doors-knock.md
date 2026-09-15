e376e8e4caa2ced90167fc5439a24a067c482393
George Mulhearn <gmulhearn@anonyome.com>
2026-09-16 09:14:43 +1000

    some review fixes
    Signed-off-by: George Mulhearn <gmulhearn@anonyome.com>

--- .changeset/bumpy-doors-knock.md
+++ .changeset/bumpy-doors-knock.md
@@ -1,5 +1,5 @@
 ---
-"@credo-ts/didcomm": minor
+"@credo-ts/didcomm": patch
 ---
 
 Added Aries RFC 881 (vc+sd-jwt) format handler
--- packages/didcomm/src/modules/credentials/formats/dataIntegrity/DidCommDataIntegrityCredentialFormatService.ts
+++ packages/didcomm/src/modules/credentials/formats/dataIntegrity/DidCommDataIntegrityCredentialFormatService.ts
@@ -67,7 +67,7 @@ import type {
   W3C_VC_DATA_MODEL_VERSION,
 } from './dataIntegrityExchange'
 import { DataIntegrityCredentialOffer } from './dataIntegrityExchange'
-import { createDidCommSignedAttachment, verifyDidCommSignedAttachment } from '../shared'
+import { createDidCommSignedAttachment, verifyDidCommSignedAttachment } from '../shared/didCommSignedAttachment'
 
 const W3C_DATA_INTEGRITY_CREDENTIAL_OFFER = 'didcomm/w3c-di-vc-offer@v0.1'
 const W3C_DATA_INTEGRITY_CREDENTIAL_REQUEST = 'didcomm/w3c-di-vc-request@v0.1'
